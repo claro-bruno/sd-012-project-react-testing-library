ab17a97441c3c29e2990cb76a791792ed39a967c
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 15:28:31 -0300

    Requirement 01-> Test if title "Pokédex" was rendered

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa todo o App.js', () => {
+  it('renderiza "Pokédex"', () => {
+    renderWithRouter(<App />);
+
+    const heading = screen.getByRole('heading', { name: /Pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+});
