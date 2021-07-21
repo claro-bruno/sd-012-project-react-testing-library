a0c842ec764bd5663340e5ad3a1601efabcd47f3
Euller Braz <euller.braz99@gmail.com>
2021-07-21 16:24:55 -0300

    Requisito 2: Adicionado testes a About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test.skip('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About.js', () => {
+  it('Testa as informações da página About', () => {
+    const urlImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    renderWithRouter(<About />);
+    expect(screen.getByText('About Pokédex')).toBeDefined();
+    expect(screen.getByText(/This application simulates a Pokédex/i)).toBeDefined();
+    expect(screen.getByText(/One can filter Pokémons by type/i)).toBeDefined();
+    expect(screen.getByAltText('Pokédex')).toHaveProperty('src', urlImage);
+  });
+});
