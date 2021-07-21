7a60acd916ac209403b6e2d31aab09ef90047614
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-21 17:39:37 -0400

    completa o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithRouter from './helpers/RenderWithRouter';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste do componente FavoritePokemons', () => {
+  test('Verifica se caso não haja Pokemon favorito, renderiza uma mensagem', () => {
+    RenderWithRouter(<FavoritePokemons />);
+
+    const message = 'No favorite pokemon found';
+
+    const notFoundFavorite = screen.getByText(message);
+
+    expect(notFoundFavorite).toBeInTheDocument();
+  });
+
+  test('Verifica se é renderizado os pokemons favoritos', () => {
+    RenderWithRouter(<App />);
+
+    userEvent.click(screen.getByRole('link', { name: /More details/i }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémon/i }));
+
+    const favorites = screen.getByRole('link', { name: /More details/i });
+    expect(favorites).toBeInTheDocument();
+  });
+});
