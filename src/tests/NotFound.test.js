794f98336ced4605fef5711d60fc336fa1f71290
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 18:31:42 -0300

    Requirement 04-> Adding test that checks whether the title "Page requested..." is rendered

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa todo o NotFound.js', () => {
+  it('renderiza titulo "Page requested..."', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeVisible();
+  });
+});
