846429246cad2ff4792c363d32dc5c6d36a47ecf
Adryan Rosa <adryanrosa374@gmail.com>
2021-07-21 15:29:04 -0300

    Adiciona teste para App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente App', () => {
+  test('Testa o link home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkToHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkToHome).toBeInTheDocument();
+    fireEvent.click(linkToHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Testa o link about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkToHome = screen.getByRole('link', { name: 'About' });
+    expect(linkToHome).toBeInTheDocument();
+    fireEvent.click(linkToHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa o link favorites', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkToHome = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkToHome).toBeInTheDocument();
+    fireEvent.click(linkToHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
