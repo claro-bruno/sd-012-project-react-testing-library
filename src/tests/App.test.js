c4db98e3b5b569858f3716eba572753e5633bed9
Willian Prado <willian_prado@hotmail.com>
2021-07-21 15:18:24 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import App from '../App';
+
+describe('Testa os links do componente App', () => {
+  test('', () => {});
+});
