d55b74301a8e57e898059f135296a6592fe63c99
Matheus Proença <mproenca.marketing@gmail.com>
2021-07-21 17:47:05 -0300

    teste componente NotFound - req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../help/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Test component Not Found', () => {
+  it('The page contains one heading h2 with the text Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const $notFoundH2 = screen.getByText(/Page requested not found/i);
+    expect($notFoundH2).toBeInTheDocument('');
+  });
+
+  it('Test if page show the image', () => {
+    renderWithRouter(<NotFound />);
+
+    const $image = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect($image).toBeInTheDocument('');
+
+    const $IMG_GIF = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect($image.src).toBe($IMG_GIF);
+  });
+});
