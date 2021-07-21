5568c7c55d6d0de3410b6ad1eb25084e5cbed4ea
cristianocsl <cslcristiano@gmail.com>
2021-07-21 19:49:47 -0300

    Requisito 4: verifica se o conteudo da heading h2 e Page requested not found 😭 e se a imagem e mostrada na pagina

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste do componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina-que-nao-existe');
+    const noMacth = screen.getByText(/Page requested not found/i);
+    expect(noMacth).toBeDefined();
+    // Feito com ajuda de Daniel Batista - turma 12
+    const link = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByAltText(/Pikachu crying because the page/i);
+    expect(img.src).toBe(link);
+  });
+});
