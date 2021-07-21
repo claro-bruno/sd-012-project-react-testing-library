e1ed62e815c0e9a6fb3a205f787e1b374359e05a
Daniel <danielbped@gmail.com>
2021-07-21 17:51:26 -0300

    Resolve requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Teste componente Pokedex', () => {
+  it('Testa se a página contém um heading com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido o próximo Pokémon ao clicar em "Próximo pokémon"', () => {
+    renderWithRouter(<App />);
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextPokemonButton);
+    const nextPokemonName = screen.getByText(/charmander/i);
+    expect(nextPokemonName).toBeInTheDocument();
+  });
+
+  it('Testa se os botões de filtro funcionam', () => {
+    renderWithRouter(<App />);
+    const fireFilter = screen.getByRole('button', { name: /fire/i });
+    userEvent.click(fireFilter);
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toHaveTextContent(/Fire/i);
+  });
+
+  it('Testa se ao clicar no botão "All", o filtro reseta', () => {
+    renderWithRouter(<App />);
+    const allFilter = screen.getByRole('button', { name: /all/i });
+    userEvent.click(allFilter);
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toHaveTextContent(/electric/i);
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testa o componente Pokemon', () => {
+  it('Testa se o card renderizado contém o nome do Pokémon', () => {
+    renderWithRouter(<App />);
+    const name = screen.getByTestId('pokemon-name');
+    expect(name).toBeInTheDocument();
+  });
+
+  it('Testa se o card renderizado contém o tipo do Pokémon', () => {
+    renderWithRouter(<App />);
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toBeInTheDocument();
+  });
+
+  it('Testa se o card possui o peso do Pokémon', () => {
+    renderWithRouter(<App />);
+    const weight = screen.getByTestId('pokemon-weight');
+    expect(weight).toBeInTheDocument();
+  });
+});
