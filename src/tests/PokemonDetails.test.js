52d133af7919598b06fad7aab8952c4f995a39cd
Adryan Rosa <adryanrosa374@gmail.com>
2021-07-21 19:35:05 -0300

    Finaliza teste para PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
 
-describe('Testa o componente NotFound', () => {
+describe('Testa o componente PokemonDetails', () => {
   const mockedPokemon = pokemons[0];
 
-  test('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela',
+  test('Testa se as informações detalhadas do Pokémon selecionado são mostradas na tela',
     () => {
       const { history } = renderWithRouter(<App />);
       history.push(`/pokemons/${mockedPokemon.id}`);
@@ -22,5 +22,47 @@ describe('Testa o componente NotFound', () => {
       expect(linkToDetail).not.toBeInTheDocument();
       expect(summaryTitle).toBeInTheDocument();
       expect(summary).toBeInTheDocument();
+    });
+
+  test('Teste se existe na página uma seção com os mapas das localizações do pokémon',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push(`/pokemons/${mockedPokemon.id}`);
+
+      const locationsTitle = screen.getByText(`Game Locations of ${mockedPokemon.name}`);
+      expect(locationsTitle).toBeInTheDocument();
+
+      mockedPokemon.foundAt.forEach((area, index) => {
+        const locationName = screen.getByText(area.location);
+        expect(locationName).toBeInTheDocument();
+
+        const imgUrl = area.map;
+
+        const image = screen.getAllByAltText(`${mockedPokemon.name} location`);
+        expect(image[index]).toHaveProperty('src', imgUrl);
+      });
+    });
+
+  test('Teste se o usuário pode favoritar um pokémon através da página de detalhes',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push(`/pokemons/${mockedPokemon.id}`);
+
+      const checkbox = screen.getByLabelText('Pokémon favoritado?');
+      expect(checkbox).toBeInTheDocument();
+
+      expect(screen
+        .queryByAltText(`${mockedPokemon.name} is marked as favorite`))
+        .not.toBeInTheDocument();
+
+      fireEvent.click(checkbox);
+      expect(screen
+        .queryByAltText(`${mockedPokemon.name} is marked as favorite`))
+        .toBeInTheDocument();
+
+      fireEvent.click(checkbox);
+      expect(screen
+        .queryByAltText(`${mockedPokemon.name} is marked as favorite`))
+        .not.toBeInTheDocument();
     });
 });
