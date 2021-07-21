11db20130a65ef154fe972021428498f20918c99
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-07-21 17:50:18 -0300

    Resolve o requisito 2 - Teste o componente <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
+describe('Testa o componente <About.js />', () => {
+  test('Testa se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByText(/About Pokédex/i);
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstP = screen.getByText(/This application/i);
+    expect(firstP).toBeInTheDocument();
+    const secondP = screen.getByText(/One can filter/i);
+    expect(secondP).toBeInTheDocument();
+  });
+  test('Testa se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    const imageSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(imageSrc);
+  });
+});
