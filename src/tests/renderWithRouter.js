d423d88e62fcc378bc2280465b6fbfb373077776
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 14:43:03 -0300

    criando funcao renderWithRouter

---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import render from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history } >
+      { component }
+    </Router>
+  );
+
+  return history;
+}
+
+export default renderWithRouter;
