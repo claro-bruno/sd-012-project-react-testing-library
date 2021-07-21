f536351dccd419e790a29c49f2d32ac67a74791c
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-21 15:22:20 -0300

    Resolve requisito 1 - Teste no App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 
 describe('Testa todo o App', () => {
   it('Testa se o App é renderizado em seu estado inicial', () => {
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -3,12 +3,10 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-//Lib 'history' cria um histórico de navegação
-
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={history}>{component}</Router>), history,
+    ...render(<Router history={ history }>{component}</Router>), history,
   });
 };
 
