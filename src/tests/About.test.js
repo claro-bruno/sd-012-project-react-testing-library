35ef3660b00ec3db5c217597aa75438aa61c4216
gabrielfndz <gabriel.omercador@hotmail.com>
2021-07-21 17:08:42 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+
+import About from '../components/About';
+
+test('Página contém as infos sobre a Pokédex', () => {
+  render(<About />);
+
+  const aboutText = screen.getByText('About Pokédex');
+  expect(aboutText).toBeInTheDocument();
+
+  const paragraph1 = screen.getByText(/This application simulates a Pokédex/i);
+  expect(paragraph1).toBeInTheDocument();
+  const paragraph2 = screen.getByText(/One can filter pokémons by type/i);
+  expect(paragraph2).toBeInTheDocument();
+
+  const imgElement = screen.getByAltText('Pokédex');
+  expect(imgElement.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
