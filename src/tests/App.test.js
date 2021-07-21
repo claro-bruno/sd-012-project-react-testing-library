9701d45548b0511c7e1a1b59560760382896747a
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-07-21 18:29:42 -0300

    faz correcoes ao lint do req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { getByAltText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from '../helper/renderWithRouter';
 
