341c79b8a3c98972994b0c9386d5487fcc2998d2
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:16:06 -0300

    testa h2 com texto especifico notfound.test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,3 +1,14 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 import renderWithRouter from '../renderWithRouter';
+
+describe('Verificações do componente NotFound', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Verifica se possui um h2 com texto específico', () => {
+    expect(screen.getByText('Page requested not found')).toBeTruthy();
+    // ref https://stackoverflow.com/questions/55509875/how-to-query-by-text-string-which-contains-html-tags-using-react-testing-library
+  });
+});
