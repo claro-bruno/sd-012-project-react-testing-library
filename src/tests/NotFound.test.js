4a64762a8215dbe91e1626c6d115bbabe870db04
Euller Braz <euller.braz99@gmail.com>
2021-07-21 17:38:53 -0300

    Requisito 4: Adicionado testes a NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  it('Testa a renderização sem pokemons favoritados', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    const image = screen.getByAltText(/Pikachu crying/i);
+    const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(notFound).toBeDefined();
+    expect(image).toHaveProperty('src', imageUrl);
+  });
+});
