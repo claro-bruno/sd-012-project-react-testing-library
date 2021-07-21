8e234ac8e8566e9609898b8a2c8b7f1cf4cd0aa7
Isaac Cruz <isaac.clm1@gmail.com>
2021-07-21 16:14:51 -0300

    Requsito 2 - Testa page About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Testa page About', () => {
+  const { history } = renderWithRouter(<App />);
+  const aboutLink = screen.getByRole('link', { name: /About/i });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+  const pathAbout = history.location.pathname;
+  expect(pathAbout).toBe('/about');
+
+  const aboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i });
+  expect(aboutPokedex).toBeInTheDocument();
+
+  const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+  expect(firstParagraph).toBeInTheDocument();
+
+  const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+  expect(secondParagraph).toBeInTheDocument();
+
+  const pokedexImage = screen.getByRole('img');
+  expect(pokedexImage).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
