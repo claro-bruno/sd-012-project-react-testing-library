e03b858b557ebd223c90123239f15f98ba85dae7
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-07-21 15:04:54 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <NotFound />', () => {
+  it('Testa se o component possui um H2 com o texto "Page requested not found"',
+    () => {
+      renderWithRouter(<NotFound />);
+      const h2 = screen.getByRole('heading',
+        { name: /Page requested not found/i });
+
+      expect(h2).toBeInTheDocument();
+      expect(h2).toHaveTextContent('😭');
+    });
+
+  it('Testa se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getAllByRole('img')[1];
+
+    expect(img).toHaveProperty('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(img).toBeInTheDocument();
+  });
+});
