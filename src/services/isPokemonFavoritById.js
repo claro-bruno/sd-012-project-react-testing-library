ba5954b0eb59f08a14b30e47499566836a7c0836
Júlio Mota <juliomotacampos@gmail.com>
2021-07-21 18:41:15 -0300

    Adiciona requisito 05

---
+++ src/services/isPokemonFavoritById.js
@@ -0,0 +1,13 @@
+const isPokemonFavoriteById = {
+  4: false,
+  10: false,
+  23: false,
+  25: false,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+export default isPokemonFavoriteById;
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,75 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+import isPokemonFavoriteById from '../services/isPokemonFavoritById';
+
+describe('Test all `Pokédex` component', () => {
+  test('if contains a heading h2 with `Encountered Pokémons` text', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const heading = screen.getByRole('heading');
+    expect(heading.tagName).toBe('H2');
+    expect(heading).toHaveTextContent(/encountered pokémons/i);
+  });
+
+  test('if shows each pokemon after click in next button', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const nextButton = screen.getByTestId('next-pokemon');
+    pokemons.forEach((pokemon) => {
+      expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(nextButton);
+    });
+  });
+
+  test('if contains all types buttons for filter', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const pokemonTypes = ['All', ...new Set(pokemons.map(({ type }) => type))];
+    const buttons = screen.getAllByRole('button');
+
+    pokemonTypes.forEach((type, i) => {
+      expect(buttons[i].textContent).toBe(type);
+      fireEvent.click(buttons[i]);
+      expect(screen.getByText('All')).toBeInTheDocument();
+    });
+  });
+
+  test('if shows only a certain type of pokemon after clink in a filter button', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const pokemonTypes = [...new Set(pokemons.map(({ type }) => type))];
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+
+    pokemonTypes.forEach((pokemonType, i) => {
+      fireEvent.click(typeButtons[i]);
+      const filteredPokemons = pokemons.filter(({ type }) => type === pokemonType);
+
+      if (filteredPokemons.length !== 1) {
+        expect(filteredPokemons.every(({ type }) => type === pokemonType));
+      }
+    });
+  });
+});
