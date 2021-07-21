6fb401f5d6fce71f6002867039df7c1078b80151
lucasnahas <lucasahas@gmail.com>
2021-07-21 17:03:13 -0300

    Termina os testes do componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+// import App from '../App';
+
+test('Testa se, quando nao ha pokemons favoritos, o texto eh exibido', () => {
+  render(<FavoritePokemons />);
+  const parag = screen.getByText('No favorite pokemon found');
+  expect(parag).toBeDefined();
+});
