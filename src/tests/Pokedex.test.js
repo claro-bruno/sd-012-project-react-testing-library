ff50778fc576c7d1afc80bc69874c149af328945
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:19:33 -0300

    import inicial pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+import React from 'react';
+import { findByText, fireEvent, getByText } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
