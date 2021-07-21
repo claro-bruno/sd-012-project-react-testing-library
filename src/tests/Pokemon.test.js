d2329534e7af2b3e74d9278f7b9cad311793a981
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 18:46:40 -0300

    adicionando teste para verificar favIcon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -29,7 +29,7 @@ describe('Testes para o componente Pokemon', () => {
     expect(pokeImage).toHaveAttribute('src', image);
   });
 
-  it('Testa se o card possui um link para detalhes do pokemon', () => {
+  it('Testa se o card possui um link para detalhes do pokemon e ao clicar leva a pagina de detalhes', () => {
     const { history } = renderWithRouter(
       <Pokemon
         pokemon={ pokemons[0] }
@@ -40,5 +40,17 @@ describe('Testes para o componente Pokemon', () => {
     const pokeDetails = screen.getByRole('link', { name: /More details/i });
     userEvent.click(pokeDetails);
     expect(history.location.pathname).toBe(`/pokemons/${pokemons[0].id}`);
+  });
+
+  it('Testa se existe um icone de estrela nos pokemons favoritados', () => {
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        showDetailsLink
+        isFavorite
+      />,
+    );
+    const favIcon = screen.getByAltText(`${pokemons[0].name} is marked as favorite`);
+    expect(favIcon).toHaveAttribute('src', '/star-icon.svg');
   });
 });
