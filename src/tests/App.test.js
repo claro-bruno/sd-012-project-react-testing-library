05df020e208ad95220564cf08d2718c9dcad6564
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-21 17:57:58 -0300

    Requisito 1 :  testa links de navegaçao

---
+++ src/tests/App.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithrouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Testes em App.js', () => {
+  test('testa os links de nav do App', () => {
+    const { history } = RenderWithrouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoriteLink).toBeInTheDocument();
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/pagina-que-nao-existe');
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
