17d03fc30f3a485c739e871947b0446f4c99a4a7
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-21 18:33:51 -0300

    Inicio do requisito 5 - Testes da pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import data from '../data';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente Pokedex', () => {
+  it('Verifica se renderia o header com o texto correspondente', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('heading', { name: 'Encountered pokémons' }));
+  });
+
+  it('Verifica se o botão proximo pokemon funciona corretamente', () => {
+    renderWithRouter(<App />);
+    const button = screen.getByRole('button', { name: /Próximo pokémon/i });
+
+    const pokeInicial = screen.getByText('Pikachu');
+    expect(pokeInicial).toBeDefined();
+
+    data.forEach((item) => {
+      const pokemon = screen.getByText(item.name);
+      expect(pokemon).toBeDefined();
+      userEvent.click(button);
+    });
+
+    expect(pokeInicial).toBeDefined();
+  });
+
+  it('Ter apenas um pokémon sendo carregado na página', () => {
+    renderWithRouter(<App />);
+    const pokeId = screen.getAllByTestId('pokemon-name');
+    expect(pokeId).toHaveLength(1);
+  });
+});
