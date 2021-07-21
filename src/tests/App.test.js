b250888e01f1fad9cd8678d3b144a1daaf622544
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-21 14:40:18 -0300

    Iniciando o projeto. VAMO QUE VAMO :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/dom';
+import App from './App';
+
 test('', () => {});
