fce5b792c01439b675c582e8751a6c443621f7a5
matheustkaczyk <matheus111904@hotmail.com>
2021-07-21 15:35:14 -0300

    Correçao importaçao errada

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAllByAltText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
 describe('Testa o componente About.js', () => {
