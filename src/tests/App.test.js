748e7488a385bebd605c929061f419078c693b05
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-21 16:09:02 -0300

    Implementa testes para o App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,49 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
 describe('Testes do componente App', () => {
   test('Verifica se existe um conjunto de links de navegação fixo', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  test('Verifica se ao clicar no link Home a URL da página é "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+  });
+
+  test('Verifica se ao clicar no link About a URL da página é "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const url = history.location.pathname;
+    expect(url).toBe('/about');
+  });
+
+  test('Verifica se a URL é "/favorites" ao clicar em Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const url = history.location.pathname;
+    expect(url).toBe('/favorites');
+  });
+
+  test('Verifica se a página Not Found aparece quando tem uma URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/url/desconhecida');
+    const notFoundText = screen.getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
   });
 });
