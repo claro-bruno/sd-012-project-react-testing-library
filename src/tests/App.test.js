1481066426ed942c770662776a3e821418ecc97f
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-21 15:52:47 -0300

    adiciona requisito 1, testes no App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('App tests', () => {
+  it('Testa se ao clicar no link "Home" é redirecionado para a rota "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa se ao clicar no link "about" é redirecionado para a rota "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa se ao clicar no link "Favorite Pokémons"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa se ao digitar uma url desconhecida redireciona para "Not Found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-que-nao-existe');
+    expect(screen.getByText(/Page requested not found/)).toBeDefined();
+  });
+});
