55f8e0988765717538d1eabe8256ca5e1646053c
julio11lp <julio11lp@hotmail.com>
2021-07-21 18:10:20 -0300

    Criterio 1 aprovado no lint, tests e stryker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('Criterio 1', () => {
+  test('Testes do link `Home`', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    expect(home).toBeDefined();
+    userEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Testes do link `About`', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: 'About' });
+    expect(about).toBeDefined();
+    userEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testes do link `Favorite Pokémons`', () => {
+    const { history } = renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favorite).toBeDefined();
+    userEvent.click(favorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Testes do link `Not Found`', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/quenaoexiste');
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeDefined();
+  });
+});
