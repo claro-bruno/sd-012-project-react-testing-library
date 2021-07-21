4ddad9d0583b4da99c9430d682f4f48c1532f716
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-21 20:48:01 -0300

    refatora requisito_02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-import About from '../components/About';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
 
 describe('Testa About.js', () => {
-  beforeEach(() => render(<About />));
+  beforeEach(() => renderWithRouter(<App />));
 
   it('Existe um heading h2 com o texto About Pokédex', () => {
-    const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    const about = screen.getByRole('link', { name: /About/i });
+    userEvent.click(about);
+    const title = screen
+      .getByRole('heading', { name: /About Pokédex/i, level: 2 });
     expect(title).toBeDefined();
   });
 
   it('Existem dois parágrafos com texto sobre a Pokédex', () => {
-    const paragraphs = screen.getAllByText(/Pokémons/);
-    const size = 2;
-    expect(paragraphs.length).toBe(size);
+    const about = screen.getByRole('link', { name: /About/i });
+    userEvent.click(about);
+    const paragraphOne = 'This application simulates a Pokédex, '
+      + 'a digital encyclopedia containing all Pokémons';
+    const paragraphTwo = 'One can filter Pokémons by type, '
+      + 'and see more details for each one of them';
+    const paragraphOneTest = screen.getAllByText(paragraphOne);
+    const paragraphTwoTest = screen.getAllByText(paragraphTwo);
+    expect(paragraphOneTest).toBeDefined();
+    expect(paragraphTwoTest).toBeDefined();
   });
 
   it('Existe imagem de uma Pokédex', () => {
+    const about = screen.getByRole('link', { name: /About/i });
+    userEvent.click(about);
     const imageURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const imageAlt = 'Pokédex';
     const imageTestOne = screen.getByAltText(imageAlt);
