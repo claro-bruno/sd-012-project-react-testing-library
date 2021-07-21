72e1185861ee3d01423efb9decdf80319b4f7a01
José Carlos <kalarruda@hotmail.com>
2021-07-21 15:32:59 -0300

    requisito 4 NotFound test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa componente Not Found', () => {
+  it('Verifica se página contém heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const head = screen.getByRole('heading', { name: /Page requested not found/i });
+    // const { pathname } = history.location;
+    // history.push('/pagina-nao-encontrada');
+    // expect(pathname).toBe('/pagina-nao-encontrada');
+    expect(head).toBeDefined();
+  });
+
+  it('Verifica se a imagem do pikachu é mostrada', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img', { name: /Pikachu crying because/i });
+    expect(image).toBeDefined();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
