fa83c1816fc52e1782935c82dc68407b7a88de09
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-21 17:18:58 -0300

    Resolve requisito 5 - Teste no Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+
+describe('testa o componente pokedex', () => {
+  it('testa o conteúdo do heading', () => {
+    const headingText = screen.getByText('Encountered pokémons');
+    expect(headingText).toBeDefined();
+  });
+
+  it('Testa se o próximo pokémon é exibido ao clicar no botão', () => {
+    const button = screen.getByText(/Próximo pokémon/i);
+    userEvent.click(button);
+
+    const nextPokemon = screen.getByText('Charmander');
+    expect(nextPokemon).toBeInTheDocument();
+  });
+
+  it('Testa o botão de resetar os filtros', () => {
+    const filterButtonAll = screen.getByRole('button', { name: 'All' });
+    expect(filterButtonAll).toBeDefined();
+    userEvent.click(filterButtonAll);
+
+    const firstPokemon = screen.getByText('Pikachu');
+    expect(firstPokemon).toBeInTheDocument();
+  });
+
+  it('Testa os botões de filtrar por tipo', () => {
+    const poisonFilter = screen.getByRole('button', { name: 'Poison' });
+    userEvent.click(poisonFilter);
+    const poisonPokemon = screen.getByText('Ekans');
+    expect(poisonPokemon).toBeInTheDocument();
+  });
+});
