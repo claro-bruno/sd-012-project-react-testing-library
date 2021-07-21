9d07318c89835bafd82858ed6f2939002bf9ba7f
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 20:15:22 -0300

    Requirement 05-> Test if the title was rendered

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa todo Pokedex.js', () => {
+  it('renderiza titulo "Encountered pokémons"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    const heading = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
