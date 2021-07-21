01680c5bfd355325cb1403ea1ecd89b4e961a94f
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-07-21 18:22:16 -0300

    Testa o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('Testa se é exibida a msg No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
