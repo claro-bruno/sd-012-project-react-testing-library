5874e191b28ce57a94d197ecf76bdaae7a629815
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-21 15:34:22 -0300

    Requisito 1 - Testando renderizacao inicial e se ao clicar no link funciona corretamente

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,32 @@
-// import React from 'react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 describe('Testa o componente App', () => {
   it('Tem um header com os links', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
 
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('Testa se é redirecionado ao Home ao clicar no link home.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+
+    expect(history.location.pathname).toBe('/');
+    const header = screen.getByRole('heading', { name: 'Encountered pokémons' });
+    expect(header).toBeInTheDocument();
   });
 });
