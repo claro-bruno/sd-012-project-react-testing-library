229a963c5f5724b6e96b1bcb9792d0cfd571af28
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-21 15:36:19 -0300

    requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,26 @@
-// test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+
+describe(`Teste se o topo da aplicação contém um conjunto fixo
+de links de navegação`, () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const firstLink = screen.queryByRole('link', { name: /home/i });
+    expect(firstLink).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    const firstLink = screen.queryByRole('link', { name: /about/i });
+    expect(firstLink).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const firstLink = screen.queryByRole('link', { name: /favorite pokémons/i });
+    expect(firstLink).toBeInTheDocument();
+  });
+});
