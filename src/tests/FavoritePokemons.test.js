d50ab0e0b7e22d8cdc5f255f23b9c28c6b43f1ea
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-07-21 19:31:03 -0300

    Resolvendo requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { FavoritePokemons } from '../components';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('Testa o component FavoritePokemons.js', () => {
-  beforeEach(() => {
-    render(<FavoritePokemons />);
-  });
+  render(<FavoritePokemons />);
 
   test('Testa se é exibido a mensagem No favorite pokemon found', () => {
     const message = screen.getByText(/No favorite/i);
     expect(message).toBeInTheDocument();
+  });
+});
+
+describe('Testa se foi adicionados pokemons favoritos', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  test('Adiciona o Pikachu como favorito', () => {
+    fireEvent.click(screen.getByText(/More details/));
+    fireEvent.click(screen.getByText(/Pokémon favoritado?/));
+    fireEvent.click(screen.getByText(/Favorite Pokémon/));
+    const pikachu = screen.getByText(/Pikachu/);
+    expect(pikachu).toBeInTheDocument();
   });
 });
