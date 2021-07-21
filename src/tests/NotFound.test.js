e213c5e4de23166b23e86202c99afd6baa941957
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-21 18:04:36 -0300

    adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa NotFound', () => {
+  test('Testa mensagem de erro', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const notFound = screen.getAllByRole('heading')[1];
+    expect(notFound).toBeInTheDocument();
+    expect(notFound).toHaveTextContent('Page requested not found 😭');
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
