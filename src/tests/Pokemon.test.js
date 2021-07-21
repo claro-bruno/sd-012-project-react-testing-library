48712822e235e0ad6910c0fc588f07e5ef97033a
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-21 20:23:08 -0300

    Resolve requisito 6 - Componente Pokemon.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,50 +1,67 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 import Pokemon from '../components/Pokemon';
 import renderWithRouter from './renderWithRouter';
 
 describe('testa o componente Pokemon.js', () => {
-  it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
-    const mokePokemons = [
+  const mokePokemon = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
       {
-        id: 25,
-        name: 'Pikachu',
-        type: 'Electric',
-        averageWeight: {
-          value: '6.0',
-          measurementUnit: 'kg',
-        },
-        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-        foundAt: [
-          {
-            location: 'Kanto Viridian Forest',
-            map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-          },
-          {
-            location: 'Kanto Power Plant',
-            map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-          },
-        ],
-        summary: 'This intelligent ',
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
       },
-    ];
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity.',
+  };
 
-    // global.fetch = jest.fn().mockResolvedValue({
-    //   json: jest.fn().mockResolvedValue(mokePokemons),
-    // });
+  it('testa se é renderizado um card com as informações de determinado pokémon.', () => {
+    renderWithRouter(<App />);
+    const name = screen.getByText(mokePokemon.name);
+    expect(name).toBeInTheDocument();
 
-    renderWithRouter(<Pokemon pokemons={ mokePokemons } />);
+    const type = screen.getByTestId('pokemon-type');
+    expect(type.innerHTML).toEqual(mokePokemon.type);
 
-    const title = screen.getByRole('heading', { name: /Pikachu Details/i });
-    expect(title).toBeInTheDocument();
+    const { averageWeight: { value, measurementUnit } } = mokePokemon;
+    const average = screen.getByText(`Average weight: ${value} ${measurementUnit}`);
+    expect(average).toBeInTheDocument();
+
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', mokePokemon.image);
+    expect(img).toHaveAttribute('alt', `${mokePokemon.name} sprite`);
   });
 
-  // it('testa se a página contém uma imagem com link correto', () => {
-  //   renderWithRouter(<Pokedex />);
-  //   const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-  //   const img = screen.getAllByRole('img');
-  //   expect(img[1]).toBeInTheDocument();
-  //   expect(img[1]).toHaveAttribute('src', imgURL);
-  // });
+  it('testa se o card indicado na Pokédex contém um link para exibir detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: /More details/i });
+    expect(detailsLink).toBeInTheDocument();
+    userEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${mokePokemon.id}`);
+  });
+
+  it('testa se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    renderWithRouter(<Pokemon pokemon={ mokePokemon } isFavorite />);
+    const iconStarAlt = `${mokePokemon.name} is marked as favorite`;
+    const iconStar = screen.getAllByRole('img');
+    expect(iconStar[1]).toBeInTheDocument();
+    expect(iconStar[1]).toHaveAttribute('src', '/star-icon.svg');
+    expect(iconStar[1]).toHaveAttribute('alt', iconStarAlt);
+  });
 });
