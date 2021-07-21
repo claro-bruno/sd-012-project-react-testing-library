8ef99ea6452dcdeb7e938d81bd5faa215a1ecdc6
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-21 17:19:21 -0300

    cria testes para <FavoritePokemons /> - Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+const detailsAlakazam = '/pokemons/65';
+const detailsDragonair = '/pokemons/148';
+
+describe('Testa componente FavoritePokemons', () => {
+  it(`Verifica se é exibido na tela a mensagem No favorite pokemon found, se a pessoa
+  não tiver pokémons favoritos.`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  it('Verifica se todos os cards de pokemons favoritados são exibidos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(detailsAlakazam);
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    history.push(detailsDragonair);
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    history.push('/favorites');
+    expect(screen.getByText('Alakazam')).toBeInTheDocument();
+    expect(screen.getByText('Dragonair')).toBeInTheDocument();
+  });
+});
