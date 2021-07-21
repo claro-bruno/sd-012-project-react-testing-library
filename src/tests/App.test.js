b5cee4f690b74d6bd5e309daa17c7e36594d9ce2
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 14:57:47 -0300

    adicinando imports ao test do APP, render do app antes dos testes e pegando links pro primeiro teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('comecando o projeto', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('testes realizados no App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  it('Verifica se App possui conjunto fixo de links de nav', () => {
+    const linkHome = screen.getByRole('link', { name: 'Home'} );
+    const linkAbout = screen.getByRole('link', { name: 'About'} );
+    const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons'} );
+  })
+})
+
