f9e307b7b0b7e2d36a7be4751bffbb107941c036
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:12:39 -0300

    pokemon favorito adicionado teste

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,11 +5,20 @@ import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa os pokemóns favoritos', () => {
-  beforeEach(() => {
-    renderWithRouter(<FavoritePokemons />);
-  });
   it('Se a pessoa não tiver pokemóns, deverá mostrar uma mensagem', () => {
+    renderWithRouter(<FavoritePokemons />);
     const noPokesText = screen.getByText(/No favorite pokemon found/i);
     expect(noPokesText).toBeInTheDocument();
+  });
+  it('Quando favorita um pokémon, deverá mostrar o card na tela', () => {
+    renderWithRouter(<App />);
+    const pokeDetails = screen.getByText(/more details/i);
+    fireEvent.click(pokeDetails);
+    const markFavorite = screen.getByLabelText(/Pokémon favoritado?/);
+    fireEvent.click(markFavorite);
+    const favoritesLink = screen.getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoritesLink);
+    const chosenPokemon = screen.getByText(/pikachu/i);
+    expect(chosenPokemon).toBeDefined();
   });
 });
