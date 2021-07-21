21a4325105e3bb9df772559cc989e1dfa061896e
alestori <alestori7@gmail.com>
2021-07-21 15:57:02 -0300

    [11] - Requisito 1 :white_check_mark:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('shows the Pokédex when the route is `/`', () => {
-  render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Testando componente App', () => {
+  it('Renderiza um header com texto "Pokédex"', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/Pokédex/i)).toBeInTheDocument();
+  });
 
-  expect(screen.getByText('Encountered pokémons')).toBeInTheDocument();
+  it('Renderiza links com textos específicos', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent(/home/i);
+    expect(links[1]).toHaveTextContent(/about/i);
+    expect(links[2]).toHaveTextContent(/favorite pokémons/i);
+  });
+
+  it('Testa comportamento ao clicar no link de home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa comportamento ao clicar no link de about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/about/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa comportamento ao clicar no link de favorites', () => {
+    const { history } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/favorite pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa se renderiza NotFound caso utilize URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notfound');
+
+    expect(screen.getByText(/not found/i)).toBeInTheDocument();
+  });
 });
