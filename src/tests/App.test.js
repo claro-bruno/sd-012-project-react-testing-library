f6fdbb48a4ff778a2c11bdb420eb964181d904ed
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-21 15:38:19 -0300

    requisito 1 feito aprovado no lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
