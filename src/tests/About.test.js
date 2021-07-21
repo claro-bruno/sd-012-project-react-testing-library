3723eafefd528de143b79692627b4541d915d8e7
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-21 16:09:58 -0300

    adiciona requisito 2, testes no About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o component <About />', () => {
+  it('Testa se o componente possui heading h2 com texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const aboutPage = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutPage).toBeInTheDocument();
+  });
+
+  it('Test the image URL', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    const imageSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', imageSrc);
+  });
+});
+
