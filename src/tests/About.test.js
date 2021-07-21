11c98b2a4fc0fb9f211668a11bd41b2d696510d9
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 16:01:46 -0300

    Requirement 02-> Adding test to check if image is rendered

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -23,4 +23,13 @@ describe('Testa todo About.js', () => {
     const secondParagraph = screen.getByText(secondText);
     expect(secondParagraph).toBeInTheDocument();
   });
+
+  it('renderiza a imagem', () => {
+    renderWithRouter(<About />);
+
+    const IMG_URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage).toHaveAttribute('src', IMG_URL);
+  });
 });
