aa760f0b164523199c7f719610f0df3c6b04b204
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:58:55 -0300

    peso do pokemon no card teste

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,11 +7,24 @@ describe('Verifica se as informações dos pokemons estão corretas', () => {
   beforeEach(() => {
     renderWithRouter(<App />);
   });
+  const moreDetails = 'More details';
 
-  it('Verifica se o nome do pokémon está correto', () => {
-    const enteringPokeDetails = screen.getByText('More details');
+  it('Verifica se o tipo do pokémon está correto', () => {
+    const enteringPokeDetails = screen.getByText(moreDetails);
     fireEvent.click(enteringPokeDetails);
     const pokemonName = screen.getByText('Pikachu');
     expect(pokemonName).toBeDefined();
+  });
+  it('Verifica se o nome do pokémon está correto', () => {
+    const enteringPokeDetails = screen.getByText(moreDetails);
+    fireEvent.click(enteringPokeDetails);
+    const pokemonType = screen.getByText('Electric');
+    expect(pokemonType).toBeDefined();
+  });
+  it('Verifica se o nome do pokémon está correto', () => {
+    const enteringPokeDetails = screen.getByText(moreDetails);
+    fireEvent.click(enteringPokeDetails);
+    const pokemonWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(pokemonWeight).toBeDefined();
   });
 });
