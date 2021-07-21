a3a10dd35e36b05cf4dff9721e0ec9d5894be5f4
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-07-21 18:34:49 -0300

    FavoritePokemons pass on stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from './helper/renderWithRouter';
+
+describe('Verificando todos os teste de FavoritePokemons', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  afterEach(() => localStorage.clear());
+
+  it('Testa se "No favorite pokemon found" aparece quando não favorita pokemons', () => {
+    userEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(screen.getByText('No favorite pokemon found')).toBeDefined();
+  });
+
+  it('Testa se quando favoritado aparece pokemons', () => {
+    userEvent.click(screen.getByText('More details'));
+    userEvent.click(screen.getByRole('checkbox'));
+    userEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(screen.getByText(/pikachu/i)).toBeDefined();
+  });
+});
