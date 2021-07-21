65df17a1fa461c0772a6fdba0283c6d15b77991e
Isaac Cruz <isaac.clm1@gmail.com>
2021-07-21 16:55:46 -0300

    Requisito 4 - Testa page de NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Testa página NotFound', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pagina-que-nao-existe');
+
+  const noMatch = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(noMatch).toBeInTheDocument();
+
+  const Image = screen.getAllByRole('img');
+  expect(Image[1]).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
