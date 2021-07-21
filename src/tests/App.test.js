387a73ce371f3db66d737a17b38c262635d606bd
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-21 15:57:43 -0300

    (1) - Testa componente App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Testa links de navegação do componente App.js', () => {
+  const { history } = renderWithRouter(<App />);
+  const allLinks = screen.getAllByRole('link');
+  const expectedLinks = ['Home', 'About', 'Favorite Pokémons'];
+  const expectedURL = ['/', '/about', '/favorites'];
+
+  expectedLinks.forEach((link, index) => {
+    expect(link).toBe(allLinks[index].innerHTML);
+    userEvent.click(allLinks[index]);
+    const { pathname } = history.location;
+    expect(pathname).toBe(expectedURL[index]);
+  });
+
+  history.push('/paginaestranha');
+  expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+});
