c57ddf5cd89482897ec9e0c1eb9cc1776a9b3887
gabrielfndz <gabriel.omercador@hotmail.com>
2021-07-21 17:53:35 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,80 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'with electricity to make them tender enough to eat.',
+  },
+];
+
+test('Testa se contém um heading com Encountered Pokémons', () => {
+  renderWithRouter(<App />);
+  const pokeHeading = screen.getByText(/Encountered pokémons/);
+  expect(pokeHeading).toBeInTheDocument();
+});
+
+test('Se é exibido o próximo pokémon quando clica', () => {
+  renderWithRouter(<App />);
+  const firstPoke = screen.getByText(/Pikachu/);
+  expect(firstPoke).toBeInTheDocument();
+  const nextButton = screen.getByTestId(/next-pokemon/);
+  userEvent.click(nextButton);
+  const nextPoke = screen.getByText(/Charmander/);
+  expect(nextPoke).toBeInTheDocument();
+});
+
+test('Se é exibido apenas um pokemon por vez', async () => {
+  global.fetch = jest.fn()
+    .mockResolvedValue({ json: () => Promise.resolve(pokemons) });
+
+  renderWithRouter(<App />);
+  const pokeLen = screen.getAllByText('Pikachu');
+  expect(pokeLen.length).toBe(1);
+});
+
+test('Se existem os botões de filtro', () => {
+  renderWithRouter(<App />);
+  const buttonLen = screen.getAllByTestId(/pokemon-type-button/);
+  expect(buttonLen[0].innerHTML).toBe('Electric');
+  expect(buttonLen[1].innerHTML).toBe('Fire');
+  expect(buttonLen[2].innerHTML).toBe('Bug');
+  expect(buttonLen[3].innerHTML).toBe('Poison');
+  expect(buttonLen[4].innerHTML).toBe('Psychic');
+  expect(buttonLen[5].innerHTML).toBe('Normal');
+  expect(buttonLen[6].innerHTML).toBe('Dragon');
+
+  const allButton = screen.getByText('All');
+  expect(allButton).toBeInTheDocument();
+});
+
+test('Se contem um botao para resetar filtro', () => {
+  renderWithRouter(<App />);
+  const allButton = screen.getByText('All');
+  expect(allButton).toBeInTheDocument();
+  userEvent.click(allButton);
+  const initialPokemon = screen.getByText(/Pikachu/);
+  expect(initialPokemon).toBeInTheDocument();
+});
