6e54bc738827b22d1c7f64e9ed02eaea967b8b93
José Carlos <kalarruda@hotmail.com>
2021-07-21 15:06:36 -0300

    requisito 3 FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa componente FavoritePokemons', () => {
+  it('Testa se renderiza página de pokemons favoritos', () => {
+    const { history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/favorites');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa se é exibido mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
