9dcfb8a9f34610eec8e3d6eb65c6ab27b610a1a1
MGSiqueira <marcos.siqueira98@gmail.com>
2021-07-21 18:21:17 -0300

    De novo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import App from '../App';
 import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testando o componente Pokemon', () => {
