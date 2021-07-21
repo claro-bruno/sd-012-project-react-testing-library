cefebe0018bea3c8e747a91b8fb218f70f78a68e
gcesconeto <cesconeto@gmail.com>
2021-07-21 19:47:55 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByText(/Page requested not found/);
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem correta', () => {
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying/);
+    expect(img.src).toBe(imgURL);
+  });
+});
