161f794ffc9526b48e8017c49538f42b10c2ad8d
Armando <a.guerra58@hotmail.com>
2021-07-21 17:14:45 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('testa se e exibido na tela a  mensagem no favorite pokemon', () => {
+    render(<FavoritePokemons />);
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+  it('Testa se e exibido os cards favoritados', () => {
+    const pokemon = [{
+      id: 143,
+      name: 'Snorlax',
+      type: 'Normal',
+      averageWeight: {
+        value: '460.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Vermillion City',
+          map: 'https://cdn2.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
+        },
+      ],
+    }];
+    renderWithRouter(<FavoritePokemons pokemons={ pokemon } />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+  });
+});
