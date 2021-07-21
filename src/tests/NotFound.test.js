8a651df22f355323f868becd3a85f7019f48e18f
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-07-21 20:01:08 -0300

    Resolvendo requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound.js', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  test('Testa se contém um h2 com o texto Page requested not found', () => {
+    const noMatch = screen.getByRole('heading', { name: /Page requested not/ });
+    expect(noMatch).toBeInTheDocument();
+  });
+
+  test('Testa se contém uma imagem', () => {
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', { name: /Pikachu crying/ });
+    expect(img.src).toBe(url);
+  });
+});
