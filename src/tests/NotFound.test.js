65fa989b400e64e4aef82fab811be354ade37955
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-21 18:03:46 -0300

    Implementa os testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do componente NotFound', () => {
+  test('Verifica se a página contém um h2 com o texto "Page requested not found"', () => {
+    render(<NotFound />);
+    const notFoundMSG = screen.getByText(/Page requested not found/i);
+    const emoji = screen.getByText('😭');
+    expect(notFoundMSG).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém um gif', () => {
+    render(<NotFound />);
+    const imageLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toHaveProperty('src', imageLink);
+  });
+});
