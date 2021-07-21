b5cf57e8454d292475559972f17c285cf354608a
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-07-21 14:40:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o component <FavoritePokemons />', () => {
+  it(`Testa se o componente exibe a msg "No favorites pokemon found", 
+  se não houver favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const heading = screen.getByRole('heading', { name: /Favorite pokémons/i });
+    expect(heading).toBeInTheDocument();
+
+    const notFavs = screen.getByText(/no favorite pokemon found/i);
+    expect(notFavs).toBeInTheDocument();
+  });
+
+  it('Testa se os cards favoritos são exibidos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favInputCheckbox = screen.getByRole('checkbox');
+    userEvent.click(favInputCheckbox);
+    history.push('/pokemons/10');
+    userEvent.click(favInputCheckbox);
+    history.push('/favorites');
+
+    const pokeCard = screen.getAllByText(/average weight/i);
+    expect(pokeCard.length).toBe(2);
+
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
+  });
+
+  it('Testa se nenhum card de pokemon é exibido se não for favoritado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favInputCheckbox = screen.getByRole('checkbox');
+    userEvent.click(favInputCheckbox);
+    history.push('/favorites');
+
+    const pokeCard = screen.getAllByText(/average weight/i);
+    expect(pokeCard.length).toBe(1);
+
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
+  });
+});
