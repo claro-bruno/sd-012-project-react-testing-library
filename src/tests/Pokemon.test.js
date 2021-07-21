8442d6a9ec42c151184aa9960381d2857cb2f86d
Daniel <danielbped@gmail.com>
2021-07-21 18:07:13 -0300

    Resolve requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
@@ -19,6 +20,24 @@ describe('Testa o componente Pokemon', () => {
   it('Testa se o card possui o peso do Pokémon', () => {
     renderWithRouter(<App />);
     const weight = screen.getByTestId('pokemon-weight');
-    expect(weight).toBeInTheDocument();
+    expect(weight).toHaveTextContent('Average weight: 6.0 kg');
+  });
+
+  it('Testa se o card mostra a imagem do pokémon corretamente', () => {
+    renderWithRouter(<App />);
+    const image = screen.getByAltText('Pikachu sprite');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Testa o link para mais detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    const favoriteCheckbox = screen.getByRole('checkbox', { checked: false });
+    userEvent.click(favoriteCheckbox);
+    const favoriteStar = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favoriteStar.src).toBe('http://localhost/star-icon.svg');
   });
 });
