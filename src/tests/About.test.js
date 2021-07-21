998e9962915cabc1aef45501d0aa2eeff754d123
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-21 16:36:57 -0300

    requisito 2 feito e aprovado no lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+beforeEach(() => {
+  renderWithRouter(<About />);
+});
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/About Pokédex/i);
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const textOne = /this application simulates a pokédex/i;
+    const textTwo = /One can filter Pokémons by type/i;
+    const firstParagraph = screen.getByText(textOne);
+    const secondParagraph = screen.getByText(textTwo);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const imgUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', imgUrl);
+  });
+});
