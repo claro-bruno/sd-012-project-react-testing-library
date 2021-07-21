86ed9842fa535d82ae637a26b977cb8cb6d6427a
gcesconeto <cesconeto@gmail.com>
2021-07-21 18:41:43 -0300

    Requisito 1 corrigido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <App.js />', () => {
+  it('O primeiro link deve possuir o texto Home.', () => {
+    renderWithRouter(<App />);
+    const linkToHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkToHome).toBeInTheDocument();
+  });
+  it('O primeiro link deve possuir o texto About.', () => {
+    renderWithRouter(<App />);
+    const linkToAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkToAbout).toBeInTheDocument();
+  });
+  it('O primeiro link deve possuir o texto Favorite Pokémons.', () => {
+    renderWithRouter(<App />);
+    const linkToFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkToFavorite).toBeInTheDocument();
+  });
+  it('Testa redirecionamento para / ao clicar no link Home.', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkToHome = screen.getByRole('link', { name: /Home/i });
+    fireEvent.click(linkToHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('Testa redirecionamento para /about ao clicar no link Home.', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkToAbout = screen.getByRole('link', { name: /About/i });
+    fireEvent.click(linkToAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('Testa redirecionamento para /favorites ao clicar no link Home.', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkToFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    fireEvent.click(linkToFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
