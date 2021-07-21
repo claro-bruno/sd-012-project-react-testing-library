12761a4478251a60c1f4169aa402ad82fdb1aaff
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 18:20:15 -0300

    arrumando lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
-import Pokemon from '../components/Pokemon';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './renderWithRouter';
+// import Pokemon from '../components/Pokemon';
 
 describe('Testes para o componente Pokemon', () => {
 
