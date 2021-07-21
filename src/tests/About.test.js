36d2444312c47374fbf3d289d38fa31b2fa6497b
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-21 16:50:42 -0300

    adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa About', () => {
+  test('Testa se há informações sobre a Pokédex', async () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading');
+    expect(aboutPokedex).toHaveTextContent('About Pokédex');
+    const paragraphs = screen.getAllByText(/Pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+    const image = screen.getByAltText('Pokédex');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
