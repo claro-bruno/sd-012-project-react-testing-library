a8292d0be03ed374fc10a420d7eb339a3ab56681
raphael <raphael.ph2ll@email.com>
2021-07-21 19:27:03 -0300

    Resolve 03 - <FavoritePokemons />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se é exibido na tela a mensagem No favorite pokemon found, [...]', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const paragraph = screen.getByText(/No favorite pokemon found/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+});
