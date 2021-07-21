3f1091aa3a8608ad21565a4050153b2f3395aec1
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 15:30:50 -0300

    Requirement 01-> Testing if the links were rendered

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,4 +11,26 @@ describe('Testa todo o App.js', () => {
     expect(heading).toBeInTheDocument();
   });
 
+  it('renderiza o conjunto de link', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    const navLinks = screen.getAllByRole('link');
+
+    const linksLength = 4;
+    expect(navLinks.length).toBe(linksLength);
+
+    const homeLink = navLinks[0];
+    expect(homeLink).toBeInTheDocument();
+    expect(homeLink).toHaveTextContent(/Home/i);
+
+    const aboutLink = navLinks[1];
+    expect(aboutLink).toBeInTheDocument();
+    expect(aboutLink).toHaveTextContent(/About/i);
+
+    const favoritesLink = navLinks[2];
+    expect(favoritesLink).toBeInTheDocument();
+    expect(favoritesLink).toHaveTextContent(/Favorite Pokémons/i);
+  });
 });
