4ffda397d0ef70fb79add205438de8589073505d
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-21 19:28:01 -0300

    Requisito 5 - realizados testes para checkar heading e funcionalidade do botao proximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import data from '../data';
+
+describe('Testando o componente Pokedex', () => {
+  const favoriteFake = {
+    4: false,
+    10: false,
+    23: false,
+    25: false,
+    65: false,
+    78: false,
+    143: false,
+    148: false,
+    151: false,
+  };
+
+  beforeEach(() => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ data }
+        isPokemonFavoriteById={ favoriteFake }
+      />,
+    );
+  });
+
+  it(('Testando se a página contém o heading'), () => {
+    const encounteredTitle = screen.getByText('Encountered pokémons');
+    expect(encounteredTitle).toBeInTheDocument();
+  });
+
+  it(('Testando funcionalidade do "botão Próximo Pokémon"'), () => {
+    const nextPokemonButton = screen.getByText('Próximo pokémon');
+    expect(nextPokemonButton).toBeInTheDocument();
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    // Matcher -toHaveTextContent- visto no link shorturl.at/antT2;
+    data.forEach(({ name, type }) => {
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toHaveTextContent(type);
+      userEvent.click(nextPokemonButton);
+    });
+  });
+});
