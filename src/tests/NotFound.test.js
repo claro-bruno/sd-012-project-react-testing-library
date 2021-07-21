9105d1079c939c4097db33d81c97cfe0f391825e
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-21 16:12:29 -0300

    Requisito 4 - Testa o componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Teste do componente NotFound.js', () => {
+  const imgName = 'Pikachu crying because the page requested was not found';
+  const { history } = renderWithRouter(<App />);
+  history.push('/not-found');
+
+  const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(heading).toBeInTheDocument();
+
+  const img = screen.getByRole('img', { name: imgName });
+  expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
