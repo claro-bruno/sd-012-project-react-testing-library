475ff198b498b23ff03ffd999bdb5d3ca10deb88
Wellington <junior.wel02@exemplo.br>
2021-07-21 17:03:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound />', () => {
+  it('Testa se a página possui um heading ', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2 = screen.getByText('Page requested not found');
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
