8723457eeb4cbc693b043c7c429cb1c88dbac4f1
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-21 19:26:09 -0300

    testa componente about, se possui heading com texto, se possui dois p e se possui img especifica

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('testa se o About.js', () => {
+  it('contem heading com texto about pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutText = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  it('contem dois p com texto sobre a pokédex', () => {
+    renderWithRouter(<About />);
+    const numberOfP = screen.getAllByText(/Pokémons/);
+    expect(numberOfP).toHaveLength(2);
+  });
+  it('contem imagem específica de Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImg = screen.getByAltText('Pokédex');
+    expect(pokedexImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
+
+/* referencias
+- https://testing-library.com/docs/queries/bytext
+- https://testing-library.com/docs/queries/byalttext
+*/
