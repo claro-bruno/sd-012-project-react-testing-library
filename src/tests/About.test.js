dbd65aea507efd0384736f3ba1327142a173a7ab
cristianocsl <cslcristiano@gmail.com>
2021-07-21 17:23:04 -0300

    Requisito 2: testa que a pagina contem: a) as informações sobre a Pokédex; b) um heading h2 com o texto About Pokédex; c) dois parágrafos com texto sobre a Pokédex; d) imagem de uma Pokédex a partir de seu url

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste do componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+
+    expect(screen
+      .getByRole('heading', { name: 'About Pokédex' }))
+      .toBeInTheDocument();
+
+    // Feito com ajuda de Nuwanda turma 12
+    // expect(screen
+    //   .getByRole('img'))
+    //   .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
+    // Feito com ajuda de Daniel Batista - turma 12
+    const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByAltText('Pokédex');
+    expect(img.src).toBe(link);
+
+    expect(screen
+      .getByText(/This application/i));
+    expect(screen
+      .getByText(/One can filter/i));
+  });
+});
