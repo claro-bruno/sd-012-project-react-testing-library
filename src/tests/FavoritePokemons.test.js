1de3c6872fa66cb5638f977fc6698619b24d099e
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 16:06:56 -0300

    Requirement 03-> Adding test to check if the message "No favorite pokemon found" was rendered

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa todo FavoritePokemons.js', () => {
+  it('renderiza "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const noFavoriteFound = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoriteFound).toBeVisible();
+  });
+});
