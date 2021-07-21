584f983407a3bbda051c1a175e995c13fd92af5d
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-21 18:20:06 -0300

    Requisito 4 - Resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe(('Testando o componente NotFound'), () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it(('Testa se existe o título "About Pokedex"'), () => {
+    const title = screen.getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  it(('Testa se a imagem é renderizada'), () => {
+    const imagePikachu = screen.getByAltText('Pikachu crying because the '
+    + 'page requested was not found');
+    expect(imagePikachu).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(imagePikachu).toBeInTheDocument();
+  });
+});
