27eaa452ecf7e2183cc433e59f8dace64f0b21d4
Gabriel Takaki <gabitj@icloud.com>
2021-07-21 16:24:30 -0300

    Requisito 4. Testa a pagina NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem Crying-emoji', () => {
+    // AJUDA EM: https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+    renderWithRouter(<NotFound />);
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
