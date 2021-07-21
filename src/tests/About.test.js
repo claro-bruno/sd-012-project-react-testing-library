7eb23e8a87326d54657ae5bc6d147fe5fdc206a5
raphael <raphael.ph2ll@email.com>
2021-07-21 18:48:14 -0300

    Resolve 02 - <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const aboutPokedex = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const paragraph01 = /This application simulates a Pokédex/i;
+    expect(screen.getByText(paragraph01)).toBeInTheDocument();
+    const paragraph02 = /One can filter Pokémons by type/i;
+    expect(screen.getByText(paragraph02)).toBeInTheDocument();
+    // const paragraph01 = screen.getByRole('p', {name:/This application simulates a Pokédex/i }) Não existe
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
+// Link
+// https://trybecourse.slack.com/archives/C01T2C18DSM/p1626896066010100
+// https://trybecourse.slack.com/archives/C01T2C18DSM/p1626897108017100
