eef5cdd22a1a08a4b4843e4b956e4bba1d1dd914
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:06:45 -0300

    mensagem sem favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
+
+describe('Testa os pokemóns favoritos', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+  it('Se a pessoa não tiver pokemóns, deverá mostrar uma mensagem', () => {
+    const noPokesText = screen.getByText(/No favorite pokemon found/i);
+    expect(noPokesText).toBeInTheDocument();
+  });
+});
