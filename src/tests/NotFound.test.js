49f965ff59a6b610d2d826de01e251b5932cb318
Patrick Dack <patrickdack89@gmail.com>
2021-07-21 18:42:13 -0300

    Requisito 4 implementado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+beforeEach(() => {
+  render(<NotFound />);
+});
+
+const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+const ALT = 'Pikachu crying because the page requested was not found';
+
+describe('Testa o componente <NotFound />', () => {
+  it('testa headin h2', () => {
+    const title = screen.getByText(/Page requested not found/);
+    const img = screen.getByRole('img', { name: ALT });
+    expect(img).toHaveAttribute('src', URL);
+    expect(title).toBeInTheDocument();
+  });
+});
