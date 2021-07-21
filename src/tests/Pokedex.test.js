7b71c78390d4622be9034df3859c81308fb9958a
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-21 17:42:02 -0300

    Resolve requisito 5(ajustes no lint) - Teste no Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByTestId, getByText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
