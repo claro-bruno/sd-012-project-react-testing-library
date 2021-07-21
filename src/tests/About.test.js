f9f82a2b48c3397829d11367214f803040409b95
victordg00 <victord.goncalves@outlook.com>
2021-07-21 20:20:27 -0300

    req 1 e 2 feitos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('About tests', () => {
+  it('Test page subtitle', () => {
+    renderWithRouter(<About />);
+    const subtitle = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  it('Test the image URL', () => {
+    renderWithRouter(<About />);
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgElement = screen.getByRole('img');
+
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement.src).toBe(imgURL);
+  });
+
+  it('Page must contains some informations about Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstInfo = screen.getByText(/application simulates a pokédex/i);
+    const secondInfo = screen.getByText(/one can filter pokémons by type/i);
+
+    expect(firstInfo).toBeInTheDocument();
+    expect(secondInfo).toBeInTheDocument();
+  });
+});
