a8b97b742e19736c8ddeaec75d31a6f8c470d4de
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-21 17:21:06 -0300

    adiciona requisito 4, testes no NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <NotFound />', () => {
+  it('Teste se página contém um texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getAllByRole('img')[1];
+
+    expect(img).toHaveProperty('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(img).toBeInTheDocument();
+  });
+});
