9590b878afad43e6ad3e839a690fd71ee29e9d18
Daniel <danielbped@gmail.com>
2021-07-21 15:58:31 -0300

    Resolve requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se a página contem o título Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem do Pikachu Tristão', () => {
+    renderWithRouter(<NotFound />);
+    const altText = /Pikachu crying because the page requested was not found/i;
+    const image = screen.getByAltText(altText);
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
