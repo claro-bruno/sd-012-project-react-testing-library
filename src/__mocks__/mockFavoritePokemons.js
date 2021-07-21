3f68cb4738377fe8eb10e65743317a46821923be
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-21 16:17:04 -0300

    Requisito 3 - Implementa os testes no <FavoritePokemons />

---
+++ src/__mocks__/mockFavoritePokemons.js
@@ -0,0 +1,46 @@
+const mockFavoritePokemons = [
+  {
+    id: 151,
+    name: 'Mew',
+    type: 'Psychic',
+    averageWeight: {
+      value: '4.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Faraway Island',
+        map: 'https://cdn2.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
+      },
+    ],
+    summary: 'Apparently, it appears only to those people'
+      + ' who are pure of heart and have a strong desire to see it.',
+  },
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries'
+      + 'with electricity to make them tender enough to eat.',
+  },
+];
+
+export default mockFavoritePokemons;
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import mockFavoritePokemons from '../__mocks__/mockFavoritePokemons';
+
+describe('Teste do componente <About />', () => {
+  it(
+    'Aparece na tela a mensagem "No favorite pokemon found" se não há pokémons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+      const noFavPokeFound = screen.getByText(/no favorite pokemon found/i);
+
+      expect(noFavPokeFound).toBeInTheDocument();
+    },
+  );
+
+  it(
+    'Aparece na tela a mensagem "No favorite pokemon found" se não há pokémons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons pokemons={ mockFavoritePokemons } />);
+      mockFavoritePokemons.forEach(({ name }) => {
+        const pokemonName = screen.getByText(name);
+        expect(pokemonName).toBeInTheDocument();
+      });
+    },
+  );
+});
