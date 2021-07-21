4fa6d977b2affe042e27d6775798b0132d548b20
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-21 19:57:47 -0300

    conclui requisito_02 - Teste o componente About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+
+describe('Testa About.js', () => {
+  beforeEach(() => render(<About />));
+
+  it('Existe um heading h2 com o texto About Pokédex', () => {
+    const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(title).toBeDefined();
+  });
+
+  it('Existem dois parágrafos com texto sobre a Pokédex', () => {
+    const paragraphs = screen.getAllByText(/Pokémons/);
+    const size = 2;
+    expect(paragraphs.length).toBe(size);
+  });
+
+  it('Existe imagem de uma Pokédex', () => {
+    const imageURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imageAlt = 'Pokédex';
+    const imageTestOne = screen.getByAltText(imageAlt);
+    const imageTextTwo = screen.getByRole('img');
+    expect(imageTestOne).toBeDefined();
+    expect(imageTextTwo.src).toBe(imageURL);
+  });
+});
