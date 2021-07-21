93b07f670a28393f5cf4ea2ee446f20e8cdfcfdc
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-21 17:47:29 -0300

    Requisito 4 - Testando a pagina not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o component Not Found', () => {
+  it('Testa se a pagina contem um heading com a mensagem certa', () => {
+    renderWithRouter(<NotFound />);
+    const text = screen.getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(text).toBeDefined();
+  });
+  it('Testa se a imagem mostrada apresenta o link correto', () => {
+    renderWithRouter(<NotFound />);
+    const imgText = 'Pikachu crying because the page requested was not found';
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', { name: imgText });
+    expect(img).toBeDefined();
+    expect(img).toHaveAttribute('src', imgSrc);
+  });
+});
