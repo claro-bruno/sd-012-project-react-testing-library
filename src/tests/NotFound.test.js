41fc0be2e771d5627844276fb4f32d400c34ef19
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-21 17:23:28 -0300

    Resolve requisito 4 - Componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa o componente NotFound.js', () => {
+  it('testa se a tela exibe No favorite pokemon found, se nao tiver favoritos', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('testa se a página contém uma imagem com link correto', () => {
+    renderWithRouter(<NotFound />);
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toBeInTheDocument();
+    expect(img[1]).toHaveAttribute('src', imgURL);
+  });
+});
