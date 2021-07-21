e81994b43fb1dafbe1413e6159203d01bb874f48
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-07-21 18:57:37 -0300

    NotFound pass on stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+
+describe('Verificando paginá não existente', () => {
+  beforeEach(() => render(<NotFound />));
+
+  it('Testando se o titulo h2 mostra "Page requested not found 😭"', () => {
+    const h2 = screen.getByRole('heading');
+    expect(h2).toContainHTML('<h2>Page requested not found');
+    expect(h2).toContainHTML('😭');
+  });
+
+  it('Testando o link da imagem', () => {
+    const img = screen.getByRole('img', { name: /pikachu/i });
+    expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
