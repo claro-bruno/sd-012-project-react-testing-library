bdebb3358357d301718cebf0984c77de4a028788
cristianocsl <cslcristiano@gmail.com>
2021-07-21 18:37:31 -0300

    Testa se No favorite pokemon found aparece quando nao ha pokemons favoritos e se sao exibidos todos os cards de pokemons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste do componente FavoritePokémons', () => {
+  test(' Teste se No favorite pokemon found é encontrad se não há favorito', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se são exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkDetails = screen.getByRole('link', { name: /More details/i });
+    expect(linkDetails).toBeInTheDocument();
+    userEvent.click(linkDetails);
+    expect(history.location.pathname).toBe('/pokemons/25');
+
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+
+    const textFavorite = screen.getByText('Favorite pokémons');
+    expect(textFavorite).toBeInTheDocument();
+  });
+});
