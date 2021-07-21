5609b342c71fe03efe6a9dca899d57b4e232e62f
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-21 16:00:39 -0300

    Requirement 02-> Adding test to check if h2 is rendered

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa todo About.js', () => {
+  it('renderiza "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const headingAbout = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(headingAbout).toBeVisible();
+  });
+
+});
