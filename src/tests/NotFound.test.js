2c0623e20a4f87c3f7dbe8494beab8ff4a4ae517
Erick Silva <erick.silva6653@gmail.com>
2021-07-21 15:59:21 -0300

    requisito 4 - testa o NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4 - Teste o componente <NotFound.js /> ', () => {
+  it('', () => {
+    render(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying/i);
+    expect(screen.getByRole('heading')).toHaveTextContent('Page requested not found 😭');
+    expect(img).toBeInTheDocument();
+  });
+});
