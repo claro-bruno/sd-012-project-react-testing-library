cac94cf4a246b8ca7bf6a6160ab54bacb3ec6c9a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-21 15:18:34 -0300

    req 2. Teste o componente `<About.js />.`

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('testa componente About', () => {
+  it('testa renderização do about', () => {
+    render(<About />);
+    const heading = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(heading).toBeInTheDocument();
+
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex, /i);
+    expect(paragraph1).toBeInTheDocument();
+
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type, /i);
+    expect(paragraph2).toBeInTheDocument();
+
+    const img = screen.getByRole('img', { name: /Pokédex/i });
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
