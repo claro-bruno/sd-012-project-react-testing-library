3396c3939c1daed79852321823d90d4b04c1c72e
Willian Prado <willian_prado@hotmail.com>
2021-07-21 19:25:26 -0300

    Req 3 - Teste o componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente Favorite Pokemons', () => {
+  it('Shows "No favorite pokemon found" if favorite Pokemon list is empty', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
