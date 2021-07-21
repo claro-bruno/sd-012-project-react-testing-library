0e1b7aa452020af0231e9ba49d5688cc42c95c79
RyanLaiber <ryanlaiber@gmail.com>
2021-07-21 18:26:44 -0300

    adiciona teste ao componente NotFound, que e renderizado em path nao existente --- Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('requisito 4- Testa componente NotFound', () => {
+  it('path errado direciona para pagina notFound com um heading específico', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/fofis');
+
+    const headingName = 'Page requested not found Crying emoji';
+    const heading = screen.getByRole('heading', { name: headingName });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('pagina notFound contem imagem com src específico', () => {
+    const { history } = renderWithRouter(<App />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    history.push('/fofis');
+
+    const imgName = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByRole('img', { name: imgName });
+
+    expect(img.src).toBe(url);
+  });
+});
