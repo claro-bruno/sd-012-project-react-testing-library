3262b743cf66059aca3c58c07a4ce08e61972378
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 16:51:19 -0300

    imports + texto describe

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
 test('', () => {});
