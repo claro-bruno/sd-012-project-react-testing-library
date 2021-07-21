0fc251fdb238603cb1a35e4925b79cd5e2c2d2fc
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-21 17:08:03 -0300

    (3) - Testa componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa informações do componente FavoritePokemons.js', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Testa texto da página de favoritos quando não tem pokemons', () => {
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémons/i }));
+    const paragraph = screen.getByText('No favorite pokemon found');
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it('Testa se a página de favoritos exibe pokemóns favoritados', () => {
+    userEvent.click(screen.getByRole('link', { name: /More details/i }));
+    userEvent.click(screen.getByRole('checkbox'));
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémons/i }));
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
+});
