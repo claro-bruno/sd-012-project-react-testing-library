4e815d605b27f84cedc4a2abe2cdd3b20428393d
Janderson Soares <jandinsm@gmail.com>
2021-07-21 20:05:24 -0300

    Requisito 1, App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+// import BrowserRouter from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
+
+describe('Verifica App.test.js', () => {
+  test('Verifica se a links na pagina Inicial', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeDefined();
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeDefined();
+    const FavoritePokemon = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(FavoritePokemon).toBeDefined();
+  });
+});
