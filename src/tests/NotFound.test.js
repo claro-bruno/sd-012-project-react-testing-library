cf903c8d1e94dd4a3d0006c2da22c08bb23dfacb
raphael <raphael.ph2ll@email.com>
2021-07-21 19:48:20 -0300

    Resolve 04 - NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste do componente <NotFound.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      const pageRequest = screen.getByRole(
+        'heading', { level: 2, name: /Page requested not found/i },
+      );
+      expect(pageRequest).toBeInTheDocument();
+    });
+  it('Teste se página mostra a imagem', () => {
+    const altText = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByAltText(altText);
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    // Mais de uma img no documento
+  });
+});
