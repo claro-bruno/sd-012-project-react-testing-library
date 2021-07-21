9c8cd259036f2d8cee5a19e6a40a739a9656db19
alestori <alestori7@gmail.com>
2021-07-21 15:35:37 -0300

    [10] - Requisito 4 :white_check_mark:

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  it('Contém um heading h2 com texto específico', () => {
+    render(<NotFound />);
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+
+  it('Contém uma imagem com src path específico', () => {
+    render(<NotFound />);
+    expect(screen.getByAltText(/Pikachu/i).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
