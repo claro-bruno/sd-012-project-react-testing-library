95338d02a41115c891ec3b9615cd21c26cc0f297
gabrielfndz <gabriel.omercador@hotmail.com>
2021-07-21 17:15:50 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('Se é exibido na tela a mensagem no faovrite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const pageTextWithoutFav = screen.getByText('No favorite pokemon found');
+  expect(pageTextWithoutFav).toBeInTheDocument();
+});
+
+test('Se é exibida a tela com o pokémon favoritado', () => {
+  renderWithRouter(<App />);
+  const pokeType = screen.getByText('Normal');
+  userEvent.click(pokeType);
+  const details = screen.getByText('More details');
+  userEvent.click(details);
+  const checkFavorite = screen.getByRole('checkbox');
+  userEvent.click(checkFavorite);
+  const favoriteName = screen.getByText('Snorlax');
+  expect(favoriteName).toBeInTheDocument();
+});
