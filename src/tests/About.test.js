40b60252c589c0182a0857ccc14f18d06a68c962
Euller Braz <euller.braz99@gmail.com>
2021-07-21 17:24:44 -0300

    Requisito 3: Adicionado testes a FavoritePokemons

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  it('Testa a renderização sem pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(screen.getByText('No favorite pokemon found')).toBeDefined();
+  });
+
+  it('Testa a renderização sem pokemons favoritados', () => {
+    const pokemonTest = {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    };
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemonTest] } />);
+    expect(screen.getByRole('heading', { name: 'Favorite pokémons' }));
+  });
+});
