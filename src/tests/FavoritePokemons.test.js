fee0af824152db006c55f7dbb17b30cf87b95764
Gabriel Takaki <gabitj@icloud.com>
2021-07-21 15:52:39 -0300

    Requisito 3. Verifica se ha pokemons com favorito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa componente <FavoritePokemons.js />', () => {
+  it('No favorite pokemon found se não tiver nenhum favorito.', () => {
+    // Lógica retirada em: https://medium.com/@drake_beth/how-to-test-images-in-react-a70053b1634a
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
