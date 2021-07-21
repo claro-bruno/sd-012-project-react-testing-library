ba31a58e06903722fc5b1b360e29a3912c35c215
PGP27 <peg.gep27@gmail.com>
2021-07-21 19:08:58 -0300

    Correcao requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -12,5 +12,31 @@ describe('Testa o componente PokemonDetails.js', () => {
     userEvent.click(links[3]);
     const pikachu = screen.getByText('Pikachu');
     expect(pikachu).toBeInTheDocument();
+    const summary = screen.getByRole('heading', { name: /Summary/i });
+    expect(summary).toBeInTheDocument();
+    const string1 = 'This intelligent Pokémon roasts hard berries ';
+    const string2 = 'with electricity to make them tender enough to eat.';
+    const string = string1 + string2;
+    const p = screen.getByText(string);
+    expect(p).toBeInTheDocument();
+  });
+  it('Testa uma seção com os mapas contendo as localizações do pokémon', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[3]);
+    const location = screen.getByRole('heading', { name: /Game Locations of Pikachu/i });
+    expect(location).toBeInTheDocument();
+    const imgs = screen.getAllByRole('img');
+    expect(imgs[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(imgs[2]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(imgs[1]).toHaveAttribute('alt', 'Pikachu location');
+    expect(imgs[2]).toHaveAttribute('alt', 'Pikachu location');
+  });
+  it('Testa se é possível favoritar um pokémon através da página de detalhes.', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[3]);
+    const text = screen.getByText('Pokémon favoritado?');
+    expect(text).toBeInTheDocument();
   });
 });
