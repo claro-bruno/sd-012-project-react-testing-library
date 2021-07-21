62c58eac40c8be3f38706fb96af175ea6095aea9
Eric Kreis <erickreis793@gmail.com>
2021-07-21 14:23:11 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound tests', () => {
+  it('Screen must have a specific "h2"', () => {
+    renderWithRouter(<NotFound />);
+    const subtitle = screen.getByRole('heading', {
+      name: /page requested not found crying emoji/i,
+    });
+    expect(subtitle).toBeInTheDocument();
+    expect(subtitle).toHaveTextContent(/page requested not found/i);
+  });
+
+  it('Screen must have a image with a specific url', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen
+      .getByAltText(/pikachu crying because the page requested was not found/i);
+
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'); // 100% mutants;
+  });
+});
