b97ca82cb338fe322beeccb229518993f5051680
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:03:55 -0300

    import inicial favoritepokemon.test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,5 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
