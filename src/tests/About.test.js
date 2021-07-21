400b89c4e3370c584b64298c8d259f2875f66094
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-21 15:51:42 -0300

    Resolve requisito 1 - Componente App.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,1 @@
 test('', () => {});
-// iniciando o projeto
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa o componente App.js', () => {
+  it('testa link Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('testa link About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testa link Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoriteLink).toBeInTheDocument();
+    userEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('testa url desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/xablau');
+    const noMatch = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(noMatch).toBeInTheDocument();
+  });
+});
