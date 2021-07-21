527cf3591a6e5bed0270558281780703d8b95001
Caio Morato <morato_dev@protonmail.com>
2021-07-21 15:56:32 -0300

    verifica nome do pokemon no card

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica se as informações dos pokemons estão corretas', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Verifica se o nome do pokémon está correto', () => {
+    const enteringPokeDetails = screen.getByText('More details');
+    fireEvent.click(enteringPokeDetails);
+    const pokemonName = screen.getByText('Pikachu');
+    expect(pokemonName).toBeDefined();
+  });
+});
