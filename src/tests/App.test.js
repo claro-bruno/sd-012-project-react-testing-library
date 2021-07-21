1ba1f2883f22523f09a77ff8d2aac1b42732a34b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 15:06:31 -0300

    arrumando import de renderWithRouter e finalizando teste dos links de nav de app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('testes realizados no App', () => {
   beforeEach(() => {
-    render(<App />);
+    renderWithRouter(<App />);
   });
   it('Verifica se App possui conjunto fixo de links de nav', () => {
     const linkHome = screen.getByRole('link', { name: 'Home'} );
     const linkAbout = screen.getByRole('link', { name: 'About'} );
     const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons'} );
-  })
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorites).toBeInTheDocument();
+  });
 })
 
