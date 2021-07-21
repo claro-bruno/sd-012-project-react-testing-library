8906473b27207a9e8e0f8cf362370abc65c4085d
Willian Prado <willian_prado@hotmail.com>
2021-07-21 17:45:56 -0300

    Req 1- Testa o componente App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,43 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-describe('Testa os links do componente App', () => {
-  test('', () => {});
+describe('Testa o componente App', () => {
+  it('Encontra os links no topo da página', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+  it('Redireciona para "/" ao clicar em Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+    const url = history.location.pathname;
+    expect(url).toBe('/');
+  });
+  it('Redireciona para "/about" ao clicar em About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+    const url = history.location.pathname;
+    expect(url).toBe('/about');
+  });
+  it('Redireciona para "/favorite" ao clicar em Favorite Pokemons', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorite);
+    const url = history.location.pathname;
+    expect(url).toBe('/favorites');
+  });
+  it('Redireciona para NotFound ao passar URL inválida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pikachu-e-o-melhor');
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
 });
