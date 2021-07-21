2222b8672cfae87264ec8046aa17ef32e4035210
Tulio <tuliotrindade99@gmail.com>
2021-07-21 17:05:24 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('testa pag not found', () => {
+  it('texto pag not found', () => {
+    renderWithRouter(<NotFound />);
+    const texto = screen.getByText('Page requested not found');
+    expect(texto).toBeInTheDocument();
+  });
+  it('testa imagem', () => {
+    renderWithRouter(<NotFound />);
+    const alt = 'Pikachu crying because the page requested was not found';
+    const
+      imagem = screen.getByAltText(alt);
+    expect(imagem).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
