b5d1e528d8670e06fa66f5a1c2b9cd2af7c7c30c
Caio Morato <morato_dev@protonmail.com>
2021-07-21 13:44:45 -0300

    imports iniciais

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
