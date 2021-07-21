9518d2704048449a0e6aa61b78ead6fa586e6bf6
cristianocsl <cslcristiano@gmail.com>
2021-07-21 15:54:35 -0300

    Requisito 1: testes: a) links de navegaçao presentes e seus respectivos textos; b) redirecionamento de cada link ao clica-lo; c) redirecionamento para pagina Not Found em caso de URL desconhecida

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Testes em App', () => {
+  test('Teste se App contém um conjunto fixo de links de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/pagina-que-nao-existe');
+    const noMacth = screen.getByText('Page requested not found');
+    expect(noMacth).toBeDefined();
+  });
+});
