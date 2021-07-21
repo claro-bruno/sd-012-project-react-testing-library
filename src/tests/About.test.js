9ec4462853cc783e0a007cd40b1737a5ae0138fa
igor <igorsife80@gmail.com>
2021-07-21 18:32:37 -0300

    cria os teste de About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+import { screen } from '@testing-library/react';
+
+describe('testa o component "About" e seus elementos', () => {
+  test('verifica a existencia de <h2> about pokédex', ()=> {
+    render(<About/>);
+    const findH2 = screen.getByRole('heading', { level: 2});
+    expect(findH2).toHaveTextContent(/About Pokédex/i);
+  });
+
+  test('verifica a existencia  da  pokedex info ', () => {
+    render(<About/>);
+    const infoPokedex = screen.getByText(/This application simulates/i);
+    const infoPokedex2 = screen.getByText(/One can filter/i)
+    expect(infoPokedex).toBeInTheDocument();
+    expect(infoPokedex2).toBeInTheDocument();
+  });
+
+test('verifica a existencia da imagem o src desejado', () => {
+  render(<About/>);
+  const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    const img = screen.getByAltText(/Pokédex/i);
+    expect(img.src).toBe(link)
+  })
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter'
-import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('verefica a existencia dos links de navegação', () => {
   test('verifica que há os links "home"  na tela', () => {
-    const { getByText, history } = renderWithRouter(<App/>);
-    fireEvent.click(getByText(/Home/i));
+    const { history } = renderWithRouter(<App/>);
+    userEvent.click(screen.getByText(/Home/i));
     const pathname = history.location.pathname;
     expect(pathname).toBe('/');
-    const HomeAll = getByText(/Encountered pokémons/);
+    const HomeAll = screen.getByText(/Encountered pokémons/);
     expect(HomeAll).toBeInTheDocument();
   });
 
   test('verifica que há os links "About"  na tela', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/About/i));
     const pathname = history.location.pathname;
     expect(pathname).toBe('/about');
-    const aboutAll = getByText(/About Pokédex/);
+    const aboutAll = screen.getByText(/About Pokédex/);
     expect(aboutAll).toBeInTheDocument();
   });
   
   test('verifica que há os links "Favorite pokémons"  na tela', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/Favorite pokémons/i));
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/Favorite pokémons/i));
     const pathname = history.location.pathname;
     expect(pathname).toBe('/favorites');
-    const favoritetAll = getByText(/Favorite pokémons/);
+    const favoritetAll = screen.getByText(/Favorite pokémons/);
     expect(favoritetAll).toBeInTheDocument();
   });
   
   test('verifica a não existencia da pagina "Xablau" = true ', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     history.push('/Xablau');
-    const noMatch = getByText(/Page requested not found/i)
+    const noMatch = screen.getByText(/Page requested not found/i)
     expect(noMatch).toBeInTheDocument();
-  })
+  });
 });
