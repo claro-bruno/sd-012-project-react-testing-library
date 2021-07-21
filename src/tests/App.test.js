b934ddce3f5c0e9e2e14eeb7dc6c67dfc36fb71c
lucasnahas <lucasahas@gmail.com>
2021-07-21 16:23:08 -0300

    Teste do componente App completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Testa se o topo contém um conjunto fixo de links de navegação.', () => {
+  renderWithRouter(<App />);
+  const links = screen.getAllByRole('link');
+  expect(links[0]).toHaveTextContent('Home');
+  expect(links[1]).toHaveTextContent('About');
+  expect(links[2]).toHaveTextContent('Favorite Pokémons');
+});
