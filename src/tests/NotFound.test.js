98db795db82b4913acf113a7d9113cf8bd53ba0d
ArturAlvaro <artur.alvaro73@gmail.com>
2021-07-21 15:44:26 -0300

    [Requisito 4 - Teste NotFound.js]

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa componente NotFound', () => {
+  it('Testa se página contém heading h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const h2Text = screen.getByRole('heading', { level: 2 });
+    expect(h2Text).toBeInTheDocument();
+    expect(h2Text).toHaveTextContent('Page requested not found');
+  });
+
+  it('Testa se a página renderiza imagem de erro', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen
+      .getByAltText(/pikachu crying because the page requested was not found/i);
+
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
