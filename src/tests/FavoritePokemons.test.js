573693ad9599fde8df8641e8dd4018df42ef7a8b
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-21 17:04:04 -0300

    Resolve requisito 3 - Componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa o componente FavoritePokemons.js', () => {
+  it('testa se a tela exibe No favorite pokemon found, se nao tiver favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+
+  it('testa se é exibido todos os cards de pokémons favoritados', () => {
+    // testa se é exibido todos os cards de pokémons favoritados
+  });
+});
