d8e6e320d8f77c82ab30e8e651ea566236cd5fe2
Matheus Proença <mproenca.marketing@gmail.com>
2021-07-21 15:35:07 -0300

    Adiciona teste de links - req01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../help/renderWithRouter';
+
+describe('Teste de links de navegação', () => {
+  it('Teste do texto da Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const $linkHome = screen.getByText(/Home/i);
+    expect($linkHome).toBeInTheDocument('');
+
+    const $linkAbout = screen.getByText(/About/i);
+    expect($linkAbout).toBeInTheDocument('');
+
+    const $linkFavoritePokemons = screen.getByText(/Favorite Pokémons/i);
+    expect($linkFavoritePokemons).toBeInTheDocument('');
+
+    userEvent.click($linkHome);
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click($linkAbout);
+    expect(history.location.pathname).toBe('/about');
+
+    userEvent.click($linkFavoritePokemons);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/notIsAPage');
+    const notFoundTxt = screen.getByText(/Page requested not found/i);
+    expect(notFoundTxt).toBeInTheDocument();
+  });
+});
