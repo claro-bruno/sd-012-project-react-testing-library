71d32d0e7cb777688c59f6143b2bfc30a4eb49f7
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-21 18:54:20 -0300

    requisito 4 feito e aprovado no lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+beforeEach(() => {
+  renderWithRouter(<NotFound />);
+});
+describe('Teste o componente <NotFound.js />', () => {
+  it(`Teste se página contém um heading h2 com o
+  texto Page requested not found 😭`, () => {
+    const message = screen.getByRole('heading', { name: /page requested not found/i });
+    const emoji = screen.getByLabelText(/crying emoji/i);
+    expect(message).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.queryAllByRole('img');
+    expect(image[1]).toHaveAttribute('src', imgUrl);
+  });
+});
