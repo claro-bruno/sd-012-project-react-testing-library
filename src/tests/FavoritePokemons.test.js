fac28f9d681916d178f0a38442132079e08c4e28
Adriana Biberg <adribiberg@hotmail.com>
2021-07-21 19:13:09 -0300

    Cumpre requisito 3 - Testa o componente Favorite Pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWhitRouter from './renderWhithRouter';
+import App from '../App';
+
+describe('Testa o componente Favorite Pokemons', () => {
+  test('Testa se uma mensagem aparece quando não há Pokémons favoritos', () => {
+    renderWhitRouter(<App />);
+    const clickAtFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(clickAtFavorite);
+
+    const NotFound = /No favorite pokemon found/i;
+    const checkNotFavorite = screen.getByText(NotFound);
+    expect(checkNotFavorite).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWhitRouter(<App />);
+    const moreDetails = screen.getByText('More details');
+    userEvent.click(moreDetails);
+
+    const clickFavorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(clickFavorite);
+
+    const clickAtFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(clickAtFavorite);
+
+    const checkFavorite = screen.getByAltText(/is marked as favorite/i);
+    expect(checkFavorite).toBeInTheDocument();
+  });
+});
