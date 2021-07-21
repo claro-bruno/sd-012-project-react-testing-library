bd365ee6111b160f0e900b37da9a78a8a2143734
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-21 17:04:01 -0300

    testes para h2 e next button

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { getByRole, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
 
-test('', () => {});
+describe('Testes para componente Pokedex', () => {
+  beforeEach(() => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ { 25: true, 4: true, } }
+      />
+    );
+  })
+  it('Verifica se a pagina tem uma heading h2 com texto definido', () => {
+    const headingText = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(headingText).toBeInTheDocument();
+  });
+  it('Verifica se mostra o proximo pokemon da lista ao clicar em next', () => {
+    const nextButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+    const pikachu = screen.getByText(pokemons[0].name);
+    expect(pikachu).toBeInTheDocument();
+    userEvent.click(nextButton);
+    const charmander = screen.getByText(pokemons[1].name);
+    expect(charmander).toBeInTheDocument();
+  });
+});
