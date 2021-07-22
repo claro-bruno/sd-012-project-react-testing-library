a3bf4187de09856d352a1d1d21915d46a0cd5fe4
jmlinger <jmlinger10@gmail.com>
2021-07-22 18:53:43 -0300

    Resolve requisito 3: Verifica o conteúdo do componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Verifica o conteúdo do componente FavoritePokemons', () => {
+  test('Verifica a renderização de uma mensagem caso não haja favoritos', () => {
+    render(<FavoritePokemons />);
+    const mesageNotFound = screen.getByText('No favorite pokemon found');
+    expect(mesageNotFound).toBeInTheDocument();
+  });
+
+  test('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetailsLink);
+    const favoritePokemon = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(favoritePokemon);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const pokemonCardName1 = screen.getByText('Pikachu');
+    expect(pokemonCardName1).toBeInTheDocument();
+  });
+});
