20cf2a96613ddf052583be02ae5d8633ecd2b2f2
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-22 00:03:11 -0300

    cria testes para <Pokemon /> - Requisito 06

---
+++ src/__mocks__/data.js
@@ -0,0 +1,22 @@
+const pokemons = [
+  {
+    id: 65,
+    name: 'Alakazam',
+    type: 'Psychic',
+    averageWeight: {
+      value: '48.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Unova Accumula Town',
+        map: 'https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+      },
+    ],
+    summary: 'Closing both its eyes heightens all its other senses.',
+  },
+];
+
+export default pokemons;
