c0a2c6da86aba2be82dad8f8f5b82ebba0f6d549
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 06:36:41 -0300

    Faz teste que verifica se pokedex contém h1 com determinado texto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { Pokedex } from '../components';
+import App from '../App';
+
+describe('Testa Pokedex', () => {
+  test('Verifica se a página contém um h2', () => {
+    renderWithRouter(<App />);
+    const h1 = screen.getAllByRole('heading')[1];
+    const TEXT = /Encountered pokémons/i;
+    expect(h1).toHaveTextContent(TEXT);
+  });
+});
