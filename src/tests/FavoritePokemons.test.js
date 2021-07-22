83018d86bc00181b35f79c299b1cbc45bccd29d6
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-22 16:39:46 -0300

    Corrige requisito 3 - FavoritePokemons - Teste se é exibido todos os cards de pokémons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('testa o componente FavoritePokemons.js', () => {
@@ -10,30 +12,43 @@ describe('testa o componente FavoritePokemons.js', () => {
     expect(msg).toBeInTheDocument();
   });
 
-  // it('testa se é exibido todos os cards de pokémons favoritados', () => {
-  //   const mokePokemon = {
-  //     id: 25,
-  //     name: 'Pikachu',
-  //     type: 'Electric',
-  //     averageWeight: {
-  //       value: '6.0',
-  //       measurementUnit: 'kg',
-  //     },
-  //     image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-  //     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-  //     foundAt: [
-  //       {
-  //         location: 'Kanto Viridian Forest',
-  //         map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-  //       },
-  //       {
-  //         location: 'Kanto Power Plant',
-  //         map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-  //       },
-  //     ],
-  //     summary: 'This intelligent Pokémon roasts hard berries with electricity.',
-  //   };
-  //   renderWithRouter(<FavoritePokemons pokemons={ mokePokemon } />);
-  //   // testa se é exibido todos os cards de pokémons favoritados
-  // });
+  it('testa se é exibido todos os cards de pokémons favoritados', () => {
+    const mokePokemon = {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary: 'This intelligent Pokémon roasts hard berries with electricity.',
+    };
+
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${mokePokemon.id}`);
+
+    const label = screen.getByLabelText(/Pokémon favoritado?/i);
+    expect(label).toBeInTheDocument();
+    const favoriteFalse = screen.getByRole('checkbox', { checked: false });
+    expect(favoriteFalse).toBeInTheDocument();
+    userEvent.click(favoriteFalse);
+    const favoriteTrue = screen.getByRole('checkbox', { checked: true });
+    expect(favoriteTrue).toBeInTheDocument();
+
+    history.push('/favorites');
+    const pikachu = screen.getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
 });
