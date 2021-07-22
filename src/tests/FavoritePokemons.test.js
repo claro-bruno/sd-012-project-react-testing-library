da0c1e28ab825f460ea291e20aed90ea6aa19db0
Dayvid Ruan <ruan12bbg@gmail.com>
2021-07-22 15:50:01 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+beforeEach(() => {
+  renderWithRouter(<FavoritePokemons />);
+});
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const mensagem = screen.getByText(/No favorite pokemon found/i);
+
+    expect(mensagem).toBeInTheDocument();
+  });
+});
