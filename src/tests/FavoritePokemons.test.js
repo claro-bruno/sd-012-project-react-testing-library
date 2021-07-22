39c80503c915f22513461547aa6d54c91b945c1b
Milton Castro <castro.milton07@gmail.com>
2021-07-22 03:14:56 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('3 - Testa o componente <FavoritePokemons.js />', () => {
+  it('Verifica se é exibido na tela a mensagem No favorite pokemon found,'
+      + 'se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundParagraph = 'No favorite pokemon found';
+    expect(screen.getByText(notFoundParagraph)).toBeInTheDocument();
+  });
+
+  /* it('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+  }); */
+});
