5aea2abb9246abba9659053b6766bcb234bd1738
Samuel Constantino <samuelicapuidados@gmail.com>
2021-07-22 12:44:25 -0300

    refatora requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
-describe('Teste o componente <About.js />.', () => {
-  it('Testa informações sobre Pokédex', () => {
-    render(<About />);
+beforeEach(() => {
+  render(<About />);
+});
 
+describe('Testa informações sobre Pokédex em <About.js />.', () => {
+  it('Testa descrição da Pokédex em parágrafos', () => {
     const p1 = screen.getByText('This application simulates a Pokédex', { exact: false });
 
     const p2 = screen.getByText('One can filter Pokémons by type, and', { exact: false });
@@ -15,16 +17,14 @@ describe('Teste o componente <About.js />.', () => {
   });
 
   it('Testa h2 com texto About Pokédex ', () => {
-    render(<About />);
-
     const h2 = screen.getByRole('heading', { level: 2 });
     expect(h2).toHaveTextContent('About Pokédex');
   });
 
   it('Testa se págica contém imagem expecífica', () => {
-    render(<About />);
-
     const img = screen.getByAltText('Pokédex');
-    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    const srcImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(img.src).toBe(srcImg);
   });
 });
