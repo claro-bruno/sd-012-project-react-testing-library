dec383cc1e689171f3a365ca2d9e02f9eea0b1fa
Teika <shimworks.tk@gmail.com>
2021-07-22 17:08:27 -0300

    resolve requisito 07

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import Pokemon from '../components/Pokemon';
 
 describe('testa o componente Pokemon', () => {
+  const pokemon = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries'
+    + ' with electricity to make them tender enough to eat.',
+  };
   it('Teste se é renderizado um card com as informações de determinado pokémon', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite />);
     const pokeName = screen.getByTestId('pokemon-name');
     const pokeType = screen.getByTestId('pokemon-type');
     const pokeWeig = screen.getByTestId('pokemon-weight');
@@ -18,36 +40,13 @@ describe('testa o componente Pokemon', () => {
     expect(pokeImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
   it('confere link de detalhes', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite />);
     const link = screen.getByText('More details');
     userEvent.click(link);
     const { location: { pathname } } = history;
     expect(pathname).toBe('/pokemons/25');
   });
   it('testa favoritos', () => {
-    const pokemon = {
-      id: 25,
-      name: 'Pikachu',
-      type: 'Electric',
-      averageWeight: {
-        value: '6.0',
-        measurementUnit: 'kg',
-      },
-      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-      foundAt: [
-        {
-          location: 'Kanto Viridian Forest',
-          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-        },
-        {
-          location: 'Kanto Power Plant',
-          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-        },
-      ],
-      summary: 'This intelligent Pokémon roasts hard berries'
-      + ' with electricity to make them tender enough to eat.',
-    };
     renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite />);
     const star = screen.getByAltText('Pikachu is marked as favorite');
     expect(star).toBeInTheDocument();
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Testa Pokemon Details', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    const link = screen.getByText(/More Details/i);
+    userEvent.click(link);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  it('todos os elementos devem estar na tela', () => {
+    const pName = screen.getByText('Pikachu Details');
+    const sum2 = screen.getByRole('heading', { level: 2, name: 'Summary' });
+    const parag = screen.getByText(pokemons[0].summary);
+    expect(pName).toBeInTheDocument();
+    expect(sum2).toBeInTheDocument();
+    expect(sum2.textContent).toBe('Summary');
+    expect(parag).toBeInTheDocument();
+  });
+  it('verifica se tem os mapas da localização do pokemon', () => {
+    const headingLoc = screen
+      .getByRole('heading', { level: 2, name: 'Game Locations of Pikachu' });
+    expect(headingLoc).toBeInTheDocument();
+    const imgs = screen.getAllByAltText(/Pikachu location/i);
+    imgs.forEach((e) => {
+      expect(e).toBeInTheDocument();
+    });
+    expect(imgs[0].src).toBe(pokemons[0].foundAt[0].map);
+    expect(imgs[1].src).toBe(pokemons[0].foundAt[1].map);
+  });
+  it('verifica se há um checkbox', () => {
+    const label = screen.getByLabelText('Pokémon favoritado?');
+    expect(label).toBeInTheDocument();
+  });
+});
