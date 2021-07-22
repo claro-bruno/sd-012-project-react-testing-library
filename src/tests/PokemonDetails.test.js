0a42d8f27ebff3647b818228658e3129af4d2467
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-22 12:55:24 -0300

    Requirement 07-> Testing if the pokemon infos ara shown

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa todo PokemonDetails.js', () => {
+  it('renderiza as info do pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    let linkDetails = screen.getByText(/More details/i);
+    expect(linkDetails).toBeInTheDocument();
+    userEvent.click(linkDetails);
+
+    const pkmId = parseInt(history.location.pathname.split('/pokemons/')[1], 10);
+    const pokemon = pokemons.find((pkm) => pkm.id === pkmId);
+
+    const heading = screen.getByRole('heading', { name: `${pokemon.name} Details` });
+    expect(heading).toBeInTheDocument();
+
+    linkDetails = screen.queryByText(/More Details/i);
+    expect(linkDetails).not.toBeInTheDocument();
+
+    const summaryHeading = screen.getByRole('heading', { name: /Summary/i });
+    expect(summaryHeading).toBeInTheDocument();
+
+    const summaryText = screen.getByText(pokemon.summary);
+    expect(summaryText).toBeInTheDocument();
+  });
+});
