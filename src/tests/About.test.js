5cce8c37d2c4e192e6cb99c2e5bda64deae838e9
jmlinger <jmlinger10@gmail.com>
2021-07-22 14:08:30 -0300

    Tentativa 1 para resolver problema no linter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,17 @@
 // import React from 'react';
-// import { screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 // import About from '../components/About';
 
-// describe('', () => {
-//   test('', () => {});
+// describe('Verifica o conteúdo do componente About', () => {
+//   test('Verifica se a página contém as informações sobre a Pokédex', () => {
+//     render(<About />);
+//     const title = screen.getByRole('heading', { name: 'About Pokédex' });
+//     expect(title).toBeInTheDocument();
+
+//     const paragraph1 = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+//     expect(paragraph1).toBeInTheDocument();
+
+//     const paragraph2 = screen.getByText('One can filter Pokémons by type, and see more details for each one of them');
+//     expect(paragraph2).toBeInTheDocument();
+//   });
 // });
