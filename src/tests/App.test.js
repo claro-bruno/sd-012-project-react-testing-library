a5be205d3d7810cf83e55024094059350ce7d260
Alice Gonçalves <galice9992@gmail.com>
2021-07-22 18:17:38 -0300

    resolve requisito 1, testa App.js: links e rotas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-// test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('App test', () => {
+  test('Testa links de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Testa o evento de click dos links e suas respectivas rotas', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favoritePokemons = screen.getByText('Favorite Pokémons');
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+
+    fireEvent.click(favoritePokemons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
