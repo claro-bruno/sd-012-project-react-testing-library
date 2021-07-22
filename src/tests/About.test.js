d36466b847d187b872c91d07e77977d4417222cf
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-22 19:35:46 -0300

    requisito 2- testa infos sobre pokedex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,7 +5,7 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('testa componente About', () => {
-  test('testa info sobre Pokédex na página', () => {
+  test('teste se a página contém as informações sobre a Pokédex.', () => {
     const { history } = renderWithRouter(<App />);
     const aboutLink = screen.getByRole('link', { name: /about/i });
     expect(aboutLink).toBeInTheDocument();
@@ -15,8 +15,8 @@ describe('testa componente About', () => {
     expect(screen.getByRole('heading', { name: 'About Pokédex' }))
       .toBeInTheDocument();
 
-    const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-    const img = screen.getAllByText('Pokédex');
+    const link = "https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png";
+    const img = screen.getByAltText('Pokédex');
     expect(img.src).toBe(link);
 
     expect(screen.getByText(/This application/i));
