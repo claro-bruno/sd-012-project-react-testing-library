56204db6a8453e4207ccd67dfccdc62226bd260e
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-07-22 12:12:21 -0300

    Resolve requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('exibe mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+});
