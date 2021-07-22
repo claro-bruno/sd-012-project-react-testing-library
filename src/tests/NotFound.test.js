8cb82690290a40deab6c37715cd9db232b5c2354
David Azevedo <zevid.dev@outlook.com>
2021-07-22 00:56:49 -0300

    Create test for NotFound.js, requirement 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica NotFound', () => {
+  test('Testa renderização de h2 e img em notfound', () => {
+    renderWithRouter(<NotFound />);
+
+    const SRC_NOTFOUND = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(screen.getByText('Page requested not found')).toBeDefined();
+    expect(screen.getAllByRole('img')[1]).toHaveAttribute('src', SRC_NOTFOUND);
+  });
+});
