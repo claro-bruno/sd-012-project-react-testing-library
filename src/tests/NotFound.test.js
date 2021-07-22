ed464b546193fa38d4d2d2b367dfa1cd15b49657
Stefano <stefanooliveira1@gmail.com>
2021-07-22 20:19:13 -0300

    Req 4 | Fix

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
-import renderWithRouter from '../renderWithRouter';
 
 describe('Teste o componente <NotFound.js />', () => {
-  test('Teste se página contém um heading h2 com o texto "Page requested not found 😭"',
+  beforeEach(() => render(<NotFound />));
+
+  test('Teste se página contém um heading h2 com o texto "Page requested not found"',
     () => {
-      renderWithRouter(<NotFound />);
       const heading = screen.getByRole('heading', {
-        level: 2,
-        Name: 'Page requested not found Crying emoji',
+        name: /page requested not found crying emoji/i,
       });
 
       expect(heading).toBeInTheDocument();
     });
 
   test('Teste se página mostra a imagem', () => {
-    renderWithRouter(<NotFound />);
     const image = screen.getByRole('img', {
       name: 'Pikachu crying because the page requested was not found',
     });
