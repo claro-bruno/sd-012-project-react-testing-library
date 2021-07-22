eb7cd18b54bb61119b36103eb1590f26bc0c1253
rachidb5 <jordan.rachid@gmail.com>
2021-07-22 16:43:53 -0300

    testes about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />)
+    const title = screen.getByRole('heading', { level: 2 })
+    expect(title).toHaveTextContent('About Pokédex');
+
+    const paragraph = screen.getAllByText(/pokémon/i)
+    expect(paragraph).toHaveLength(2)
+
+    const image = screen.getByAltText("Pokédex");
+    expect(image.src).toContain(`https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`);
+  });
+});
