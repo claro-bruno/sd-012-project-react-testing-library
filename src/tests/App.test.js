1bf968de8c9de29eb8ac3585b7e7eae4c61380f0
Elias-Forte <eliasforte.tur@gmail.com>
2021-07-21 21:40:23 -0300

    requiriment 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,32 @@
-test('', () => {
-  
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderwithRouter';
+
+describe('Testing component App.js', () => {
+  it('verify link in Nav have a text "Home" and redirect to "/"', () => {
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    const { history } = renderWithRouter(<App />);
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('verify link in Nav have a text "About" and redirect to "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('verify link in Nav have a text "Favorite Pokemons" and redirect to "/favorites"',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+      expect(linkFavorite).toBeInTheDocument();
+      userEvent.click(linkFavorite);
+      expect(history.location.pathname).toBe('/favorites');
+    });
 });
