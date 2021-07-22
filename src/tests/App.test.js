21c00446d620000d1f06b42635e603f804ed4c37
julio11lp <julio11lp@hotmail.com>
2021-07-22 00:25:47 -0300

    Criterio 2 aprovado no lint, test e stryker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -21,6 +21,15 @@ describe('Criterio 1', () => {
     userEvent.click(about);
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
+    const title = screen.getAllByRole('heading', 'About Pokédex');
+    expect(title).toBeDefined();
+    const textFirstP = 'This application simulates a Pokédex';
+    const textSecondP = 'One can filter Pokémons by type';
+    expect(textFirstP).toBeDefined();
+    expect(textSecondP).toBeDefined();
+    const image = screen.getByRole('img');
+    const imgSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image).toHaveAttribute('src', imgSrc);
   });
 
   test('Testes do link `Favorite Pokémons`', () => {
