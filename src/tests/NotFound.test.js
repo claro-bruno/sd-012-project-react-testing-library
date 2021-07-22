60855b746f0a85f7c626ec252b8a9dcb17911fc1
Tiago Ornelas <tiagoornelas@pop-os.localdomain>
2021-07-22 18:48:08 -0300

    [R4] Corrige typo na descrição dos testes

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,13 +4,13 @@ import renderWithRouter from '../../renderWithRouter';
 import NotFound from '../components/NotFound';
 
 // prettier-ignore
-describe('Requisito 3', () => {
-  it('3.1 - Teste se página contém o texto Page requested not found', () => {
+describe('Requisito 4', () => {
+  it('4.1 - Teste se página contém o texto Page requested not found', () => {
     renderWithRouter(<NotFound />);
     const error404 = screen.getByText(/Page requested not found/i);
     expect(error404).toBeInTheDocument();
   });
-  it('3.2 - Teste se página mostra a imagem.', () => {
+  it('4.2 - Teste se página mostra a imagem.', () => {
     renderWithRouter(<NotFound />);
     const img = screen.getByAltText(/Pikachu crying because the page/i);
     expect(img).toHaveProperty(
