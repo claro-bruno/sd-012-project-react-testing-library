0e947599eb20c42b6525f4a616ce94059550f25f
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 05:26:55 -0300

    Faz o teste que verifica se não é encontrado nenhum pokémon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testa FavoritePokemons', () => {
+  test('Verifica se não é encontrado nenhum pokémon favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const TEXT = /No favorite pokemon found/i;
+
+    expect(screen.getByText(TEXT)).toBeDefined();
+  });
+});
