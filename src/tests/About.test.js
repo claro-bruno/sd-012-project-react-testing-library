da6b355c10af1993954143d7ec68007932bf2b32
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 05:19:37 -0300

    Faz teste que verifica texto do about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -8,5 +8,11 @@ describe('Testa About', () => {
     renderWithRouter(<About />);
 
     expect(screen.getByRole('heading', { name: /about pokédex/i })).toBeDefined();
+
+    const TEXT_A = /This application simulates a Pokédex,/i;
+    const TEXT_B = /One can filter Pokémons by type,/i;
+
+    expect(screen.getByText(TEXT_A)).toBeDefined();
+    expect(screen.getByText(TEXT_B)).toBeDefined();
   });
 });
