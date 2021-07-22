e0754f0180e49fafa6a50d0d523f16bf908f240a
David Azevedo <zevid.dev@outlook.com>
2021-07-21 23:55:48 -0300

    Create test for About.js, requirement 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Verifica About.js', () => {
+  test('Testa se a página tem informações sobre a pokedex, h2 e imagem', () => {
+    renderWithRouter(<About />);
+
+    const subtitle = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(subtitle).toBeDefined();
+
+    const PARAGRAPH_ONE = /This application simulates a Pokédex,/i;
+    const PARAGRAPH_TWO = /One can filter Pokémons by type,/i;
+
+    expect(screen.getByText(PARAGRAPH_ONE)).toBeInTheDocument();
+    expect(screen.getByText(PARAGRAPH_TWO)).toBeDefined();
+
+    const SRC_IMG = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(screen.getByRole('img')).toHaveAttribute('src', SRC_IMG);
+  });
+});
