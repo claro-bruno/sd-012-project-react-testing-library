6997a38e48e708f605fe0371fdd3280fe8e30384
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-22 19:08:24 -0300

    requisito 3 - testa componente FavoritePokemons-message no favorite found e lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import  renderWithRouter from '../services/renderWithRouter';
-import FavoritePokemons  from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
 
 beforeEach(() => {
-    renderWithRouter(<FavoritePokemons />);
+  renderWithRouter(<FavoritePokemons />);
 });
 
 describe('Teste componente FavoritePokemons', () => {
-    test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
     const message = screen.getByText(/no favorite pokemon found/i);
     expect(message).toBeInTheDocument();
-    });
+  });
 });
