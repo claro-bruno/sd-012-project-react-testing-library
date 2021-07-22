746fe1ff9aecc5c780ca04dc7e4435d03d18e07d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-21 21:18:21 -0300

    conclui requisito_03 - Teste o componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Testa FavoritePokemons', () => {
+  beforeEach(() => renderWithRouter(<App />));
+
+  it('Existe mensagem "No favorite pokemon found", em caso de não favoritados',
+    () => {
+      const favoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+      userEvent.click(favoritePokemons);
+      const paragraphOne = 'No favorite pokemon found';
+      const paragraphOneTest = screen.getByText(paragraphOne);
+      expect(paragraphOneTest).toBeDefined();
+    });
+});
