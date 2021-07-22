b145d0cdfb1342f2c6f6463325412b0c7b24888d
Álvaro Palma <palma.alvaro@outlook.com>
2021-07-22 17:59:00 -0300

    Implementa testes do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa componente NotFound', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/notfound');
+  });
+  it('Mensagem de página não encontrada', () => {
+    const message = screen.getByRole(
+      'heading',
+      { name: /Page requested not found Crying emoji/i },
+    );
+
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Imagem de página não encontrada', () => {
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
