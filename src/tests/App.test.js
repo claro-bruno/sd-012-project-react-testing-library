38ac81ac76ddc1fb6dd0a867e650fa6ddcfa5769
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 04:55:30 -0300

    Faz o teste de redirecionamento de página ao clicar em Home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -10,5 +11,13 @@ describe('Testa App', () => {
     expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /favorite pokémons/i })).toBeInTheDocument();
+  });
+
+  test('Verifica a página é redicionada ao inicio quando clicado em Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.click(screen.getByRole('link', { name: /home/i }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
 });
