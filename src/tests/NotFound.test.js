98b39c7db96be61995c24a1e1413cab440de598c
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-22 15:35:57 -0300

    Teste do Componente Not Found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const headingMsg = 'Page requested not found';
+    const notFoundMsg = screen.getByText(headingMsg);
+    expect(notFoundMsg).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+    const altMsg = 'Pikachu crying because the page requested was not found';
+    const imgScreen = screen.getByAltText(altMsg);
+    expect(imgScreen.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
