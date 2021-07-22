121ad7fe5b8d776716c692cab7e22dfed3696158
cristianocsl <cslcristiano@gmail.com>
2021-07-22 15:42:54 -0300

    Requisito 5: Testes nos botoes de filtragem - remove comentarios desnecessario

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -12,24 +12,8 @@ describe('Testes no componente <Pokedex.js />', () => {
     expect(heading).toBeInTheDocument();
     const button = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(button).toBeInTheDocument();
-
-    // userEvent.click(heading);
-    // expect(history.location.pathname).toBe('/');
-
-    // const linkAbout = screen.getByRole('link', { name: /about/i });
-    // expect(linkAbout).toBeInTheDocument();
-    // userEvent.click(linkAbout);
-    // expect(history.location.pathname).toBe('/about');
-
-    // const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
-    // expect(linkFavorite).toBeInTheDocument();
-    // userEvent.click(linkFavorite);
-    // expect(history.location.pathname).toBe('/favorites');
-
-    // history.push('/pagina-que-nao-existe');
-    // const noMacth = screen.getByText('Page requested not found');
-    // expect(noMacth).toBeDefined();
   });
+
   test('Cliques sucessivos mostram cada pokémon. O primeiro deve ser "pikachu"', () => {
     // Feito com a ajuda de Elias Forte - turma 12.
     data.map(({ name }) => {
@@ -38,10 +22,12 @@ describe('Testes no componente <Pokedex.js />', () => {
       expect(pokemonName).toBeInTheDocument();
       return userEvent.click(buttonNext);
     });
+
     expect(screen // após passar por todo os itens do array, o primeiro nome a ser mostrado deve ser pikachu.
       .getByText(/pikachu/i))
       .toBeInTheDocument();
   });
+
   test('Apenas um pokemon é mostrado por vez', () => {
     expect(screen
       .getAllByTestId(/pokemon-name/i))
@@ -68,6 +54,7 @@ describe('A Pokédex tem os botões de filtro.', () => {
       expect(typeButton).toHaveTextContent(type);
       userEvent.click(typeButton);
     });
+
     const buttonAll = screen.getByRole('button', { name: 'All' });
     expect(buttonAll).toBeInTheDocument();
     userEvent.click(buttonAll);
