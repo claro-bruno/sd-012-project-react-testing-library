65f25c4fc651d8b182af068834fbb88d17709631
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-07-21 22:30:09 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <FavoritePokemons/>', () => {
+  test('Testa se é exibida a msg "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noPokemons = screen.getByText('No favorite pokemon found');
+    expect(noPokemons).toBeInTheDocument();
+  });
+});
