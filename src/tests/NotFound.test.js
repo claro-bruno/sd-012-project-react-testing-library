a0610353db795b190b7b4b9544063dadaa863bc1
CamilaDamasio <camiladamasioef@hotmail.com>
2021-07-22 16:13:46 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica NotFound.js', () => {
+  test('Testa se página contém um heading', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/erro');
+
+    const notFoundPage = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+    expect(notFoundPage.localName).toBe('h2');
+
+    expect(notFoundPage).toBeInTheDocument();
+  });
+
+  test('Testa se página mostra uma imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/erro');
+
+    const imagem = screen.getAllByRole('img');
+    expect(imagem[1]).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
