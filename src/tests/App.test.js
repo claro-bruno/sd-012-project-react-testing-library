aa38cba234eb67b7f96bff69f7e8c1a3d01e97bf
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 04:50:32 -0300

    Faz testes de links de navegação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa App', () => {
+  test('Verifica se existe links de navegação', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /favorite pokémons/i })).toBeInTheDocument();
+  });
+});
