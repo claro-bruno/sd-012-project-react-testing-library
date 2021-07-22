5c0fa7b3ca8e21e771fe8c60ef2d6127f7209ec7
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 05:37:18 -0300

    Faz testes que verifica pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemons } from '../components';
+import App from '../App';
 
 describe('Testa FavoritePokemons', () => {
   test('Verifica se não é encontrado nenhum pokémon favorito', () => {
@@ -10,5 +12,21 @@ describe('Testa FavoritePokemons', () => {
     const TEXT = /No favorite pokemon found/i;
 
     expect(screen.getByText(TEXT)).toBeDefined();
+  });
+
+  test('Testa os pokémons favoritos', () => {
+    renderWithRouter(<App />);
+
+    userEvent.click(screen.getByRole('link', { name: /more details/i }));
+
+    expect(screen.getByText('Summary')).toBeDefined();
+
+    userEvent.click(screen.getByRole('checkbox'));
+    const ICON = 'favorite-icon';
+    expect(screen.getAllByRole('img')[1]).toHaveAttribute('class', ICON);
+
+    const pokemons = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(pokemons);
+    expect(screen.getAllByRole('img')[1]).toHaveAttribute('class', ICON);
   });
 });
