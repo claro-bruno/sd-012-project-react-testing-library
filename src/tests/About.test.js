fc4af51cc42b13ab91cff21aac76d5c5e5f6332b
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-07-22 20:01:46 -0300

    Requisito 2 - Testes do About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Teste o componente About.js', () => {
+  test('Testa o conteúdo da página', () => {
+    render(
+      <About />,
+    );
+    const aboutTitle = screen.getByRole('heading', { name: /About Pokédex/i });
+    const aboutParagraphs = screen.getAllByText(/pokémons/i);
+    const imageSource = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const aboutImage = screen.getByRole('img', { src: imageSource });
+    expect(aboutTitle).toBeInTheDocument();
+    expect(aboutParagraphs.length).toEqual(2);
+    expect(aboutImage).toHaveProperty('src', imageSource);
+  });
+});
