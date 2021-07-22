dd4660527022acf86b3617abb3fa51c259919192
Eurides <euridesalves@yahoo.com.br>
2021-07-22 17:06:31 -0300

    Adicionado requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente FavoritePokemons', () => {
+  test('Testando se nao houver Pokemon Favorito', () => {
+    render(<FavoritePokemons />);
+    const messageDisplayed = screen.getByText('No favorite pokemon found');
+    expect(messageDisplayed).toBeInTheDocument();
+  });
+  // test('Testando se é exibido exibido todos os cards de pokémons favoritados', () => {
+  //  });
+});
