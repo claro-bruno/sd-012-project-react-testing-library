3a84077b035c9bb043b192fc1edd06f510b51d8f
RenanMiyashita <renanmiyashita@gmail.com>
2021-07-22 09:54:00 -0300

    add "renderWithRouter" and R1(App.test.js)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,52 @@
-//requisito 1
-test('', () => {});
+// requisito 1
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test if App.js', () => {
+  test('Have the links: Home, About and Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  test('on click "home" redirects to "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('on click "about" redirects to About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('on click "favorites" redirects to "Favorites Pokémons"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('enters in a unknown URL redirects to "Not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/naoEncontrado');
+    const noMatch = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(noMatch).toBeInTheDocument();
+  });
+});
