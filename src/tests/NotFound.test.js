39cd3da501d6d157f8e330f183b1a47c57049920
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-07-22 14:16:57 -0300

    Requisito 4 testado e aprovado localmente

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Teste do componente NotFound', () => {
+  test('Testa se a pagina contém um h2 com o Page requested not found', () => {
+    const { history } = RenderWithRouter(<NotFound />);
+
+    history.push('/not-found');
+    const subtitleH2 = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(subtitleH2).toBeInTheDocument();
+  });
+  test('Testa se renderiza a imagem esperada', () => {
+    RenderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByAltText(/pikachu crying/i);
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
