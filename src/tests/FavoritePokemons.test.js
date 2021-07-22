59eabd46fc5062fe134bd8057a6c54cff0d216d0
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-07-22 18:40:45 -0300

    adiciona req 3 e 4 prontos com lint corrigido (ajuda de Le Bora e Paulinha)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('testa o componente favorite pokemon', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const nofave = screen.getByText(/No favorite pokemon found/i);
+    expect(nofave).toBeInTheDocument();
+  });
+  it('Testa se é exibido na tela os cards de pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favorite = screen.getByRole('checkbox');
+    userEvent.click(favorite);
+    history.push('/favorites');
+    const favepoke = screen.getAllByText(/weight/i);
+    expect(favepoke.length).toBe(1);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('testa pagina Not Found', () => {
+  it('testa se tem h2', () => {
+    renderWithRouter(<NotFound />);
+    const page = /Page requested not found/i;
+    const check = screen.getByRole('heading', { name: page });
+    expect(check).toBeInTheDocument();
+  });
+  it('testa se tem imagem', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/not found/i);
+    expect(image.src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
