b72c649cc089234ae588839b89378895da038c4d
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 06:21:05 -0300

    Faz teste que verifica se NotFound contém imagem do pikachu

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { NotFound } from '../components';
+import App from '../App';
 
 describe('Testa NotFound', () => {
   test('Verifica se a página contém um h2.', () => {
@@ -10,4 +11,11 @@ describe('Testa NotFound', () => {
     const TEXT = /Page requested not found/i;
     expect(h2).toHaveTextContent(TEXT);
   });
+
+  test('Verifica se a página contém uma imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/adsadasdsad');
+    const SRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(screen.getAllByRole('img')[1].src).toBe(SRC);
+  })
 });
