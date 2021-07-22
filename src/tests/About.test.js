757363596c4873e44662ebc1c6da04ac9996921d
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 05:16:44 -0300

    Faz teste que verifica título da página

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Testa About', () => {
+  test('Testa se about contém informações sobre a pokedex', () => {
+    renderWithRouter(<About />);
+
+    expect(screen.getByRole('heading', { name: /about pokédex/i })).toBeDefined();
+  });
+});
