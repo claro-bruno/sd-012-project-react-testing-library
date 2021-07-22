2d020bc379403c0c228db5fae2a3761587b17fba
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-21 23:58:43 -0300

    testa se componente notFound exibe msg e img especifica

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('testa se o notFound', () => {
+  it('exibe msg especifica', () => {
+    renderWithRouter(<NotFound />);
+    const NotFoundfMsg = screen.getByRole('heading',
+      { name: /Page requested not found Crying emoji/i });
+    expect(NotFoundfMsg).toBeInTheDocument();
+  });
+  it('exibe imagem especifica', () => {
+    renderWithRouter(<NotFound />);
+    const pikachuCryImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(pikachuCryImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
