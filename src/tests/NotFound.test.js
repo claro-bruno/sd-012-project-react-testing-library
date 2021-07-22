35b211700d3b1c68c8946e67aa6edd3c4ab25f60
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-22 06:05:13 -0300

    Faz teste que verifica se h2 contém determinado texto

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa NotFound', () => {
+  test('Verifica se a página contém um h2.', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading');
+    const TEXT = /Page requested not found/i;
+    expect(h2).toHaveTextContent(TEXT);
+  });
+});
