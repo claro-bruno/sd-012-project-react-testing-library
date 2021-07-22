3283a2bf1b8919386d1e786840d99772a7dc13b7
Stefano <stefanooliveira1@gmail.com>
2021-07-22 17:50:39 -0300

    Req 2 | Image test fixed to look for correct src

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -28,10 +28,9 @@ describe('Testa o componente <About.js />', () => {
 
   test('Teste se a página contém a imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
-    const imageURL = screen.getByRole('img', {
-      src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
-    });
+    const imageURL = screen.getByRole('img');
 
     expect(imageURL).toBeInTheDocument();
+    expect(imageURL).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png')
   });
 });
