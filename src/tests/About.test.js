7ab5f2193b0921c2ed112732ec218d45395ed9f5
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-07-22 15:39:57 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
+
+describe('Verifica se a página contém as informações sobre a Pokédex.', () => {
+  test('Verifica se a página contém um heading h2', () => {
+    renderWithRouter(<About />);
+
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém o primeiro paragrafo', () => {
+    const half1Text1 = 'One can filter Pokémons by type,';
+    const half2Text1 = 'and see more details for each one of them';
+    const textComplete = `${half1Text1} ${half2Text1}`;
+
+    renderWithRouter(<About />);
+
+    const firstParagraph = screen.getByText(textComplete);
+
+    expect(firstParagraph).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém o segundo paragrafo', () => {
+    const textComplete = (
+      'One can filter Pokémons by type, and see more details for each one of them'
+    );
+
+    renderWithRouter(<About />);
+
+    const secondParagraph = screen.getByText(textComplete);
+
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém uma imagem', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
