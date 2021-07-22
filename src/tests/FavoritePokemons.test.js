e7a21e080d1501b3e8f8c93a16abbd095de55328
julio11lp <julio11lp@hotmail.com>
2021-07-22 01:10:43 -0300

    Criterio 3 aprovado no lint, test e stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('Teste na pagina de pokemons favoritos', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  test('Caso nao haja pokemon favorito renderiza a msg No favorite pokemon found', () => {
+    const link = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(link);
+    const msg = screen.getByText('No favorite pokemon found');
+    expect(msg).toBeDefined();
+  });
+  test('Verifica se os pokemons favoritos sao renderizados', () => {
+    const details = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(details);
+    const input = screen.getByRole('checkbox');
+    userEvent.click(input);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const title = screen.getByText('Favorite pokémons');
+    expect(title).toBeDefined();
+  });
+});
