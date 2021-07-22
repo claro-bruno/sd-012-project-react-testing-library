ed564b33a047e6ec1225d21caa706582439f7a02
RenanMiyashita <renanmiyashita@gmail.com>
2021-07-22 14:50:27 -0300

    refatora R1 e add R2 (About.js)

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+// requisito 2
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('if About tests:', () => {
+  test('info about pokédex working', () => {
+    renderWithRouter(<About />);
+    const info1 = screen.getByText(/simulates a pokédex/i);
+    const info2 = screen.getByText(/filter pokémons by type/i);
+
+    expect(info1).toBeInTheDocument();
+    expect(info2).toBeInTheDocument();
+  });
+
+  test('"About" working', () => {
+    renderWithRouter(<About />);
+    const about = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(about).toBeInTheDocument();
+  });
+
+  test('image working', () => {
+    renderWithRouter(<About />);
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', imgURL);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,47 +6,26 @@ import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('test if App.js', () => {
-  test('Have the links: Home, About and Favorite Pokémons', () => {
-    renderWithRouter(<App />);
-
+  test('Have the links: Home, About and Favorite Pokémons working', () => {
+    const { history } = renderWithRouter(<App />);
     const linkHome = screen.getByRole('link', { name: /Home/i });
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    expect(history.location.pathname).toBe('/');
     expect(linkHome).toBeInTheDocument();
-
-    const linkAbout = screen.getByRole('link', { name: /About/i });
     expect(linkAbout).toBeInTheDocument();
-
-    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
     expect(linkFavorite).toBeInTheDocument();
-  });
 
-  test('on click "home" redirects to "Home"', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkHome = screen.getByRole('link', { name: /Home/i });
     userEvent.click(linkHome);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-  });
-
-  test('on click "about" redirects to About', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(history.location.pathname).toBe('/');
     userEvent.click(linkAbout);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-  });
-
-  test('on click "favorites" redirects to "Favorites Pokémons"', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(history.location.pathname).toBe('/about');
     userEvent.click(linkFavorite);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
-  });
+    expect(history.location.pathname).toBe('/favorites');
 
-  test('enters in a unknown URL redirects to "Not found"', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/naoEncontrado');
-    const noMatch = screen.getByRole('heading', { name: /page requested not found/i });
-    expect(noMatch).toBeInTheDocument();
+    history.push('/notIsAPage');
+    const notFound = screen.getByText(/Page requested not found/i); // getByRole não funcionou aqui :(
+    expect(notFound).toBeInTheDocument();
   });
 });
