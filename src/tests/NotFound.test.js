4aee58260355f5c8b347a0c6f9e5cd1fc59e9793
julio11lp <julio11lp@hotmail.com>
2021-07-22 01:47:50 -0300

    Criterio 4 aprovado no lint, test e stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('Testes relacionados a pagina Not Found', () => {
+  test('Verifica se o titulo e o esperado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/wrongurl');
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Page requested not found');
+    const image = screen.getAllByRole('img');
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image[1]).toHaveAttribute('src', imgSrc);
+  });
+});
