0b19506f1e189fa3c9cb5174c647b3f8f3c601c9
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-07-22 16:04:04 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Verifica NotFound.js', () => {
+  it('Verifica se página contém um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const headingNotFound = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+
+  it('Verifica se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imageAlt = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByAltText(imageAlt);
+    expect(image).toHaveAttribute('src', imageURL);
+  });
+});
