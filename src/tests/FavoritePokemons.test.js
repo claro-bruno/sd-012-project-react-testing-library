9c197605fafe62ad2c1e6eb56e0a9be90264b1ca
CamilaDamasio <camiladamasioef@hotmail.com>
2021-07-22 15:22:28 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica FavoritePokemons.js', () => {
+  test(
+    'Testa se é exibido na tela uma mensagem se a pessoa não tiver pokémons favoritos',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const favorite = screen.getByText(/favorite/i);
+
+      userEvent.click(favorite);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+      const msgFavorite = 'No favorite pokemon found';
+      expect(screen.getByText(msgFavorite)).toBeInTheDocument();
+    },
+  );
+
+  test(
+    'Testa se é exibido todos os cards de pokémons favoritados',
+    () => {
+      renderWithRouter(<App />);
+      const moreDetails = screen.getByText(/More details/i);
+      userEvent.click(moreDetails);
+
+      const favoritarPokemon = screen.getByRole('checkbox');
+      userEvent.click(favoritarPokemon);
+
+      const favorite = screen.getByText(/favorite/i);
+      userEvent.click(favorite);
+
+      const pokemonsFavoritados = screen.getAllByTestId('pokemon-name');
+      expect(pokemonsFavoritados.length).toBe(1);
+    },
+  );
+});
