4b182295d462d5606327e47ed976cfda55e5771a
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-07-22 17:05:39 -0300

    Reinstall node_modules

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from './helper/renderWithRouter';
+
+let history;
+
+describe('Verificar todo o component pokemon', () => {
+  beforeEach(() => {
+    ({ history } = renderWithRouter(<App />));
+  });
+
+  it('Teste a tela de PokemonDetails', () => {
+    userEvent.click(screen.getByText('More details'));
+    
+  });
+  
+
+});
