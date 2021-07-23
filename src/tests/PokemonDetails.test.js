d0af5de526386711b570eb47bf7b02d0077fe1e4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-22 21:30:33 -0300

    refatora PokemonDetails.test.js

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,74 +6,83 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Testa PokemonDetails.js', () => {
-  beforeEach(() => renderWithRouter(<App />));
+  it('Existe um H2 com nome "NomeDoPoKemon + Details"', () => {
+    const { history } = renderWithRouter(<App />);
 
-  it('Existe um H2 com nome "Pikachu Details"', () => {
-    const { name } = pokemons[0];
+    pokemons.forEach((pokemon) => {
+      const { name, id } = pokemon;
 
-    const linkDetails = screen.getByRole('link', { name: /More details/ });
-    expect(linkDetails).toBeDefined();
+      history.push(`/pokemons/${id}`);
 
-    userEvent.click(linkDetails);
+      const title = screen.getByRole('heading', { name: `${name} Details`, level: 2 });
+      expect(title).toBeDefined();
+    });
+  });
 
-    const title = screen.getByRole('heading', { name: `${name} Details` });
-    expect(title).toBeDefined();
+  it('Existe paragrafo com o resumo do Pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    pokemons.forEach((pokemon) => {
+      const { summary, id } = pokemon;
+
+      history.push(`/pokemons/${id}`);
+
+      const paragraphSummary = screen.getByText(summary);
+      expect(paragraphSummary).toBeDefined();
+    });
+  });
+
+  it('Existe um H2 com nome "Game Locations of + NomeDoPokemon"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    pokemons.forEach((pokemon) => {
+      const { name, id } = pokemon;
+
+      history.push(`/pokemons/${id}`);
+
+      const title = screen
+        .getByRole('heading', { name: `Game Locations of ${name}`, level: 2 });
+      expect(title).toBeDefined();
+    });
+  });
+
+  it('É renderizado todas as imagens com a localização do Pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    pokemons.forEach((pokemon) => {
+      const { name, foundAt, id } = pokemon;
+
+      history.push(`/pokemons/${id}`);
+
+      const localizationsAlt = screen.getAllByAltText(`${name} location`);
+      const sizeLocalization = foundAt.length;
+      expect(localizationsAlt.length).toBe(sizeLocalization);
+
+      foundAt.forEach((found, index) => {
+        const { map, location } = found;
+        expect(localizationsAlt[index].src).toBe(map);
+
+        const titleLocalization = screen.getAllByText(location);
+        expect(titleLocalization).toBeDefined();
+      });
+    });
   });
 
   it('Existe um H2 com nome "Summary"', () => {
+    renderWithRouter(<App />);
+
     const linkDetails = screen.getByRole('link', { name: /More details/ });
     expect(linkDetails).toBeDefined();
 
     userEvent.click(linkDetails);
 
-    const title = screen.getByRole('heading', { name: 'Summary' });
+    const title = screen.getByRole('heading', { name: 'Summary', level: 2 });
     expect(title).toBeDefined();
-  });
-
-  it('Existe paragrafo com o resumo do "Pikachu"', () => {
-    const { summary } = pokemons[0];
-
-    const linkDetails = screen.getByRole('link', { name: /More details/ });
-    expect(linkDetails).toBeDefined();
-
-    userEvent.click(linkDetails);
-
-    const paragraphSummary = screen.getByText(summary);
-    expect(paragraphSummary).toBeDefined();
-  });
-
-  it('Existe um H2 com nome "Game Locations of Pikachu"', () => {
-    const { name } = pokemons[0];
-    const linkDetails = screen.getByRole('link', { name: /More details/ });
-    expect(linkDetails).toBeDefined();
-
-    userEvent.click(linkDetails);
-
-    const title = screen.getByRole('heading', { name: `Game Locations of ${name}` });
-    expect(title).toBeDefined();
-  });
-
-  it('É renderizado todas as imagens com a localização do "Pikchu"', () => {
-    const { name, foundAt } = pokemons[0];
-    const linkDetails = screen.getByRole('link', { name: /More details/ });
-    expect(linkDetails).toBeDefined();
-
-    userEvent.click(linkDetails);
-
-    const localizationsAlt = screen.getAllByAltText(`${name} location`);
-    const sizeLocalization = foundAt.length;
-    expect(localizationsAlt.length).toBe(sizeLocalization);
-
-    for (let index = 0; index < foundAt.length; index += 1) {
-      const { map, location } = foundAt[index];
-      expect(localizationsAlt[index].src).toBe(map);
-
-      const titleLocalization = screen.getAllByText(location);
-      expect(titleLocalization).toBeDefined();
-    }
   });
 
   it('Existe checkbox "Pokémon favoritado?"', () => {
+    renderWithRouter(<App />);
+
     const linkDetails = screen.getByRole('link', { name: /More details/ });
     expect(linkDetails).toBeDefined();
 
