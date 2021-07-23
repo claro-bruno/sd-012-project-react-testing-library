9c5127c1c7b2e4541b10a463b6aa84c158ed21e4
Gabriel Viana <g.viana87@hotmail.com>
2021-07-23 00:14:09 -0300

    req 4 - Testa NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa NotFound', () => {
+  it('Testa se contém heading Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se contém img na página NotFound', () => {
+    renderWithRouter(<NotFound />);
+
+    const IMG_URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgComponent = screen.getAllByRole('img');
+
+    expect(imgComponent[1]).toBeInTheDocument();
+    expect(imgComponent[1]).toHaveAttribute('src', IMG_URL);
+  });
+});
