2c329642777d1b6a2fa57cb67a41467d6e432c63
Julio Pereira <jandrade.pereira1@gmail.com>
2021-07-22 22:32:59 -0300

    Requisito 1 - Inclue os testes do componente App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('1 - Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    it('1.1 - Verify funcionality of Home element', () => {
+      const { history } = renderWithRouter(<App />);
+      const homeElement = screen.getByText(/Home/i);
+      expect(homeElement).toBeInTheDocument();
+      fireEvent.click(homeElement);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+
+    it('1.2 - Verify funcionality of About element', () => {
+      const { history } = renderWithRouter(<App />);
+      const aboutElement = screen.getByText(/About/i);
+      expect(aboutElement).toBeInTheDocument();
+      fireEvent.click(aboutElement);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/about');
+    });
+
+    it('1.3 - Verify funcionality of Favorite Pokémons element', () => {
+      const { history } = renderWithRouter(<App />);
+      const favoritePokemonsElement = screen.getByText(/Favorite Pokémons/i);
+      expect(favoritePokemonsElement).toBeInTheDocument();
+      fireEvent.click(favoritePokemonsElement);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+
+    it('1.4 - Verify if Not Found component works when a URL is unknown', () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/notfound');
+      const notFoundPage = screen.getByText(/page requested not found/i);
+      expect(notFoundPage).toBeInTheDocument();
+    });
+  });
