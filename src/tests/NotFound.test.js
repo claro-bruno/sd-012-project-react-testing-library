9436629419f0731be0fe63ccdc763cbfa698f7fd
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-22 23:29:41 -0300

    testing NotFound.js component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testando o componente "NotFound"', () => {
+  it('Testando se a pagina contém um heading "h2" com um texto', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeDefined();
+  });
+
+  it('Testando se é exibida a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const img = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getAllByRole('img', { src: img });
+    expect((image)[1]).toHaveAttribute('src', img); // ? Encontrar forma de pegar a imagem diretamente;
+  });
+});
