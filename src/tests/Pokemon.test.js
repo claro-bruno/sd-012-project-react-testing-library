35ad0918bb075b81ee447005c9c71fbca0e41726
rachidb5 <jordan.rachid@gmail.com>
2021-07-23 20:33:47 -0300

    testa o componente pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import data from '../data';
+
+describe('Testa o componente <About.js />.', () => {
+  test('Testa se a imagem do Pokémon é exibida', () => {
+    renderWithRouter(<App />);
+    const image = screen.getByAltText(`${data[0].name} sprite`);
+    expect(image.src).toBe(data[0].image)
+  });
+
+});
