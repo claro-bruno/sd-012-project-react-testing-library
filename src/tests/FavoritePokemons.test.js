d05dd1569110a4d229f3b94563ba4c6ec3482be3
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-22 23:10:22 -0300

    testing FavoritePokemons.js component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testando o componente "FavoritePokemons"', () => {
+  it('Testa se exibido na tela a mensagem "No favorite pokemon find"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFavorites = screen.getByText(/No favorite pokemon found/i);
+    expect(notFavorites).toBeDefined();
+  });
+});
