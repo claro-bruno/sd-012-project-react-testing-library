718f1ce49fd0e8f0f4c6f8b1dbfbb02abbdcf863
Tiago Ornelas <tiagoornelas@pop-os.localdomain>
2021-07-22 21:47:44 -0300

    [R6] Resolve o requisito 6

---
+++ mocks/mockPokemon.js
@@ -0,0 +1,22 @@
+const mockPokemon = [
+  {
+    id: 143,
+    name: 'Snorlax',
+    type: 'Normal',
+    averageWeight: {
+      value: '460.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Vermillion City',
+        map: 'https://cdn2.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
+      },
+    ],
+    summary: 'What sounds like its cry may actually be its snores or the rumblings...',
+  },
+];
+
+export default mockPokemon;
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,53 +1,56 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../../renderWithRouter';
-import Pokemon from '../components/Pokemon';
+import App from '../App';
+import Pokedex from '../components/Pokedex';
+import mockPokemon from '../../mocks/mockPokemon';
+import mockFavArray from '../../mocks/mockFavArray';
+
+const { averageWeight, id, image, name, type } = mockPokemon[0];
 
 // prettier-ignore
 describe('Requisito 6', () => {
+  beforeEach(() => renderWithRouter(
+    <Pokedex
+      pokemons={ mockPokemon }
+      isPokemonFavoriteById={ mockFavArray }
+    />,
+  ));
   it('6.1 - Teste se é renderizado um card com as informações de pokémon.', () => {
-    renderWithRouter(<Pokemon />);
-    const error404 = screen.getByText(/Page requested not found/i);
-    expect(error404).toBeInTheDocument();
+    const info = screen.getByText(/Snorlax/i);
+    expect(info).toBeInTheDocument();
   });
   it('6.1.1 - O nome correto do Pokémon deve ser mostrado na tela.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const pokemonName = screen.getByText(name);
+    expect(pokemonName).toBeInTheDocument();
   });
   it('6.1.2 - O tipo correto do pokémon deve ser mostrado na tela.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent(type);
   });
   it('6.1.3 - O peso médio do pokémon deve ser exibido.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const pokemonWeigth = screen.getByText(
+      `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`,
+    );
+    expect(pokemonWeigth).toBeInTheDocument();
   });
   it('6.1.4 - A imagem do Pokémon deve ser exibida..', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const pokemonImage = screen.getByRole('img');
+    const altImage = screen.getByAltText(`${name} sprite`);
+    expect(pokemonImage).toHaveAttribute('src', image);
+    expect(altImage).toBeInTheDocument();
   });
   it('6.2 - Teste se o card do Pokémon indicado na Pokédex contém um link.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
-  });
-  it('6.3 - Teste se é feito o redirecionamento para detalhes de Pokémon.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
-  });
-  it('6.4 - Teste também se a URL exibida no navegador muda para /pokemon/<id>.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const detailsLink = screen.getByRole('link', { name: /details/i });
+    expect(detailsLink).toHaveAttribute('href', `/pokemons/${id}`);
   });
   it('6.5 - Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
-  });
-  it('6.5.1 - O ícone deve ser uma imagem.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
-  });
-  it('6.5.2 - A img deve ter o atributo alt, onde <pokemon> é o nome do Pokémon.', () => {
-    renderWithRouter(<Pokemon />);
-    expect(1).toBe(1);
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favoriteBtn = screen.getByLabelText('Pokémon favoritado?');
+    fireEvent.click(favoriteBtn);
+    expect(favoriteBtn).toBeChecked();
+    const favoriteImg = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favoriteImg).toHaveAttribute('src', '/star-icon.svg');
   });
 });
