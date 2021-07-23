34ddc94a16fb8da104b90fd1e938306670d8fd58
Josimar Schneider <josimarschneider@gmail.com>
2021-07-23 12:17:07 -0400

    Requisito 4. Teste o componente <NotFound.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -18,7 +18,7 @@ describe('2. Teste o componente <About.js />.', () => {
   });
   it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
     const srcImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-    const imgTest = screen.getByRole('img');
+    const imgTest = screen.getAllByRole('img');
     expect(imgTest).toHaveAttribute('src', srcImage);
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+  it('Se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const h2Text = screen.getByRole('heading', { level: 2 });
+    expect(h2Text).toHaveTextContent('Page requested not found 😭');
+
+    const erro404 = screen.getAllByText(/Page requested not found/i);
+    expect(erro404).toBeDefined();
+  });
+  it('Se página mostra a imagem', () => {
+    const srcImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgName = 'Pikachu crying because the page requested was not found';
+    const imgTest = screen.getByRole('img', { name: imgName });
+    expect(imgTest.src).toBe(srcImage);
+  });
+});
