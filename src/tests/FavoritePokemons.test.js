774d8823b9938cf8cb61663a68f8728f2fe8f4b1
Sass <dudu_sass12@hotmail.com>
2021-07-23 11:59:11 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../rota/renderWithRoute';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa Favorite Pokemons.js', () => {
+  it('Testa mensagem de Favorite', () => {
+    renderWithRouter(<FavoritePokemons/>);
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+})
