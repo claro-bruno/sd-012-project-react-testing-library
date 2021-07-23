652829fa94c5ebb8ec2c2f6649cec3c971025092
Eric Faria <ericdeofaria@gmail.com>
2021-07-23 11:26:21 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../etc/renderWithRouter';
+
+describe('FavoritePokemons.js', () => {
+  it('Not Found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+
+  it('Favoritos', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByText(/More details/i);
+    userEvent.click(details);
+    const checkBox = screen.getByRole('checkbox');
+    userEvent.click(checkBox);
+    const favorites = screen.getByText(/favorite/i);
+    userEvent.click(favorites);
+    const favoritesArray = screen.getAllByTestId('pokemon-name');
+    expect(favoritesArray.length).toBe(1);
+  });
+});
