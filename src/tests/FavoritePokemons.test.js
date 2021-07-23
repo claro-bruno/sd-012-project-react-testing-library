815f8e1dd73c8d7a04c2c1fb83661a8f48439b3b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-23 10:19:25 -0300

    Add third requirement - incomplete

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Component "Favorite Pokemons" test', () => {
+  beforeEach(() => renderWithRouter(<FavoritePokemons />));
+
+  it('Tests If doesn\'t has favorite pokemons, shows "No favorite pokemon found"', () => {
+    const noFavoritePokemons = screen.getByText(/No favorite pokemon found/);
+
+    expect(noFavoritePokemons).toBeInTheDocument();
+  });
+
+  it('Tests all favorite pokémons cards', () => {
+  });
+});
