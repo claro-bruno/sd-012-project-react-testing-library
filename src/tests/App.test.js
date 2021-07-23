31c8790233da899167ea0e6b39a521300c201b8a
Gabriel Viana <g.viana87@hotmail.com>
2021-07-22 21:40:51 -0300

    req 1 - testa App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes App.js', () => {
+  it('Testa Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: /Home/i });
+    expect(home).toBeInTheDocument();
+
+    userEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: /About/i });
+    expect(about).toBeInTheDocument();
+
+    userEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritePokemon = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritePokemon).toBeInTheDocument();
+
+    userEvent.click(favoritePokemon);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/erro');
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFound).toBeInTheDocument();
+  });
+});
