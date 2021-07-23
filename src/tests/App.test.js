1e1efe2743c9b247656acf54ee5c2b6a3afc4272
rachidb5 <jordan.rachid@gmail.com>
2021-07-22 23:08:58 -0300

    testa o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />.', () => {
+  test('testa a rota para páginas não encontradas', () => {
+    const {
+      history,
+    } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+
+  test('Testa a rota da home', () => {
+    const {
+      history,
+    } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/Home/i));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+
+  test('Testa a rota da about', () => {
+    const {
+      history,
+    } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/About/i));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa a rota da Favorite Pokemons', () => {
+    const {
+      history,
+    } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/Favorite pokémons/i));
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Favorite from '../components/FavoritePokemons';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<Favorite />);
+    const paragraph = screen.getByText('No favorite pokemon found');
+    expect(paragraph).toBeInTheDocument();
+  });
+});
