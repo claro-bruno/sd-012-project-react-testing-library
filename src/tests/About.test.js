bba06217fc826d89a073fdbd46e30423b998e30e
Josimar Schneider <josimarschneider@gmail.com>
2021-07-23 12:35:28 -0400

    corrigi o requisito 2-About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -18,7 +18,7 @@ describe('2. Teste o componente <About.js />.', () => {
   });
   it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
     const srcImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-    const imgTest = screen.getAllByRole('img');
+    const imgTest = screen.getByRole('img');
     expect(imgTest).toHaveAttribute('src', srcImage);
   });
 });
