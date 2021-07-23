42baf145ab95399f1c32b7f6724ce2dd5f0156d5
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-23 20:58:49 -0300

    Teste do Componente Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,9 +1,46 @@
-describe('', () => {
-  it('', () => {
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
+describe('Teste o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+    renderWithRouter(<App />);
+    screen.getByTestId('pokemon-name');
+    screen.getByTestId('pokemon-type');
+    screen.getByTestId('pokemon-weight');
+    const altText = `${pokemons[0].name} sprite`;
+    const image = screen.getByAltText(altText);
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
 
-  it('', () => {
+  it(`Teste se o card do Pokémon indicado na Pokédex contém um link de navegação
+   para exibir detalhes deste Pokémon. O link deve possuir a URL
+    /pokemons/<id>, onde <id> é o id do Pokémon exibido;`, () => {
+    renderWithRouter(<App />);
+    const linkDetails = screen.getByText(/more details/i);
+    expect(linkDetails).toBeInTheDocument();
+  });
 
+  it(`Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento
+   da aplicação para a página de detalhes de Pokémon.`, () => {
+    const { history } = renderWithRouter(<App />);
+    const linkDetails = screen.getByText(/more details/i);
+    fireEvent.click(linkDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${pokemons[0].id}`);
+  });
+
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /More details/i });
+    fireEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${pokemons[0].id}`);
+    fireEvent.click(screen.getByRole('checkbox', { name: /Pokémon favoritado\?/i }));
+    const altText = 'Pikachu is marked as favorite';
+    const image = screen.getByAltText(altText);
+    expect(image.src).toBe('http://localhost/star-icon.svg');
   });
 });
