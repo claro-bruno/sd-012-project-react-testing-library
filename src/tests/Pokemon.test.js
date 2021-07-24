5f6ed61e197c580fe3e370d86b8ccad199855474
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-07-24 17:33:11 -0300

    Requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,63 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import Pokemon from '../components/Pokemon';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testa componente Pokemon', () => {
+  const MOCK_POKEMON = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+  };
+
+  it('Testa pokemons', () => {
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ MOCK_POKEMON }
+        isFavorite
+      />,
+    );
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonName.textContent).toBe('Pikachu');
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonType.textContent).toBe('Electric');
+
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight).toBeInTheDocument();
+    expect(pokemonWeight.textContent).toBe('Average weight: 6.0 kg');
+
+    const link = screen.getByRole('link', { name: 'More details' });
+    expect(link).toBeInTheDocument();
+    fireEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    const pokemonImage = screen.getByRole('img', { name: /Pikachu Sprite/i });
+    expect(pokemonImage).toBeInTheDocument();
+    expect(pokemonImage).toHaveAttribute('src', MOCK_POKEMON.image);
+
+    const favIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favIcon).toBeInTheDocument();
+    expect(favIcon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
