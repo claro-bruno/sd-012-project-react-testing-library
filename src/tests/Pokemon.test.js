2992b2a7b7b29d780f14cbacec0b2f89f2be3431
rachidb5 <jordan.rachid@gmail.com>
2021-07-23 21:45:21 -0300

    testa o componente pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -8,7 +8,15 @@ describe('Testa o componente <About.js />.', () => {
   test('Testa se a imagem do Pokémon é exibida', () => {
     renderWithRouter(<App />);
     const image = screen.getByAltText(`${data[0].name} sprite`);
-    expect(image.src).toBe(data[0].image)
+    expect(image.src).toBe(data[0].image);
   });
-
+  test('Testa se a imagem do Pokémon é exibida', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByText('More details');
+    fireEvent.click(moreDetails);
+    const check = screen.getByRole('checkbox');
+    fireEvent.click(check);
+    const star = screen.getByAltText(`${data[0].name} is marked as favorite`);
+    expect(star.src).toContain('/star-icon.svg');
+  });
 });
