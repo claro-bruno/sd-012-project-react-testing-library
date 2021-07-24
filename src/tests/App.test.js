3536af086306599161688717cb2ee365005c4c0c
Ana Laura <analauraalves25@gmail.com>
2021-07-24 16:19:09 -0300

    testa componente App - primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa App.js', () => {
+  test('Testa se há textos Home, About e Favorite Pokémons na tela', () => {
+    renderWithRouter(<App />);
+
+    const homeText = screen.getByText(/home/i);
+    expect(homeText).toBeDefined();
+
+    const aboutText = screen.getByText(/about/i);
+    expect(aboutText).toBeDefined();
+
+    const favoritePokemonText = screen.getByText(/favorite pokémons/i);
+    expect(favoritePokemonText).toBeDefined();
+  });
+
+  test('Testa link Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Testa link About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByText(/about/i);
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa link Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritePokemonLink = screen.getByText(/favorite pokémons/i);
+    userEvent.click(favoritePokemonLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
