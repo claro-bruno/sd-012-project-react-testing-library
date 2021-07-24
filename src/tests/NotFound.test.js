903bf656416e7ad5a70e86db3853a2b1ef3325c3
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-07-24 12:57:19 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('2 - Testando o componente <About />', () => {
+  it('Teste se a aplicação contém um "heading".', () => {
+    renderWithRouter(<NotFound />);
+    const header = screen.getByRole('heading', { name: /Page requested not found /i });
+    expect(header).toBeInTheDocument();
+  });
+  it('Teste se a aplicação contém uma imagem.', () => {
+    renderWithRouter(<NotFound />);
+    const urlImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    // const image = screen.getByRole('img', { src: urlImg });
+    expect(screen.getAllByRole('img')[1]).toHaveAttribute('src', urlImg);
+  });
+});
