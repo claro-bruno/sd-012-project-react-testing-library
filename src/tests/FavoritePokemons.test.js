976f82162d9033786fc55131513d3cf8a41e59ed
Renata Nunes <renatanunes231@yahoo.com.br>
2021-07-24 03:19:29 -0300

    Teste para o componente FavoritePokemons.js implementado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  it('Testa se é exibida uma mensagem se não houver pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
