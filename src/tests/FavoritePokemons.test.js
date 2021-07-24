f8c7e83025867579bd7dd5c2a32be7f20fad7536
Heder Waine <hederw@hotmail.com>
2021-07-24 15:49:20 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Favorite from '../components/FavoritePokemons';
+
+describe('Teste do componente FavoritePokemons.js', () => {
+  test('Teste se é exibido uma mensagem, quando não tiver pokémons favoritos.', () => {
+    renderWithRouter(<Favorite />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<Favorite />);
+    const found = screen.getByText('Favorite pokémons');
+    expect(found).toBeInTheDocument();
+  });
+});
