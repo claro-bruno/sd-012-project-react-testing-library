d6b8252a05956202b2d6191a6ecf20ceb892e7f7
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-07-24 13:05:58 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('2 - Testando o componente <About />', () => {
+  it('Teste se a aplicação contém um texto.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNoFavorite = screen.getByText('No favorite pokemon found');
+    expect(textNoFavorite).toBeInTheDocument();
+  });
+});
