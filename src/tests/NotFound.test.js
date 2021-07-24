fa804080a59eba9a2e7f9168252a1a26c27ac439
Renata Nunes <renatanunes231@yahoo.com.br>
2021-07-24 03:42:33 -0300

    Teste para o componente NotFound.js implementado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+beforeEach(() => renderWithRouter(<NotFound />));
+
+describe('Testa o componente NotFound.js', () => {
+  it('Testa se página contém um heading h2 com uma determinada mensagem', () => {
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+
+  it('Testa se página mostra determinado gif', () => {
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getAllByRole('img');
+
+    expect(image[1]).toHaveAttribute('src', url);
+  });
+});
