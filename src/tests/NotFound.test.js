85c6487b5da88216903f00593eeea05446de7cfc
André Dantas <aluizcfd@gmail.com>
2021-07-25 15:45:38 -0300

    Adição de teste para verificar se consta gif Pikachu Chorando em componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -7,7 +7,17 @@ describe('Testando o componente NotFound.js', () => {
   test('Testa se página contem h2 com texto "Page requested not found 😭"', () => {
     renderWithRouter(<NotFound />);
     const h2NotFound = screen
-      .getByRole('heading', { name: 'Page requested not found 😭' });
+      .getByRole('heading', { name: 'Page requested not found Crying emoji' });
     expect(h2NotFound).toBeDefined();
+  });
+
+  test('Testa se página mostra imagem do Pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+    const gifPath = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgNotFound = screen.getByAltText('Pikachu crying because'
+    + ' the page requested was not found');
+    expect(imgNotFound).toBeDefined();
+    const { src } = imgNotFound;
+    expect(src).toBe(gifPath);
   });
 });
