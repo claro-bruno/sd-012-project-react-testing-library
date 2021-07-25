b8994b08fe6585cf390796ef8ddee42b88b65c30
André Dantas <aluizcfd@gmail.com>
2021-07-25 15:11:07 -0300

    Adição de testes para verificar se componente About possui dois parágrafos e imagem referente a Pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,4 +10,24 @@ describe('Testando o componente About.js', () => {
     const h2About = screen.getByRole('heading', { name: 'About Pokédex' });
     expect(h2About).toBeDefined();
   });
+
+  test('Testa se página contém dois parágrafos com texto sobre Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstParagraph = screen.getByText('This application simulates a Pokédex,'
+      + ' a digital encyclopedia containing all Pokémons');
+    const secondParagraph = screen.getByText('One can filter Pokémons by type,'
+    + ' and see more details for each one of them');
+
+    expect(firstParagraph).toBeDefined();
+    expect(secondParagraph).toBeDefined();
+  });
+
+  test('Testa se página contém imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const imgPath = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgAbout = screen.getByRole('img');
+    expect(imgAbout).toBeDefined();
+    const { src } = imgAbout;
+    expect(src).toBe(imgPath);
+  });
 });
