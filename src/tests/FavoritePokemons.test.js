bae4ed76f8d6623b9f2c38e4ff43b6071910e779
Bruno Augusto <brunaugusto@gmail.com>
2021-07-25 03:25:48 -0300

    Quarto  Requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+// import event from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import event from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Verifica se o componente NotFound é renderizado com H2 e IMG', () => {
+    renderWithRouter(<NotFound />);
+    const head2 = 'Page requested not found 😭';
+    const header = screen.getByRole('heading', { level: 2 });
+    expect(header).toHaveTextContent(head2);
+
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(screen.getAllByRole('img')[1]).toHaveClass('not-found-image');
+    expect(screen.getAllByRole('img')[1]).toHaveProperty('src', imgUrl);
+  });
+});
