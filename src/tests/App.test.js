433ecc398b4c08ed2051892b53fe27a03e5f8274
André Dantas <aluizcfd@gmail.com>
2021-07-25 14:14:30 -0300

    Adição de renderWithRouter no component App.js, implementação do teste de redirecionamento do link Home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-
-beforeEach(() => {
-  render(
-    <Router>
-      <App />
-    </Router>,
-  );
-});
+import renderWithRouter from './renderWithRouter';
 
 describe('Testando o componente App.js', () => {
   test('Testa o texto contido no conjunto de links no topo da página', () => {
+    renderWithRouter(<App />);
     const links = screen.getAllByRole('link');
     expect(links[0]).toHaveTextContent('Home');
     expect(links[1]).toHaveTextContent('About');
     expect(links[2]).toHaveTextContent('Favorite Pokémon');
+  });
+
+  test('Testa se ao clickar no link "Home" aplicação vai para página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const homeNav = screen.getByRole('link', { name: 'Home' });
+
+    userEvent.click(homeNav);
+    expect(pathname).toBe('/');
   });
 });
