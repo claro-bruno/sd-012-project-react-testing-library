2e3b481e79d853538a17d9b5412b3df1bd8dccec
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-07-25 11:57:40 -0300

    R4| Testa o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('[ 4 ] Testa o componente NotFound.js', () => {
+  test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const message = screen.getByRole('heading', { name: /Page requested not found/i });
+    const cryingEmoji = screen.getByRole('img', { name: /Crying emoji/i });
+    expect(message).toBeInTheDocument();
+    expect(cryingEmoji).toBeInTheDocument();
+  });
+  test('Testa se página mostra uma imagem específica', () => {
+    renderWithRouter(<NotFound />);
+    const message = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(message);
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img.src).toEqual(imgURL);
+  });
+});
