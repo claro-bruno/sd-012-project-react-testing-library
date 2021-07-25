080b8bcaddeae93e8d5e8458790272a79ea09065
André Dantas <aluizcfd@gmail.com>
2021-07-25 15:38:58 -0300

    Adição de teste para verificar se consta elemento h2 no componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Testa se página contem h2 com texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+    const h2NotFound = screen
+      .getByRole('heading', { name: 'Page requested not found 😭' });
+    expect(h2NotFound).toBeDefined();
+  });
+});
