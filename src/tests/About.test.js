d5c16c30845fff22b60973bd100621fe4510fe35
André Dantas <aluizcfd@gmail.com>
2021-07-25 14:58:44 -0300

    Adição teste se componente About.js contém h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente About.js', () => {
+  test('Testa se página contem h2 com texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const h2About = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(h2About).toBeDefined();
+  });
+});
