739bff40e0cde75fbd48ba2416863be72aac9a90
André Dantas <aluizcfd@gmail.com>
2021-07-25 14:25:47 -0300

    Adição dos testes de redirecionamento para página de About e Pokémons Favoritos, refatoração no destructuring do pathname

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -15,10 +15,29 @@ describe('Testando o componente App.js', () => {
 
   test('Testa se ao clickar no link "Home" aplicação vai para página inicial', () => {
     const { history } = renderWithRouter(<App />);
-    const { pathname } = history.location;
     const homeNav = screen.getByRole('link', { name: 'Home' });
 
     userEvent.click(homeNav);
+    const { pathname } = history.location;
     expect(pathname).toBe('/');
+  });
+
+  test('Testa se ao clickar no link "About" aplicação vai para página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutNav = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(aboutNav);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa se ao clickar no link "Favorite Pokémons"'
+    + 'vai para a página de pokémons favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokeNav = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    userEvent.click(favoritePokeNav);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });
