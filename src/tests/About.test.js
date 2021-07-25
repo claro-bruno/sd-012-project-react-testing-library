611243c28f46d2f7e33745c661a0e0f0ae2425e9
Ana Laura <analauraalves25@gmail.com>
2021-07-25 17:31:14 -0300

    testa componente About - segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa componente About', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+
+  test('Testa heading', () => {
+    const heading = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(heading).toBeDefined();
+  });
+
+  test('Testa parágrafos', () => {
+    const paragraphOne = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragraphOne).toBeDefined();
+
+    const paragraphTwo = screen.getByText(/One can filter Pokémons by/i);
+    expect(paragraphTwo).toBeDefined();
+  });
+
+  test('Testa src da imagem', () => {
+    const SRC = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const srcImg = screen.getByRole('img');
+    expect(srcImg).toHaveAttribute('src', SRC);
+  });
+});
