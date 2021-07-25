ba6b2e864ed9a26f94458a4fbf70f15033c36c87
thales-daniel <thalesdaniel2013@gmail.com>
2021-07-25 19:42:44 -0300

    feito etapa 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import event from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('testando links na tela principal', () => {
+  it('verifica se botões home, About e favorite estão na tela', () => {
+    renderWithRouter(<App />);
+    const BotaoAbout = screen.getByText('About');
+    const BotaoHome = screen.getByText('Home');
+    const BotaoFavorite = screen.getByText('Favorite Pokémons');
+    expect(BotaoAbout).toBeInTheDocument();
+    expect(BotaoHome).toBeInTheDocument();
+    expect(BotaoFavorite).toBeInTheDocument();
+  });
+
+  it('verifica se o click do botão Home esta funcionando', () => {
+    const { history } = renderWithRouter(<App />);
+    const BotaoHome = screen.getByText(/home/i);
+    event.click(BotaoHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('verifica se o click do botão About esta funcionando', () => {
+    const { history } = renderWithRouter(<App />);
+    const BotaoAbout = screen.getByText(/about/i);
+    event.click(BotaoAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('verifica se o click do botão favorite pokemon esta funcionando', () => {
+    const { history } = renderWithRouter(<App />);
+    const BotaoFavorite = screen.getByText(/favorite pokémons/i);
+    event.click(BotaoFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
---
+++ src/tests/RenderWithRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{ component }</Router>), history,
+  });
+};
+export default renderWithRouter;
