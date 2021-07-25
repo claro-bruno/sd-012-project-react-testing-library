1c1215650f068230384f3182d083b4aa606e9822
paulomichael <paulo.michael@gmail.com>
2021-07-25 19:29:59 -0300

    requisito 1. testa o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { /* render, */ screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('1. Testa o componente <App.js />', () => {
-  beforeEach(() => {
-    renderWithRouter(<App />);
-  });
-
   test('Testa se o topo contém um conjunto fixo de links de navegação.', () => {
-    // const navLinks = screen.getAllByRole('link');
-    const homeLink = screen.getByText('Home');
-    const aboutLink = screen.getByText('About');
-    const favoriteLink = screen.getByText('Favorite Pokémons');
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémon/i);
+
+    expect(history.location.pathname).toBe('/');
     expect(homeLink).toBeDefined();
     expect(aboutLink).toBeDefined();
     expect(favoriteLink).toBeDefined();
-    // falta verificar a ordem
+
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toBe('/favorites');
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+
+    history.push('/notFound');
+    const notFound = screen.getByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
   });
 });
