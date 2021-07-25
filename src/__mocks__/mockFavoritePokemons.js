5ffc026ea096c101e317b282dcfc882973117b07
Julio Pereira <jandrade.pereira1@gmail.com>
2021-07-25 16:32:34 -0300

    Requisito 3 - Insere os testes do componente Favorite Pokemons e insere a pasta mock contendo o arquivo mockFavoritePokemons

---
+++ src/__mocks__/mockFavoritePokemons.js
@@ -0,0 +1,62 @@
+const dragonairSummary = 'They say that if it emits an aura from its whole body, ';
+const dragonairSummary2 = 'the weather will begin to change instantly.';
+
+const charmanderSummary = 'The flame on its tail shows the strength of its life force.';
+const charmanderSummary1 = ' If it is weak, the flame also burns weakly.';
+
+const favoritePokemons = [
+  {
+    id: 148,
+    name: 'Dragonair',
+    type: 'Dragon',
+    averageWeight: {
+      value: '16.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 45',
+        map: 'https://cdn2.bulbagarden.net/upload/2/21/Johto_Route_45_Map.png',
+      },
+      {
+        location: 'Johto Dragon\'s Den',
+        map: 'https://cdn2.bulbagarden.net/upload/1/1e/Johto_Dragons_Den_Map.png',
+      },
+    ],
+    summary: `${dragonairSummary} ${dragonairSummary2}`,
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: `${charmanderSummary} ${charmanderSummary1}`,
+  },
+];
+
+export default favoritePokemons;
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+import favoritePokemons from '../__mocks__/mockFavoritePokemons';
+
+describe('3 - Test the component <FavoritePokemons.js />', () => {
+  it('3.1 - Test if show a message when there is no favorite pokémons', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoriteMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoriteMessage).toBeDefined();
+  });
+
+  // A lógica do código abaixo foi extraída do código do colega Thalles Carneiro.
+
+  // Considerei utilizar este código por ter uma lógica simples, e um código limpo usando mock e forEach.
+
+  // Fonte: https://github.com/tryber/sd-012-project-react-testing-library/pull/18/commits/3f68cb4738377fe8eb10e65743317a46821923be
+
+  it('3.2 - Render favorite pokémons', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+    favoritePokemons.forEach(({ name }) => {
+      const favoritePokemonName = screen.getByText(name);
+      expect(favoritePokemonName).toBeInTheDocument();
+    });
+  });
+});
