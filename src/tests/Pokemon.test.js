335dbcb5d33c1375ed32348f145fae2979b7a351
camila-mp <ca.malvp@gmail.com>
2021-07-25 17:56:19 -0300

    implementa testes do componente Pokemon (req. 6)

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -22,7 +22,7 @@ describe(
     test('Verifica nome, tipo, peso e imagem de cada pokémon', () => {
       renderWithRouter(<App />);
       pokemons.forEach((pokemon) => {
-        const { name, type, averageWeight } = pokemon;
+        const { name, type, averageWeight, image } = pokemon;
         const pokeName = screen.getByTestId('pokemon-name');
         expect(pokeName).toHaveTextContent(name);
         const pokeType = screen.getByTestId('pokemon-type');
@@ -32,10 +32,27 @@ describe(
           `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`,
         );
         const pokeImg = screen
-          .getByAltText('');
-        expect(pokeImg).toHaveProperty('src', URL);
+          .getByAltText(`${name} sprite`);
+        expect(pokeImg).toHaveProperty('src', image);
         fireEvent.click(screen.getByTestId('next-pokemon'));
       });
     });
-}
+    test('Verifica existência do link details', () => {
+      renderWithRouter(<App />);
+      const detailLink = screen.getByText(/More details/i);
+      expect(detailLink).toBeInTheDocument();
+    });
+    test('Verifica pathname na pagina details', () => {
+      const { history } = renderWithRouter(<App />);
+      fireEvent.click(screen.getByText(/More details/i));
+      const { pathname } = history.location;
+      expect(screen.getByText(/Game Locations of/i)).toBeInTheDocument();
+      expect(pathname).toBe('/pokemons/25');
+      const faveCheckbox = screen.getByLabelText('Pokémon favoritado?');
+      fireEvent.click(faveCheckbox);
+      const faveImg = screen
+        .getByAltText('Pikachu is marked as favorite');
+      expect(faveImg).toHaveProperty('src', 'http://localhost/star-icon.svg');
+    });
+  },
 );
