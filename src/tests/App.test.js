45d5e161f74491af711182d5b7629b10af83824e
André Dantas <aluizcfd@gmail.com>
2021-07-25 13:18:19 -0300

    Adição primeira bateria de testes do component App.js, verificação links de navegação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+
+beforeEach(() => {
+  render(
+    <Router>
+      <App />
+    </Router>,
+  );
+});
+
+describe('Testando o componente App.js', () => {
+  test('Testa o texto que contém conjunto de links no topo da página', () => {
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémon');
+  });
+});
