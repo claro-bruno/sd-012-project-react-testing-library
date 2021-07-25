82895b47c3e2b0cc6a11c2a1f251b1abfc12ddc0
Ana Laura <analauraalves25@gmail.com>
2021-07-25 18:25:09 -0300

    testa componente Not Found - quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa componente Not Found', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  test('Testa h2', () => {
+    const text = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(text).toBeDefined();
+  });
+
+  test('Testa src da imagem', () => {
+    const SRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const srcImg = screen.getAllByRole('img');
+    expect(srcImg[1]).toHaveAttribute('src', SRC);
+  });
+});
