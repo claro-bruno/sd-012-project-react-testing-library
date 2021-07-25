1fd0749d8c844ba97c14cc3679c96d6634a3802e
Ana Laura <analauraalves25@gmail.com>
2021-07-25 17:57:09 -0300

    testa componente Favorite Pokemons - terceiro requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testa componente Favorite Pokémons', () => {
+  test('Teste página vazia', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFound = screen.getByText(/No favorite pokemon found/i);
+    expect(noFound).toBeDefined();
+  });
+
+  test('Testa cards favoritados', () => {
+    renderWithRouter(<App />);
+    const card = screen.getByText(/more details/i);
+    userEvent.click(card);
+    const favoriteCard = screen.getByRole('checkbox');
+    userEvent.click(favoriteCard);
+
+    renderWithRouter(<FavoritePokemons />);
+    const cards = screen.getByText(/details/i);
+    expect(cards).toBeDefined();
+  });
+});
