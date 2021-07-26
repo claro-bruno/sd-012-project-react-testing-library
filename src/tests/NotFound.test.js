eb42c36f2e83e690cae0a77c45154fb91de67fbc
thales-daniel <thalesdaniel2013@gmail.com>
2021-07-25 22:25:51 -0300

    feito etapa 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './RenderWithRouter';
+
+describe('testando pagina NotFound', () => {
+  it('testando o h2 na pagina NotFound', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', { level: 2 });
+    expect(h2).toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  it('testando a tag img na pagina NotFound', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
