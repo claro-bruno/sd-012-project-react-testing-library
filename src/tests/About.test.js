fe337660e03451b23b60b8152447f9aa70844989
Caio César <caiovg32@gmail.com>
2021-07-26 14:26:06 -0300

    req.1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa os links de App', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  test('O primeiro link deve possuir o texto Home', () => {
+    const linkAppHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkAppHome).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const linkAppAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAppAbout).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const linkAppFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkAppFavorite).toBeInTheDocument();
+  });
+});
+
+describe('Testa redirecionamento dos aos links', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  test('Testa redirecionamento para o link Home', () => {
+    const linkAppHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkAppHome).toBeInTheDocument();
+  });
+
+  test('Testa redirecionamento para o link About', () => {
+    const linkAppAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAppAbout).toBeInTheDocument();
+  });
+
+  test('Testa redirecionamento para o link Favorites Pokémons', () => {
+    const linkAppFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkAppFavorite).toBeInTheDocument();
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+beforeEach(() => {
+  renderWithRouter(<FavoritePokemons />);
+});
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const mesage = screen.getByText(/No favorite pokemon found/i);
+
+    expect(mesage).toBeInTheDocument();
+  });
+});
