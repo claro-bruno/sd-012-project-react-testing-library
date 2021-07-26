789146d796e3dcf570caf6e3f2811987e02d9aae
lucasmulato <cobain.lucas23@gmail.com>
2021-07-26 16:04:36 -0300

    fazendo req 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <Notfound.js />', () => {
+    it('Contém um heading h2 com o texto Page "requested not found" ', () => {
+        const { getByRole } = render(<NotFound />);
+        const h2NotFound = getByRole('heading', {level: 2});
+        expect(h2NotFound).toBeInTheDocument();
+        expect(h2NotFound.textContent).toMatch(/Page requested not found/i);
+    });
+
+    it('Teste se página mostra a imagem da URL expectedURL.', () => {
+        const { getByAltText } = render(<NotFound />);
+        const expectedURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+        const NotFoundIMG = getByAltText(/Pikachu crying because/i);
+        expect(notFoundIMG).toHaveAtrribute('src', expectedURL);
+    });
+});
