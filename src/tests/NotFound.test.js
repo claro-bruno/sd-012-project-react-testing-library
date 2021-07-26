7e73791f21f6625700121df38c2f2deb98a071f7
lucasmulato <cobain.lucas23@gmail.com>
2021-07-26 20:09:47 -0300

    last commit indeed and finally

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -5,15 +5,15 @@ import NotFound from '../components/NotFound';
 describe('Teste o componente <NotFound.js />', () => {
   it('Contém um heading h2 com o texto Page "requested not found" ', () => {
     const { getByRole } = render(<NotFound />);
-    const h2NotFound = screen.getByRole('heading', { level: 2 });
+    const h2NotFound = getByRole('heading', { level: 2 });
     expect(h2NotFound).toBeInTheDocument();
-    expect(h2NotFound.textContent).toMatch(/Page requested not found/i);
+    expect(h2NotFound.textContent).toMatch(/requested not found/i);
   });
 
   it('Teste se página mostra a imagem da URL expectedURL.', () => {
     const { getByAltText } = render(<NotFound />);
     const expectedURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-    const notFoundIMG = screen.getByAltText(/Pikachu crying because/i);
+    const notFoundIMG = getByAltText(/Pikachu crying because/i);
     expect(notFoundIMG).toHaveAttribute('src', expectedURL);
   });
 });
