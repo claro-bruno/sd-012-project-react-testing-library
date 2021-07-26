7f2616b8be937f073fe1b7df64ce625afda6f576
Antonio Malato <malatoneto@gmail.com>
2021-07-25 23:45:49 -0300

    Requisito 5 incompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Verifica o componente "Pokedex.js"', () => {
+  test('Verifica se contém a tag "h2" com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const h2Title = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(h2Title).toBeInTheDocument();
+  });
+
+  // test('Verifica o próximo pokémon ao ser clicado o botão', () => {
+  //   renderWithRouter(<App />);
+  // })
+});
