065ef0e80164cdd58b308162af80c6092c12b09b
Augusto <augustocm2002@yahoo.com.br>
2021-07-26 11:27:03 -0300

    Fazendo requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<NotFound />);
+});
+describe('Testando o componente NotFound.js', () => {
+  test('Verifica se página contém um h2 com o texto Page requested not found ', () => {
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('Verifica se página mostra a imagem', () => {
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
