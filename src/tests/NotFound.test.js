d21fe29df6f1be50a3f7b5cbe91b7297a62b8ecd
lucasmulato <cobain.lucas23@gmail.com>
2021-07-26 17:48:17 -0300

    last commit i guess

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import NotFound from '../components/NotFound';
-// import RenderWithRouter from '../RenderWithRouter';
 
 describe('Teste o componente <NotFound.js />', () => {
   it('Contém um heading h2 com o texto Page "requested not found" ', () => {
     const { getByRole } = render(<NotFound />);
-    const h2NotFound = getByRole('heading', { level: 2 });
+    const h2NotFound = screen.getByRole('heading', { level: 2 });
     expect(h2NotFound).toBeInTheDocument();
     expect(h2NotFound.textContent).toMatch(/Page requested not found/i);
   });
@@ -14,7 +13,7 @@ describe('Teste o componente <NotFound.js />', () => {
   it('Teste se página mostra a imagem da URL expectedURL.', () => {
     const { getByAltText } = render(<NotFound />);
     const expectedURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-    const notFoundIMG = getByAltText(/Pikachu crying because/i);
+    const notFoundIMG = screen.getByAltText(/Pikachu crying because/i);
     expect(notFoundIMG).toHaveAttribute('src', expectedURL);
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './../renderWithRouter';
 import data from '../data';
 
 const pokemonType = 'pokemon-type';
