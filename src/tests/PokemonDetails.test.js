ce2348ba202a3e555887cd1185064e47d73d8f82
walttoncoelho <walltton@outlook.com>
2021-07-26 14:12:04 -0300

    quest 07 primeira parte

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import {
+  screen,
+  fireEvent,
+} from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('7- Teste o componente <PokemonDetails.js', () => {
   test('1-Teste se as informações detalhadas do Pokémon selecionado', () => {
     renderWithRouter(<App />);
-  });
-
-  test('2- Teste se existe na página uma seção com os mapas', () => {
-    renderWithRouter(<App />);
-  });
-  test('3- Teste se o usuário pode favoritar um pokémon através', () => {
-    renderWithRouter(<App />);
+    // Testando o Sumário
+    const moreDetails = (screen.getByText(/More details/));
+    fireEvent.click(moreDetails);
+    const sumary = (screen.getByRole('heading', { level: 2, name: /Summary/ }));
+    expect(sumary).toBeInTheDocument();
+    // Testando o Parágrafo
+    const paragraph = (screen.getByText(/This intelligent Pokémon/));
+    expect(paragraph).toBeInTheDocument();
+    // Testa o checkBox
+    const checkLabel = screen.getByLabelText(/Pokémon favoritado?/);
+    expect(checkLabel).toBeInTheDocument();
+    fireEvent.click(checkLabel);
+    const markedFavorite = screen.getByAltText(/Pikachu is marked as favorite/);
+    /* expect(mapPower.src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png'); */
+    expect(markedFavorite).toBeInTheDocument();
+    /* const mapPower = screen.getByText('Kanto Power Plant'); */
   });
 });
