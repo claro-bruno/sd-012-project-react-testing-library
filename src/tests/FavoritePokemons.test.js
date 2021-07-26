a794c8bc4530123c060a79c03021406e06947644
thales-daniel <thalesdaniel2013@gmail.com>
2021-07-25 21:47:44 -0300

    feito etapa 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './RenderWithRouter';
+
+const mockFavoritePokemons = [{
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+},
+{
+  id: 4,
+  name: 'Charmander',
+  type: 'Fire',
+  averageWeight: {
+    value: '8.5',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Alola Route 3',
+      map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+    },
+    {
+      location: 'Kanto Route 3',
+      map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+    },
+    {
+      location: 'Kanto Route 4',
+      map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+    },
+    {
+      location: 'Kanto Rock Tunnel',
+      map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+    },
+  ],
+}];
+describe('vendo os pokemons favortios', () => {
+  it('testando se existe pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockFavoritePokemons } />);
+    const pikachu = screen.getByText(mockFavoritePokemons[0].name);
+    const charmander = screen.getByText(mockFavoritePokemons[1].name);
+    expect(pikachu).toBeInTheDocument();
+    expect(charmander).toBeInTheDocument();
+  });
+});
