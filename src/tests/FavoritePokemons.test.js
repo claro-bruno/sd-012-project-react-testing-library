dfa741ab610928305810ff9e06a4757f0519e27f
Antonio Malato <malatoneto@gmail.com>
2021-07-25 23:00:14 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Verifica o componente "FavoritePokemons.js"', () => {
+  test('Verifica se a mensagem "No favorite pokemin found" exibe na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const foundText = screen.getByText(/No Favorite Pokemon Found/i);
+    expect(foundText).toBeInTheDocument();
+  });
+});
