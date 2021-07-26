64bb45bc2439536fba47df0cf4751865743aed0c
Antonio Malato <malatoneto@gmail.com>
2021-07-25 21:03:58 -0300

    Test link 'About'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,13 +5,23 @@ import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
 describe('Verifica o componente "App.js"', () => {
-  test('Verifica o link é de navegação e com o texto "Home"', () => {
+  test('Verifica se o link é de navegação e com o texto "Home"', () => {
     const { history } = renderWithRouter(<App />);
     const linkHome = screen.getByRole('link', { name: /Home/i });
     expect(linkHome).toBeDefined();
-    userEvent.click(linkHome);
 
+    userEvent.click(linkHome);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
+  });
+
+  test('Verifica se o linké de navegação e com o texto "About"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeDefined();
+
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
   });
 });
