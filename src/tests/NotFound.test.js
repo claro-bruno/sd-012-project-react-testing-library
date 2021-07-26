ed19c2f0cc49cc593f7b52614f8f9984ae527f38
Julio Pereira <jandrade.pereira1@gmail.com>
2021-07-25 22:14:52 -0300

    Requisito 4 - Retira o renderWithRouter e inclue o beforeEach render para cada it

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
+import { render, screen } from '@testing-library/react';
 import { NotFound } from '../components';
 
 describe('4 - Test component <NotFound.js />', () => {
+  beforeEach(() => render(<NotFound />));
   it('4.1 - Test if has a <h2> tag with the message "Encountered pokémons"', () => {
-    renderWithRouter(<NotFound />);
     const h2Tag = screen.getByRole('heading', { level: 2 });
     expect(h2Tag).toBeInTheDocument();
   });
   it('4.2 - Test if the page shows a gif error', () => {
-    renderWithRouter(<NotFound />);
     const IMAGE_URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
     const imageAltTag = 'Pikachu crying because the page requested was not found';
     const imageError = screen.getByAltText(imageAltTag);
