e113182380122ce8ec1cb442624dbb6579f2e6b8
Augusto <augustocm2002@yahoo.com.br>
2021-07-26 11:53:44 -0300

    Fazendo requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<FavoritePokemons />);
+});
+
+describe('Testando o componente FavoritePokemons.js', () => {
+  test('Verifica a mensagem No favorite pokemon found, sem pokémons favoritos ', () => {
+    const pokemonMsg = screen.getByText('No favorite pokemon found');
+    expect(pokemonMsg).toBeInTheDocument();
+  });
+});
