f94f7f99d5c7584baca3c321dfeaadc2698bf0f0
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-27 11:32:58 -0300

    add req 3 incompleto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+// import * as FavoritePokemons from '../components/FavoritePokemons';
+import { FavoritePokemons } from '../components';
+
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found, se a pessoa não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+if(FavoritePokemons.isEmpty) {
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+// } else {
+//     expect(screen.getAllByText('favorite-pokemons')).toBeDefined();
+}
+  });
+});
