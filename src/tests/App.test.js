d3212fc293a55b2634371043e55bffd406f76b28
ladcs <hpladcs@gmail.com>
2021-07-27 16:16:31 -0300

    R1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,37 @@
-test('', () => {}); // começo de tudo 
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('teste pagina inicial app.js, se há e se redireciona os links', () => {
+  test('home', () => {
+    // rederizar a page
+    const { history } = renderWithRouter(<App />);
+    // capturar o elemento
+    const linkToHome = screen.getByRole('link', { name: /Home/i });
+    // testa click
+    userEvent.click(linkToHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('About', () => {
+    // rederizar a page
+    const { history } = renderWithRouter(<App />);
+    // capturar o elemento
+    const linkToHome = screen.getByRole('link', { name: /About/i });
+    // testa click
+    userEvent.click(linkToHome);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Favorite Pokemons', () => {
+    // rederizar a page
+    const { history } = renderWithRouter(<App />);
+    // capturar o elemento
+    const linkToHome = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    // testa click
+    userEvent.click(linkToHome);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
