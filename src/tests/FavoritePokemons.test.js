dc432d1fb6a6816beb7c6d93ad6dcddfa8f2754b
ladcs <hpladcs@gmail.com>
2021-07-27 20:24:49 -0300

    R3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test favorite Pokemons', () => {
+  it('Test without pokemon', () => {
+    // render the page
+    renderWithRouter(<FavoritePokemons />);
+    // get element
+    const text = screen.getByText('No favorite pokemon found');
+    // test
+    expect(text).toBeDefined();
+  });
+
+  it('Testa caso tenha pokémon favorito', () => {
+    // render the page
+    renderWithRouter(<App />);
+    // get element
+    const details = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(details);
+    const check = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(check);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    // test
+    expect(screen.getByText('Pikachu')).toBeDefined();
+    expect(screen.getByText('Electric')).toBeDefined();
+    expect(screen.getByText('Average weight: 6.0 kg'))
+      .toBeDefined();
+  });
+});
