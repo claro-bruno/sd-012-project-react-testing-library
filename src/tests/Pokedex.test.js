886b2979c8dc5f75157d194680d894a436e3a1eb
Antonio Malato <malatoneto@gmail.com>
2021-07-27 22:23:33 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
@@ -10,7 +11,39 @@ describe('Verifica o componente "Pokedex.js"', () => {
     expect(h2Title).toBeInTheDocument();
   });
 
-  // test('Verifica o próximo pokémon ao ser clicado o botão', () => {
+  // test('Verifica se tem 1 pokemon por vez', () => {
   //   renderWithRouter(<App />);
-  // })
+  //   const verifyAmount = screen.getByRole('paragraph', { name: /pikachu/i });
+  //   expect(verifyAmount).toHaveLength(1);
+  // });
+
+  test('Verifica o próximo pokémon ao ser clicado o botão', () => {
+    renderWithRouter(<App />);
+    const button = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(button).toBeDefined();
+
+    userEvent.click(button);
+    const nextPokemon = screen.getByText(/Charmander/i);
+    expect(nextPokemon).toBeInTheDocument();
+  });
+
+  test('Verifica se o componente possui botões', () => {
+    renderWithRouter(<App />);
+    const buttons = screen.getByRole('button', { name: /Fire/i });
+    expect(buttons).toBeInTheDocument();
+    userEvent.click(buttons);
+
+    const typeButton = screen.getByTestId('pokemon-type');
+    expect(typeButton).toHaveTextContent(/Fire/i);
+  });
+
+  test('Verifica se o botao "All" reseta o filtro', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /All/i });
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(buttonAll);
+    const resetPokemon = screen.getByTestId('pokemon-type');
+    expect(resetPokemon).toHaveTextContent(/Electric/i);
+  });
 });
