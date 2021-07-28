0e559b389141bb9030053b387bc4e0266afea009
Augusto <augustocm2002@yahoo.com.br>
2021-07-28 13:18:26 -0300

    Completando requisito 3 e fazendo requisito 6

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
-
-beforeEach(() => {
-  renderWithRouter(<FavoritePokemons />);
-});
+import App from '../App';
 
 describe('Testando o componente FavoritePokemons.js', () => {
   test('Verifica a mensagem No favorite pokemon found, sem pokémons favoritos ', () => {
+    renderWithRouter(<FavoritePokemons />);
     const pokemonMsg = screen.getByText('No favorite pokemon found');
     expect(pokemonMsg).toBeInTheDocument();
+  });
+  test('Testando se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const pokemonsDetails = screen.getByText('More details');
+    userEvent.click(pokemonsDetails);
+    const favoritePokemon = screen.getByText('Pokémon favoritado?');
+    expect(favoritePokemon).toBeInTheDocument();
+    const pokemonCheckBox = screen.getByRole('checkbox');
+    userEvent.click(pokemonCheckBox);
+    const favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favorite);
+    const favoritePName = screen.getByText('Pikachu');
+    expect(favoritePName).toBeInTheDocument();
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testando o componente Pokemon.js', () => {
+  test('Verificando se o nome correto do Pokémon é mostrado na tela', () => {
+    renderWithRouter(<App />);
+    const cardPokemon = screen.getByTestId('pokemon-name');
+    expect(cardPokemon).toBeInTheDocument();
+  });
+
+  test('Verificando se o tipo correto do Pokémon é mostrado na tela', () => {
+    renderWithRouter(<App />);
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toBeInTheDocument();
+  });
+
+  test('Verificando o peso médio do pokémon e sua unidade de medida', () => {
+    renderWithRouter(<App />);
+    const pokemonWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(pokemonWeight).toBeInTheDocument();
+  });
+
+  test('Verificando a imagem do pokémon', () => {
+    renderWithRouter(<App />);
+    const pokemonImage = screen.getByRole('img');
+    expect(pokemonImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+
+  test('Verificando se o card indicado contém um link para exibir detalhes', () => {
+    renderWithRouter(<App />);
+    const pokemonDetails = screen.getByRole('link', { name: 'More details' });
+    expect(pokemonDetails).toBeInTheDocument();
+  });
+
+  test('Verificando se o link redireciona para a URL correta', () => {
+    const { history } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  test('se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const { name, id } = pokemons[0];
+    const pokemonStars = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(pokemonStars);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${id}`);
+    const favoriteP = screen.getByLabelText(/Pokémon favoritado?/i);
+    userEvent.click(favoriteP);
+    const pokemonHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(pokemonHome);
+    const pokemonData = screen.getByAltText(`${name} is marked as favorite`);
+    expect(pokemonData.src).toContain('/star-icon.svg');
+  });
+});
