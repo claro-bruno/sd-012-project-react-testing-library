76c5f5975c339aecb167e1cfefbf41e025b2f5f9
gscuri <gscuri6@hotmail.com>
2021-07-28 12:31:30 -0300

    exercico 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+beforeEach(() => {
+  renderWithRouter(<FavoritePokemons />);
+});
+
+describe('3- Teste o componente <FavoritePokemons.js />', () => {
+  test('Verifica a mensagem No favorite pokemon found, sem pokémons favoritos ', () => {
+    const msgFavPokemons = screen.getByText('No favorite pokemon found');
+    expect(msgFavPokemons).toBeInTheDocument();
+  });
+});
