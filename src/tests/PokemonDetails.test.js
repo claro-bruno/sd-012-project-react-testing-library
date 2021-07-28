b27a3c356a798c9bbf8b9b8604c59b946d749295
Victor Emmanuel <victorpassos2012@gmail.com>
2021-07-28 20:57:56 -0300

    Requisito 7 feito

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import data from '../data';
+import renderWithRouter from './renderWithRouter';
+// Codigo de Bruno Yamamoto https://github.com/tryber/sd-012-project-react-testing-library/pull/40/files?file-filters%5B%5D=.js
+const pokemon = data[4];
+
+describe('Testa o componente PokemonDetails', () => {
+  it('Testa se Pokemon details esta sendo renderizado corretamente.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${pokemon.id}`);
+    expect(screen.getByRole('heading', { name: `${pokemon.name} Details` }));
+
+    expect(screen.queryByText('More details')).toBeNull();
+
+    expect(screen.getByRole('heading', { name: 'Summary' }));
+
+    expect(screen.getByText(pokemon.summary)).toBeDefined();
+  });
+
+  it('Testa se possui mapas com as localizações', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${pokemon.id}`);
+    expect(screen.getByRole('heading', { name: `Game Locations of ${pokemon.name}` }));
+
+    const locationsLen = screen.getAllByRole('img', { name: `${pokemon.name} location` });
+    expect(locationsLen).toHaveLength(pokemon.foundAt.length);
+
+    pokemon.foundAt.forEach((item, index) => {
+      const { location, map } = item;
+      expect(screen.getByText(location)).toBeDefined();
+      const allImage = screen.getAllByRole('img', { name: `${pokemon.name} location` });
+      expect(allImage[index]).toHaveProperty('src', map);
+    });
+  });
+
+  it('Testa se é possivel favoritar um pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${pokemon.id}`);
+
+    const check = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    expect(check).toBeDefined();
+    userEvent.click(check);
+
+    const altText = `${pokemon.name} is marked as favorite`;
+    const favorite = screen.getByRole('img', { name: altText });
+    expect(favorite).toBeDefined();
+    expect(favorite).toHaveAttribute('src', '/star-icon.svg');
+
+    userEvent.click(check);
+    const uncheck = screen.queryByRole('img', { name: altText });
+    expect(uncheck).toBeNull();
+  });
+});
