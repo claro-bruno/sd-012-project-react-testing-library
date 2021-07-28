e32eaf43b555ea798fd6bb40c5ad9cff9729672c
Caio César <caiovg32@gmail.com>
2021-07-28 17:50:37 -0300

    test <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFount from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa <NotFound />', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFount />);
+  });
+
+  test('Testa se há um heading h2 com o texto Page requested not found', () => {
+    const headingh2 = screen.getByRole('heading');
+    expect(headingh2).toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  test('Testa se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const imageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByAltText(/Pikachu crying because the page requested was n/i);
+    expect(image).toHaveProperty('src', imageSrc);
+  });
+});
