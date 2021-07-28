846e30d04a90771bac1dd27221777fe37c96242a
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-28 10:38:41 -0300

    adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWhithRouter from './renderWhithRouter.test';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    renderWhithRouter(<FavoritePokemons />);
+  });
+  test('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeDefined();
+  });
+});
+// Feito com ajuda do Wadson!
