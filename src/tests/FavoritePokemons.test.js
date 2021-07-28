17b8e25968c8e5b313a8638a69991a98b9177284
Gustavo-Mourao <gcmbh1@gmail.com>
2021-07-28 12:21:50 -0300

    Q3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Teste se No favorite pokemon found`, caso não tenha pokémons favoritos', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const noFavPok = /No favorite pokemon found/i;
+  expect(screen.getByText(noFavPok)).toBeDefined();
+});
