99fa6d872fe74d2073bba4dcb7ddd60d6dc53798
Felipe Carvalho <fecarvalho066@gmail.com>
2021-07-28 15:18:45 -0300

    Requisito 2.1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
-describe('<About.js /> component testing', () => {
-  it('Se a pág contém informações sobre a "Pokédex".', () => {
-    render(<About />);
-    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+describe('Testa About.js', () => {
+  beforeEach(() => render(<About />));
+
+  it('Existe um heading h2 com o texto About Pokédex', () => {
+    const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(title).toBeDefined();
   });
 
-  it('Se a pág contém um "heading h2" com o texto "About Pokédex"', () => {
-    render(<About />);
-    const heading = screen.getByText('heading');
-    expect(heading).toBeInTheDocument();
-    expect(heading.textContent).toBe('About Pokédex');
+  it('Existem dois parágrafos com texto sobre a Pokédex', () => {
+    const paragraphs = screen.getAllByText(/Pokémons/i);
+    const size = 2;
+    expect(paragraphs.length).toBe(size);
   });
 
-  it('Se a pág contém dois parágrafos com texto sobre a Pokédex', () => {
-    render(<About />);
-    const paragraphs = querySelectorAll('p');
-    expect(paragraphs.length).toBe(2);
-  });
-
-  it('Se a pág contém a seguinte imagem de uma Pokédex', () => {
-    render(<About />);
-    const image = uerySelector('img');
-    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  it('Existe imagem de uma Pokédex', () => {
+    const imageURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imageAlt = 'Pokédex';
+    const imageTestOne = screen.getByAltText(imageAlt);
+    const imageTextTwo = screen.getByRole('img');
+    expect(imageTestOne).toBeDefined();
+    expect(imageTextTwo.src).toBe(imageURL);
   });
 });
