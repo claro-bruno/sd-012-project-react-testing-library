7a9449c1f23c0e0b7a13293a54476f42f6fe9970
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-28 10:34:59 -0300

    adiciona requisito requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,28 +1,32 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import About from '../components/About';
-// import renderWhithRouter from './renderWhithRouter.test';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWhithRouter from './renderWhithRouter.test';
 
-// describe('Testando componente About', () => {
-//   renderWhithRouter(<About />);
+describe('Testando componente About', () => {
+  beforeEach(() => {
+    renderWhithRouter(<About />);
+  });
 
-//   test('Teste se a página contém as informações sobre a Pokédex', () => {
-//     const infPokedex = screen.getByRole('link', { name: /About/i });
-//     expect(infPokedex).toBeInTheDocument();
-//   });
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    const infPokedex = screen.getByText(/About/i);
+    expect(infPokedex).toBeInTheDocument();
+  });
 
-//   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-//     const title = screen.getByText('heading', { name: /About/i });
-//     expect(title).toBeInTheDocument();
-//   });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const title = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
 
-//   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-//     const paragraph = screen.getByText('textBox', { name: /About/i });
-//     expect(paragraph).toBeInTheDocument();
-//   });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const paragraph = screen.getAllByText(/Pokémons/i);
+    expect(paragraph.length).toBe(2);
+  });
 
-//   test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
-//     const img = screen.getByRole('link', { name: /About/i });
-//     expect(img).toBeInTheDocument();
-//   });
-// });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const img = screen.getByRole('img', { name: /Pokédex/i });
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(src);
+  });
+});
+// Feito com ajuda do Wadson!
