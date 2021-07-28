7a1711d6e86a51287099b768388529860ec5b9ff
Ana Laura <analauraalves25@gmail.com>
2021-07-28 18:30:02 -0300

    refaz quinto requisito

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,26 +2,47 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import App from '../App';
+import Pokemon from '../components/Pokemon';
+import pokemon from '../data';
 
 describe('Testa componente Pokémon', () => {
   beforeEach(() => {
-    renderWithRouter(<App />);
+    renderWithRouter(<Pokemon pokemon={ pokemon[0] } isFavorite />);
   });
 
-  test('Testa se renderiza um card com as informações do pokemon', () => {
-    const pokemon = screen.getByText(/more details/i);
-    userEvent.click(pokemon);
+  const { id, name, type, averageWeight, image } = pokemon[0];
+  const { value, measurementUnit } = averageWeight;
 
-    const name = screen.getByTestId(/pokemon-name/i);
-    expect(name).toBeDefined();
-    const type = screen.getByTestId(/pokemon-type/i);
-    expect(type).toBeDefined();
-    const weight = screen.getByTestId(/pokemon-weight/i);
-    expect(weight).toBeDefined();
-    const SRC = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+  test('Testa se renderiza um card com as informações do pokemon', () => {
+    const namePok = screen.getAllByText(name);
+    expect(namePok).toBeDefined();
+    const typePok = screen.getAllByText(type);
+    expect(typePok).toBeDefined();
+    const weightPok = screen.getAllByText(`Average weight: ${value} ${measurementUnit}`);
+    expect(weightPok).toBeDefined();
     const srcImg = screen.getAllByRole('img');
-    expect(srcImg[0]).toHaveAttribute('src', SRC);
-    expect(srcImg[0]).toHaveAttribute('alt', 'Pikachu sprite');
+    expect(srcImg[0]).toHaveAttribute('src', image);
+    expect(srcImg[0]).toHaveAttribute('alt', `${pokemon[0].name} sprite`);
+  });
+
+  test('Testa se há um link de navegação para mais detalhes', () => {
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon[0] }
+        isFavorite={ false }
+        showDetailsLink
+      />,
+    );
+    const details = screen.getAllByText(/more details/i)[1];
+    expect(details).toBeDefined();
+    userEvent.click(details);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test('Testa se pokemon está favoritado', () => {
+    const favImage = screen.getAllByRole('img')[1];
+    expect(favImage).toHaveAttribute('src', '/star-icon.svg');
+    expect(favImage).toHaveAttribute('alt', `${name} is marked as favorite`);
   });
 });
