94492bdf1a171037104a93b863449514f000d106
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-28 02:38:10 -0300

    Cria teste verifica se renderiza os links de navegaçao do componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './Helper/RendeWithRouter';
+
+describe('Testa componente App', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  test('Verifica se renderiza os links de navegação', () => {
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    const links = [linkHome, linkAbout, linkFavPoke];
+
+    links.forEach((link) => {
+      expect(link).toBeInTheDocument();
+    });
+  });
+});
