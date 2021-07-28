2c146c0018908cd9aa06c0712ad416c7667c337c
LuanAlexandre <luanrb19@gmail.com>
2021-07-27 21:27:35 -0400

    Dia 15.4 - projeto-react-testing-library: solucao do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('Teste se a página contém um heading "h2" com o texto "About Pokédex"', () => {
+  renderWithRouter(<About />);
+  const tagTitle = screen.getByRole('heading');
+  expect(tagTitle).toBeInTheDocument();
+  expect(tagTitle.innerHTML).toBe('About Pokédex');
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  renderWithRouter(<About />);
+  const pokedexImage = screen.getByRole('img');
+  expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
+
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  renderWithRouter(<About />);
+  const informations = screen.getAllByTestId('pokedex-paragraph');
+  expect(informations.length).toBe(2);
+});
