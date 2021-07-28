94ed74dcf7ccb883cc978143221a4bc2bd3830e3
Wadson <wadson.dev92@gmail.com>
2021-07-28 01:22:02 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWhithRouter from './renderWithRouter.test';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    renderWhithRouter(<FavoritePokemons />);
+  });
+
+  test('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    const message = screen.getByText(/No favorite pokemon found/i);
+
+    expect(message).toBeDefined();
+  });
+});
