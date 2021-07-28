585f6d94602e72723c6c80835fff93ad32400685
gscuri <gscuri6@hotmail.com>
2021-07-28 11:32:20 -0300

    exercico 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+beforeEach(() => {
+  renderWithRouter(<NotFound />);
+});
+
+describe('4-Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading h2 c/ o texto Page requested not found', () => {
+    const notFoundH2 = screen.getByRole('heading', { level: 2 });
+    expect(notFoundH2).toHaveTextContent('Page requested not found');
+  });
+
+  test('Teste se a página contém link de imagem de uma Pokédex:', () => {
+    const imgNotFound = screen.getAllByRole('img');
+    expect(imgNotFound[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
