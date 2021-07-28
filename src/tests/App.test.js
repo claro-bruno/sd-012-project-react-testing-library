17a35024be5c4ed60cbf4a67ef8533646f8d7c49
Pedro Resck <pedroresck1@gmail.com>
2021-07-28 20:22:16 -0300

    Requisito 1 - Testando o componente APP

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste verificando se os links estão direcionando para o local correto.', () => {
+  it('Navegação do link - HOME', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[0]);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+  it('Navegação do link ABOUT', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[1]);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+  it('Navegação do link FAVORITE POKÉMONS', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[2]);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+  it('Navegação para página NOT FOUND ao entrar com URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = screen.getByText('Page requested not found');
+    expect(noMatch).toBeInTheDocument();
+  });
+});
+
+describe('Teste se o componente APP possui os links Home/About/Favorite Pokémon.', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  it('Primeiro link encontrado em APP é: HOME', () => {
+    const links = screen.getAllByRole('link');
+    expect(links[0].innerHTML).toBe('Home');
+  });
+  it('Segundo link encontrado em APP é: ABOUT', () => {
+    const links = screen.getAllByRole('link');
+    expect(links[1].innerHTML).toBe('About');
+  });
+  it('Terceiro link encontrado em APP é: FAVORITE POKÉMONS', () => {
+    const links = screen.getAllByRole('link');
+    expect(links[2].innerHTML).toBe('Favorite Pokémons');
+  });
+});
