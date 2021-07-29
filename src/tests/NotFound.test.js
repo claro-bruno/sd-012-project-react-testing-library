cdf7cc57af1286396f284caa076c72a37cc086e8
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-28 23:47:41 -0300

    add req 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o componente NotFound', () => {
+  it(`Testa se a página contém um heading
+  h2 com o texto Page requested not found`, () => {
+    render(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+  });
+  it('Testa se a página contém a seguinte imagem', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
