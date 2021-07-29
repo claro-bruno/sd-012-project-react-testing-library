fe67c1459829f810bc37a19c7f59f19848aa4981
Wadson Almeida <wadson.dev92@gmail.com>
2021-07-28 22:27:49 -0300

    Requisitos 4 e 5 com muuuuuuuito custo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWhithRouter from './renderWithRouter.test';
+
+describe('Testando componente NotFound', () => {
+  beforeEach(() => {
+    renderWhithRouter(<NotFound />);
+  });
+
+  test('Teste se pag contém um heading h2 com o texto Page requested not found', () => {
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(title).toBeDefined();
+  });
+
+  test('Teste se página mostra a imagem Pikachu not found', () => {
+    const img = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img.src).toBe(src);
+  });
+});
