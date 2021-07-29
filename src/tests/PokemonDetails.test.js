1af27f0ea9e25fbf69dfa889a59ac688ee2be2ca
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-29 10:49:45 -0300

    Requisito 7 - details, map, favorite.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa componente PokemonDetails', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByRole('link', { name: /More details/i }));
+});
+
+    test('Teste se a pagina mostra informacoes detalhadas', () => {
+        const { name, summary } = pokemons[0];
+        const h2pokemon = screen.getByRole('heading', { name: `${name} Details` });
+        expect(h2pokemon.localName).toBe('h2');
+
+        const link = screen.queryByRole('link', { name: /More details/i });
+        expect(link).not.toBeInTheDocument();
+        
+        const summary = screen.getByRole('heading', { name: /Summary/i });
+        expect(summary.localName).toBe('h2');
+
+        const summaryParagraph = screen.getByText(summary);
+        expect(summaryParagraph).toBeInTheDocument();
+        expect(summaryParagraph.localName).toBe('p');
+  });
+    test('Testa se aparecem mapas da localozação do pokémon', () => {
+
+
+    })
+
+
+  });
+
+
