0c4b1b012df01b962e941dd7c556d2903de71f7a
LuanAlexandre <luanrb19@gmail.com>
2021-07-29 16:11:47 -0400

    Dia 15.4 - projeto-react-testing-library: solucao do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Teste se página contém um heading "h2" com o texto "Page requested not found"',
+  () => {
+    render(<NotFound />);
+    const title = screen.getByRole('heading');
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent('Page requested not found');
+  });
+
+test('Teste se página mostra uma imagem',
+  () => {
+    render(<NotFound />);
+    const ALT_TEXT = 'Pikachu crying because the page requested was not found';
+    const pikachuImage = screen.getByAltText(ALT_TEXT);
+    expect(pikachuImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
