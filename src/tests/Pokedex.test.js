2af7af9c0592d4a846230b329dbd590829f2d8d7
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-29 09:42:47 -0300

    Requisito 5 - pokedex,H2 and filter

---
+++ src/tests/Pokedex.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Testa componente <Pokedex />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  test('Teste se página contém um heading h2 com texto Encountered Pokémons',() => {
+    const headingh2 = screen.getByRole('heading',
+    { name : /Encountered Pokémons/i, level: 2 });
+    expect(headingh2).toBeInTheDocument();
+  });
+  
+  const filtros = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+
+  test('Teste se ao clicar o proximo pokémon é exibido', () => {
+    const button =screen.getByRole('button', 
+    {  name: /Próximo pokémon/i,});
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+});
+test('Teste se é mostrado apenas um Pokémon por vez', () => {
+  expect(screen.getAllByTestId('pokemon-name').length).toBe(1);
+});
+
+test('Testa se a Pokedex tem os botões de filtro', () => {
+  const numberSeven = 7;
+  const buttonAll = screen.getByText('All');
+  const btnpokemons = screen.getAllByTestId('pokemon-type-button');
+
+  expect(btnpokemons.length).toBe(numberSeven);
+  expect(buttonAll).toBeInTheDocument();
+  expect(btnpokemons[0].innerHTML).toBe('Electric');
+});
+});
