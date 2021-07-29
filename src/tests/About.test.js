fa15e52cf8a83e91eaf7171b40025f782ddc3b89
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-28 23:46:33 -0300

    add req 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { About } from '../components';
 
+const p1 = 'This application simulates a Pokédex,'
++ ' a digital encyclopedia containing all Pokémons';
+const p2 = 'One can filter Pokémons by type,'
++ ' and see more details for each one of them';
+
 describe('Testa o componente About', () => {
-  it('Testa se a página contém as informações sobre a Pokédex', () => {
+  it('Testa se a página contém as informações sobre a Pokédex em 2 parágrafos', () => {
     render(<About />);
-    const section = screen.getByRole('section');
-    expect(section.length).toBe(2);
+    expect(screen.getByText(p1)).toBeDefined();
+    expect(screen.getByText(p2)).toBeDefined();
   });
+
   it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
     render(<About />);
-    const h2 = screen.getByRole('heading', { name: /about pokédex/i });
+    const h2 = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
     expect(h2).toBeInTheDocument();
   });
-  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    // const p1 = "This application simulates a Pokédex, a digital encyclopedia containing all Pokémons";
-    // const p2 = "One can filter Pokémons by type, and see more details for each one of them";
-    render(<About />);
-    const p = screen.getAllByText(/Pokémons/i);
-    expect(p.length).toBe(2);
-  });
+
   it('Testa se a página contém a seguinte imagem de uma Pokédex', () => {
     render(<About />);
     const img = screen.getByRole('img');
