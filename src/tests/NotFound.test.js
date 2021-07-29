1745c03741a58f01b08ad473a53a8988d50f5f57
Pedro Resck <pedroresck1@gmail.com>
2021-07-29 16:02:33 -0300

    Requisito 4 - Testando o componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente NotFound.js.', () => {
+  it('Verifica se: a página possui um h2 com o texto:'
+  + '"Page requested not found 😭".', () => {
+    renderWithRouter(<NotFound />);
+    const textVerifyNotFound = screen.getByRole(
+      'heading', { name: /Page requested not found/i },
+    );
+    const cryingEmoji = screen.getByRole('img', { name: 'Crying emoji' });
+    expect(textVerifyNotFound).toBeInTheDocument();
+    expect(cryingEmoji).toBeInTheDocument();
+  });
+  it('Verifica se: a página mostra o gif da URL em questão.', () => {
+    renderWithRouter(<NotFound />);
+    const gifToVerify = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const gifNotFound = screen.getAllByRole('img');
+    expect(gifNotFound[1]).toHaveAttribute('src', gifToVerify);
+  });
+});
+
+// A verificação do emoji chorando (const cryingEmoji) eu peguei a ideia do código do Milton Castro//
