ad2593887bf5739fc10aa59806ae6ea9962410a9
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-29 12:39:43 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testando component App', () => {
+  it('Testando Links', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+
+    expect(history.location.pathname).toBe('/');
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toBe('/favorites');
+
+    history.push('/NotIsAPage');
+    const pageNotFound = screen.getByText(/Page requested not found/i);
+    expect(pageNotFound).toBeInTheDocument();
+  });
+});
