def480ce6186b5a1aa4a5c613f03636016d43a5b
Pedro Resck <pedroresck1@gmail.com>
2021-07-29 13:01:08 -0300

    Requisito 3 - Testando o componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente FavoritePokemon.js.', () => {
+  it('Verifica se: aparece o texto "No favorite pokemon found",'
+    + 'se a pessoa não possuir Pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const getMessage = screen.getByText('No favorite pokemon found');
+    expect(getMessage).toBeInTheDocument();
+  });
+});
