2156f5a2ed4e0500bdee349da365219369ce4428
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-29 13:17:25 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import About from '../components/About';
+
+describe('Testando component About', () => {
+  it('Testa o subtítulo', () => {
+    renderWithRouter(<About />);
+    const subtítulo = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(subtítulo).toBeInTheDocument();
+  });
+
+  it('Testa a URL da imagem', () => {
+    renderWithRouter(<About />);
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imgElement = screen.getByRole('img');
+
+    expect(imgURL).toBeInTheDocument();
+    expect(imgElement.src).toBe(imgURL);
+  });
+
+  it('Testa informações da Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstInfo = screen.getByText(/application simulates a pokédex/i);
+    const secondInfo = screen.getByText(/one can filter pokémons by type/i);
+
+    expect(firstInfo).toBeInTheDocument();
+    expect(secondInfo).toBeInTheDocument();
+  });
+});
