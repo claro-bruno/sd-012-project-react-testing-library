9e619ae1bcdd9ffc2c609d0d31150fe592e9dc29
Gabriel <gribeiro237@gmail.com>
2021-07-29 15:54:02 -0300

    requisito 4 aproado pelo stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Testa se a página contém um h2 com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByText(/Page requested not found/i);
+    expect(h2).toBeDefined();
+  });
+  test('Testa se a página mostra a imagem do pikachu trsite', () => {
+    renderWithRouter(<NotFound />);
+    const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(screen.getAllByRole('img')[1]).toHaveProperty('src', imageUrl);
+    expect(screen.getAllByRole('img')[1]).toHaveClass('not-found-image');
+  });
+});
