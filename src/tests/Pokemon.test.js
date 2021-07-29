1f92b9bdf5ad94ec20524f1304aac4534f794f7a
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-29 10:15:19 -0300

    adiciona requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,56 +5,43 @@ import App from '../App';
 import pokemons from '../data';
 import renderWhithRouter from './renderWhithRouter.test';
 
-describe('Teste o componente Pokédex', () => {
-  beforeEach(() => {
+describe('Teste o componente Pokemon.js', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+
+    const pikachu = pokemons[0];
+    const { averageWeight, image, name, type } = pikachu;
+    const { value, measurementUnit } = averageWeight;
+    const Button = screen.getByRole('button', { name: /Electric/i });
+    userEvent.click(Button);
+    const getNamePokemon = screen.getByTestId(/pokemon-name/i);
+    expect(getNamePokemon).toHaveTextContent(/pikachu/i);
+    const powerType = screen.getByTestId('pokemon-type');
+    expect(powerType).toHaveTextContent(type);
+    const getAverageWeigth = screen.getByTestId(/pokemon-weight/i);
+    expect(getAverageWeigth)
+      .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+    const getImage = screen.getByRole('img');
+    expect(getImage.src).toBe(image);
+    expect(getImage.alt).toBe(`${name} sprite`);
+  });
+
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link', () => {
+    const { history } = renderWhithRouter(<App />);
+    const linkToDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkToDetails).toBeInTheDocument();
+    userEvent.click(linkToDetails);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
     renderWhithRouter(<App />);
-  });
-
-  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-    const title = screen.getByRole('heading', { name: /Encountered pokémons/i });
-    expect(title).toBeInTheDocument();
-  });
-
-  test('É exibido o prox Pokémon da lista quando o btn Próximo pokémon é clicado', () => {
-    const btn = screen.getByRole('button', { name: /Próximo pokémon/i });
-    pokemons.forEach((next) => {
-      const pokemon = screen.getByText(next.name);
-      expect(pokemon).toBeDefined();
-      userEvent.click(btn);
-    });
-    const nextPokemon = screen.getByText(pokemons[0].name);
-    expect(nextPokemon).toBeInTheDocument();
-  });
-
-  test('Teste se é mostrado apenas um Pokémon por vez', () => {
-    const btnFilter = screen.getByTestId('pokemon-name');
-    expect(btnFilter).toBeInTheDocument();
-  });
-
-  test('Teste se a Pokédex tem os botões de filtro', () => {
-    const btnReset = screen.getAllByTestId('pokemon-type-button');
-    expect(btnReset).toBeDefined();
-    btnReset.forEach((pokemon) => {
-      const checkButton = screen.getByRole('button', { name: pokemon.textContent });
-      userEvent.click(checkButton);
-      const checkType = screen.getByTestId('pokemon-type');
-      expect(checkButton).toHaveTextContent(checkType.textContent);
-      const nextpok = screen.getByTestId('next-pokemon');
-      userEvent.click(nextpok);
-    });
-  });
-
-  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-    const btnReset = screen.getByRole('button', { name: /All/i });
-    expect(btnReset).toBeDefined();
-    userEvent.click(btnReset);
-    const buttonAll = screen.getByRole('button', { name: /Próximo pokémon/i });
-    pokemons.forEach((next) => {
-      const pokemon = screen.getByText(next.name);
-      expect(pokemon).toBeDefined();
-      userEvent.click(buttonAll);
-    });
-    const checkfirstPkm = screen.getByText(pokemons[0].name);
-    expect(checkfirstPkm).toBeDefined();
+    const clickDetails = screen.getByText(/more details/i);
+    userEvent.click(clickDetails);
+    const clickFavorite = screen.getByText(/pokémon favoritado?/i);
+    userEvent.click(clickFavorite);
+    const verifyFavorite = screen.getByAltText('Pikachu is marked as favorite');
+    expect(verifyFavorite).toHaveAttribute('src', '/star-icon.svg');
   });
 });
+// Requisito feito com Wadson!
