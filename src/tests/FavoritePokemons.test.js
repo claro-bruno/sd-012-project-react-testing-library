8deef0022ff7e3d7cbf13f175369fe3ae316044e
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-28 23:47:08 -0300

    add req 3 completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
-// import * as FavoritePokemons from '../components/FavoritePokemons';
-import { FavoritePokemons } from '../components';
-
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('Testa o componente FavoritePokemons', () => {
-  it('Teste se é exibido na tela a mensagem No favorite pokemon found, se a pessoa não tiver pokémons favoritos', () => {
-    render(<FavoritePokemons />);
-if(FavoritePokemons.isEmpty) {
-    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
-// } else {
-//     expect(screen.getAllByText('favorite-pokemons')).toBeDefined();
-}
+  it(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+  se a pessoa não tiver pokémons favoritos`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    expect(screen.getByText(/No favorite pokemon found/i));
+  });
+  it(`Teste se é exibido todos os 
+  cards de pokémons favoritados`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    userEvent.click(screen.getByLabelText(/Pokémon favoritado/i));
+    history.push('/favorites');
+    expect(screen.getByText(/pikachu/i));
   });
 });
