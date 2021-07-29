858e69ac396c4866386569f069157229f25ca7f9
Gutemberg <gttj2010@hotmail.com>
2021-07-29 12:10:50 -0300

    Requisitos do 1 a 6 - Comitao Bomba Deu Ruim o 1º!

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
