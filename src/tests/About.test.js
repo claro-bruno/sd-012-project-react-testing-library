f757629745fa2ddb17a0460c27db4e52d13aec12
Pedro Resck <pedroresck1@gmail.com>
2021-07-29 13:18:24 -0300

    Requisito 2 - Testando o componente About.js - CORRIGIDO

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,19 +5,15 @@ import renderWithRouter from './renderWithRouter';
 
 describe('Teste do componente About.js.', () => {
   beforeEach(() => renderWithRouter(<About />));
-  it('Verifica se: a página possui um h2 com o texto:"About Pokédex".', () => {
-    const textAbout = screen.getAllByRole('heading', { name: /About Pokédex/i });
-    expect(textAbout).toBeInTheDocument();
+
+  it('Verifica se: a página possui 2 parágrafos sobre a Pokédex', () => {
+    const numberOfParagraph = screen.getAllByText(/pokémons/i);
+    expect(numberOfParagraph.length).toBe(2);
   });
 
-  it('Verifica se: a página tem 2 parágrafos sobre a Pokédex.', () => {
-    const firstParagraph = 'This application simulates a Pokédex, a digital encyclopedia'
-      + ' containing all Pokémons';
-    const secondParagraph = 'One can filter Pokémons by type, and see more details'
-      + 'for each one of them';
-
-    expect(screen.getByText(firstParagraph)).toBeInTheDocument();
-    expect(screen.getByText(secondParagraph)).toBeInTheDocument();
+  it('Verifica se: a página possui um h2 com o texto:"About Pokédex".', () => {
+    const textAbout = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(textAbout).toBeInTheDocument();
   });
 
   it('Verifica se: a página possui a seguinte imagem da Pokédex.', () => {
