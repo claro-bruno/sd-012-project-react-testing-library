89537e16fc4b179d53bbaefb094a72ab69ec56d8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-30 00:37:06 -0300

    Testando componente NotFound / Concluido task 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa componente NotFound', () => {
+  beforeEach(() => render(<NotFound />));
+  test('Verifica se o componente renderiza a msg correta', () => {
+    const message = screen.getByText('Page requested not found');
+    expect(message).toBeInTheDocument();
+  });
+  test('Verifica se renderia a img do Pikachu', () => {
+    const img = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(img).toBeInTheDocument();
+  });
+});
