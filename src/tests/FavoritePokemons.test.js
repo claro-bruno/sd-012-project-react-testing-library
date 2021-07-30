62413764ecc2e77f03404e8bba3927153cd82285
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-07-29 23:38:29 -0300

    Testando componente FavoritePokemons / Concluido task 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+import renderWithRouter from './Helper/RendeWithRouter';
+
+const mockFavPokemons = pokemons
+  .filter((pokemon) => pokemon.name === 'Charmander' || pokemon.name === 'Pikachu');
+
+describe('Testa componente FavoritePokemons', () => {
+  test('Verifica a msg retornada quando não há nenhum Pokemon favotitado', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+  test('Verifica se o componente renderiza os pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockFavPokemons } />);
+
+    const charmander = screen.getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
