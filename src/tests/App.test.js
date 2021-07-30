a987e233a5197d98efa5aa4dc2f8557d1ec1e58a
Ed Santos <edsantos2822@gmail.com>
2021-07-30 17:11:43 -0300

    Finaliza - Requisito 1 - Testando App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1 - Testando o componente <App.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Testa se há um <Link /> com o texto "Home"', () => {
+    const linkHome = screen.getByText('Home');
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('Testa se há um <Link /> com o texto "About"', () => {
+    const linkAbout = screen.getByText('About');
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('Testa se há um <Link /> com o texto "Favorite Pokémons"', () => {
+    const linkFavoritePokemons = screen.getByText('Favorite Pokémons');
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+});
