d7a8f4eaf521453f017557223d4c9b9b7578f210
Ed Santos <edsantos2822@gmail.com>
2021-07-30 19:36:32 -0300

    Corrige Lint - Requisito 3 -

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -9,7 +9,7 @@ describe('Requisito 3 - Tenstando o componente <FavoritePokemons />', () => {
 
   // Consultei o repositório de Andre Hammel para conclusão deste requisito na linha 14.
   // Link: https://github.com/tryber/sd-012-project-react-testing-library/pull/159/commits/97e0f1e5a052c60dae6352fc94d483b1be3736cc
-  
+
   it('Testa quando não há Pokémons favoritados', () => {
     const noFavoritePkm = screen.getByText('No favorite pokemon found');
     expect(noFavoritePkm).toBeDefined();
