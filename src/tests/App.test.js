48348de59b11f62a25f6c46ca19f5833c90bc259
Luís Seixas <luiseixas2@gmail.com>
2021-07-30 19:41:36 -0300

    implementa o requisito 1 - Testes no APP.js

---
+++ src/tests/App.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verificando todo o meu component APP', () => {
+  it('Se ao clicar no botao home redireciona pra raiz da minha aplicacao', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    expect(homeLink).toHaveTextContent('Home');
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Se ao clicar no botao About redireciona pra `/about`', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByText(/about/i);
+    expect(aboutLink).toHaveTextContent('About');
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Se ao clicar no botao Favorite redireciona pra `/favorites`', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByText(/favorite pokémons/i);
+    expect(favoritesLink).toHaveTextContent('Favorite Pokémons');
+    userEvent.click(favoritesLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Se app é redirecionado para a página NotFound ao entrar em uma unknown URL', () => {
+    const { history } = renderWithRouter(<App />);
+
+    let notFound = screen.queryByText(/not found/i);
+    expect(notFound).toBeNull();
+    history.push('/hdiahuaihsdui');
+    notFound = screen.queryByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
