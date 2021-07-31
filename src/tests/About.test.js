435fa6cd5faf3546f480d0b343ff45be38272c19
Janderson Soares <jandinsm@gmail.com>
2021-07-31 11:49:42 -0300

    Desafio 2, mudei a rederizaçao de App para About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
+
+// import BrowserRouter from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
+
+describe('Verifica About.test.js', () => {
+  test('Verifica se h2', () => {
+    renderWithRouter(<About />);
+
+    const AboutH2 = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(AboutH2).toBeDefined();
+    const imgAbout = screen.getByRole('img');
+    expect(imgAbout.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import App from '../App';
+
 import renderWithRouter from './renderWithRouter';
 
 // import BrowserRouter from 'react-router-dom';
