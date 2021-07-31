1c275366e774eb6d8e7ab748df4c80d955b52056
Luís Seixas <luiseixas2@gmail.com>
2021-07-30 21:44:50 -0300

    implementa requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando todo o meu componente FavoritePokemon', () => {
+  it('Se renderiza `FavoritesPokemon` com Heading `Favorite Pokémons`', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const heading = screen.getByRole('heading');
+    expect(heading.tagName).toBe('H2');
+    expect(heading).toHaveTextContent(/Favorite pokémons/i);
+  });
+
+  it('Testando se renderiza texto sem nenhum Pokemon', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFavPokMsg = screen.getByText('No favorite pokemon found');
+    expect(notFavPokMsg).toBeInTheDocument();
+  });
+
+  it('if renders a favorited pokemon', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByText('More details');
+    fireEvent.click(detailsLink);
+    const favoriteInput = screen.getByRole('checkbox');
+    expect(favoriteInput.checked).toBe(false);
+    fireEvent.click(favoriteInput);
+    expect(favoriteInput.checked).toBe(true);
+    const favoriteLink = screen.getByText('Favorite Pokémons');
+    fireEvent.click(favoriteLink);
+    const pokemons = screen.getAllByRole('img');
+    expect(pokemons).toHaveLength(2);
+    expect(pokemons[1].alt).toBe('Pikachu is marked as favorite');
+  });
+});
