e8b06545a6bc61a64369b4cc0bbf7939b6f5b344
Janderson Soares <jandinsm@gmail.com>
2021-07-31 14:46:24 -0300

    Desafio ,5 history.push

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import { NotFound } from '../components';
+import App from '../App';
 
 describe('Verifica NotFound.test.js', () => {
-  test('Verifica se h2 em notFound', () => {
-    renderWithRouter(<NotFound />);
+  test('Verificar texto h2 exist em notFound', () => {
+    const { history } = renderWithRouter(<App />);
 
-    const NotFoun = screen.getByRole('heading', { name: /Page requested not found 😭/i });
-    expect(NotFoun).toBeDefined();
-    const imgNoutFound = screen.getByRole('img');
-    expect(imgNoutFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    history.push('/xablau');
+
+    const hFou = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(hFou).toBeInTheDocument();
+
+    const imgNoutFound = screen.getAllByRole('img');
+    expect(imgNoutFound[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica App.test.js', () => {
+  test('Verifica se a links na pagina Inicial', () => {
+    renderWithRouter(<App />);
+
+    const textPoke = screen.getByText(/Encountered pokémons/i);
+    expect(textPoke).toBeDefined();
+    const bttn = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(bttn).toBeDefined();
+
+    userEvent.click(bttn);
+    const imgPok = screen.getByRole('img');
+    expect(imgPok.src).toBe('https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+  });
+});
