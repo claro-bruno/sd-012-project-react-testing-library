565a30063c9fc9c2fef6c616de5a4bc929c159ca
Janderson Soares <jandinsm@gmail.com>
2021-07-31 12:08:41 -0300

    Desafio 3, FavoriteP

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+// import BrowserRouter from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
+
+describe('Verifica About.test.js', () => {
+  test('Verifica se o texto -No found- é encontrado quando não há favorite', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const FavoriteP = screen.getByText(/No favorite pokemon found/i);
+    expect(FavoriteP).toBeDefined();
+  });
+});
