77c8be489f30f9f05316cd549f5cac1178a1d1b5
Luís Seixas <luiseixas2@gmail.com>
2021-07-30 21:08:12 -0300

    tentando o requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -33,8 +33,7 @@ describe('Verificando todo o meu component ABOUT', () => {
 
     const img = screen.getByRole('img');
     expect(img).toBeInTheDocument();
-    const imgSource = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex'
-    + '.png/800px-Gen_I_Pok%C3%A9dex.png';
-    expect(img.src).toBe(imgSource);
+    expect(img.tagName).toBe('IMG');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
