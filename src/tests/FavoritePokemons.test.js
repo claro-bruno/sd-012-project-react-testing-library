dc6865f067de8bb39bfe0cf870c267d654d0c4c8
zLibrian <kevinwillian084@gmail.com>
2021-08-01 18:39:43 -0300

    Add requisite 03, FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import { mockPokemons } from '../helpers/mockPokemons';
+
+describe('', () => {
+  it('tests if there is some message with "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundMessage = screen.getByText('No favorite pokemon found');
+    expect(notFoundMessage).toBeDefined();
+  });
+
+  it('tests if all favorite pokemons are rendered ', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockPokemons } />);
+    mockPokemons.forEach((pokemon) => {
+      const currentPokemon = screen.getByText(pokemon.name);
+      expect(currentPokemon).toBeDefined();
+    });
+  });
+});
