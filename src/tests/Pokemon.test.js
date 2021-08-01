8b5ee43686aeeab99b69c91e31ce50422274b8c4
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-01 05:28:58 -0300

    Refatora componente Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,11 +7,10 @@ import Pokemon from '../components/Pokemon';
 
 const mockFavPokemons = pokemons
   .find((pokemon) => pokemon.name === 'Charmander');
-const fav = true;
 
 describe('Testa componente Pokemon', () => {
   test('Verifica se o card renderiza as informações de um pokemon', () => {
-    renderWithRouter(<Pokemon pokemon={ mockFavPokemons } />);
+    renderWithRouter(<Pokemon pokemon={ mockFavPokemons } isFavorite={ false } />);
 
     const namePok = screen.getByText(/charmander/i);
     expect(namePok).toBeInTheDocument();
@@ -24,6 +23,8 @@ describe('Testa componente Pokemon', () => {
 
     const imgPok = screen.getByRole('img', { name: /charmander sprite/i });
     expect(imgPok).toBeInTheDocument();
+    expect(imgPok).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+    expect(imgPok).toHaveAttribute('alt', 'Charmander sprite');
   });
   test('Verifica se renderia o link para mais detalhes do pokemon', () => {
     const { history } = renderWithRouter(<Pokemon pokemon={ mockFavPokemons } />);
@@ -35,10 +36,11 @@ describe('Testa componente Pokemon', () => {
     expect(history.location.pathname).toBe('/pokemons/4');
   });
   test('Verifica se renderiza o pokemon favoritado', () => {
-    renderWithRouter(<Pokemon pokemon={ mockFavPokemons } isFavorite={ fav } />);
+    renderWithRouter(<Pokemon pokemon={ mockFavPokemons } isFavorite />);
 
     const imgFav = screen.getByRole('img', { name: /charmander is marked as favorite/i });
     expect(imgFav).toBeInTheDocument();
     expect(imgFav).toHaveAttribute('src', '/star-icon.svg');
+    expect(imgFav).toHaveAttribute('alt', 'Charmander is marked as favorite');
   });
 });
