a3cbbafb9bce5cf0306b3360914b13b64ea6c3a6
zLibrian <kevinwillian084@gmail.com>
2021-08-01 19:05:49 -0300

    Add requisite 04, NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it('tests if there is some text with "Page requested not found"', () => {
+    const message = 'Page requested not found Crying emoji';
+    const notFoundMessage = screen.getByRole('heading', { name: message });
+    expect(notFoundMessage).toBeDefined();
+  });
+
+  it('tests if there is some image with Pikachu crying because the page requested was not found ', () => {
+    const pikachuImageAlt = 'Pikachu crying because the page requested was not found';
+    const cryingPikachuImage = screen.getByRole('img', { name: pikachuImageAlt });
+    expect(cryingPikachuImage.src).toStrictEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
