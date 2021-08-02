a774ff7da43dc65b6dedcb045f154f40a82c2d72
Luís Seixas <luiseixas2@gmail.com>
2021-08-02 07:07:39 -0300

    implementando o requisito 5 - Testes no componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes no componente renderWithRouter', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Se há um h2 com o texto "Encontered pokémons"', () => {
+    const headingH2 = screen.getByText('Encountered pokémons');
+    expect(headingH2).toBeInTheDocument();
+  });
+
+  it('Se é mostrado apenas 1 pokémon por vez', () => {
+    const pokemon = screen.getAllByTestId('pokemon-name');
+    expect(pokemon.length).toBe(1);
+  });
+
+  it('Se a Pokédex pussui os botões de filtro', () => {
+    const pokemonTypes = pokemons.map((pkm) => pkm.type);
+    const types = pokemonTypes.filter((type, i) => pokemonTypes.indexOf(type) === i);
+
+    const allBtn = screen.getByRole('button', { name: /All/i });
+    expect(allBtn).toBeDefined();
+    const buttonType = screen.getAllByTestId('pokemon-type-button');
+    expect(buttonType).toBeDefined();
+    types.forEach((type) => {
+      expect(allBtn).toBeDefined();
+      const buttonTypePk = screen.getByRole('button', { name: type });
+      expect(buttonTypePk).toBeDefined();
+      userEvent.click(buttonTypePk);
+    });
+    userEvent.click(allBtn);
+    expect(screen.getByText(`${pokemons[0].name}`)).toBeDefined();
+  });
+});
