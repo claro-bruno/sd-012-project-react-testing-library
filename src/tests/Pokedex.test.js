d10506ffba4ad4ce048ffc77ea3fe75fd106c4dd
Ed Santos <edsantos2822@gmail.com>
2021-08-02 15:10:38 -0300

    Finaliza - Requisito 5 - Testando o componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+describe('Requisito 5 - Testa o componenete <Pokedex />', () => {
+  const isPokemonFavorite = pokemons.reduce((acc, pokemon) => {
+    acc[pokemon.id] = false;
+    return acc;
+  }, {});
+
+  beforeEach(() => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavorite }
+    />);
+  });
+
+  it('Testa se há um <h2> com o texto "Encountered pokémons"', () => {
+    const takeH2 = screen.getByRole('heading', { name: /encountered pokémons/i });
+    expect(takeH2).toBeInTheDocument();
+  });
+});
