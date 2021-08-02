17bb081d648dbffa7136b8e7a76f3225c3ad0142
LucasPeixoto <peixotolucas516@gmail.com>
2021-08-02 11:51:34 -0300

    code refactored

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,31 +1,49 @@
-/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import About from '../components/About';
-import renderWithRouter from './renderWithRouter';
 
-describe('Teste se a página contém as informações sobre a Pokédex', () => {
-  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const aboutText = getByRole('heading', {
-      level: 2,
-      name: 'About Pokédex',
+describe('Testing About Component, Requirement 2', () => {
+  it('there is a level 2 heading with the "About Pokédex" text in the document',
+    () => {
+      render(
+        <MemoryRouter>
+          <About />
+        </MemoryRouter>,
+      );
+
+      expect(screen.getByRole('heading', {
+        level: 2,
+        name: 'About Pokédex',
+      }))
+        .toBeInTheDocument();
     });
-    expect(aboutText).toBeInTheDocument();
-  });
 
-  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    const { getAllByText } = renderWithRouter(<About />);
+  it('the first and second paragraph are about Pokédex',
+    () => {
+      render(
+        <MemoryRouter>
+          <About />
+        </MemoryRouter>,
+      );
+      const firstParagraph = screen.getAllByText(/Pokémons/)[0];
+      const secondParagraph = screen.getAllByText(/Pokémons/)[1];
 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    const paragraphQuant = getAllByText(/Pokémons/);
-    expect(paragraphQuant.length).toBe(2);
-  });
-  test('Teste se a página contém uma imagem de Pokédex', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const pokedexImage = getByRole('img');
-    expect(pokedexImage).toHaveAttribute(
-      'src',
-      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
-    );
-  });
+      expect(firstParagraph).toBeInTheDocument();
+      expect(secondParagraph).toBeInTheDocument();
+    });
+
+  it('the image have the appropriate source and Alt',
+    () => {
+      render(
+        <MemoryRouter>
+          <About />
+        </MemoryRouter>,
+      );
+
+      expect(screen.getByRole('img', {
+        name: 'Pokédex',
+      }))
+        .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    });
 });
