397b47c007621b8a72ae90154a9b5305afe3a163
Bruno <brunocostah@hotmail.com>
2021-08-03 00:56:09 -0300

    requisito 1 e 2 teste

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente "About".', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const heading = getByText(/About Pokédex/i);
+    expect(heading.localName).toBe('h2');
+  });
+
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragraph = getAllByText(/Pokémons/i);
+    expect(paragraph.length).toBe(2);
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    const imagePath = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(imagePath);
+  });
+});
