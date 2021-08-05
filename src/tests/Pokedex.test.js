83f298e94aed9983b421591b4e1c2303b3d5bb38
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-08-04 21:40:05 -0300

    R5-pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -7,31 +7,36 @@ describe('Testa componente <Pokedex />', () => {
   beforeEach(() => {
     renderWithRouter(<App />);
   });
-  test('Teste se página contém um heading h2 com texto Encountered Pokémons',() => {
+  test('Teste se página contém um heading h2 com texto Encountered Pokémons', () => {
     const headingh2 = screen.getByRole('heading',
-    { name : /Encountered Pokémons/i, level: 2 });
+      { name: /Encountered Pokémons/i, level: 2 });
     expect(headingh2).toBeInTheDocument();
   });
-  
-  const filtros = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
 
   test('Teste se ao clicar o proximo pokémon é exibido', () => {
-    const button =screen.getByRole('button', 
-    {  name: /Próximo pokémon/i,});
+    const button = screen.getByRole('button',
+      { name: /Próximo pokémon/i });
     fireEvent.click(button);
     expect(button).toBeInTheDocument();
-});
-test('Teste se é mostrado apenas um Pokémon por vez', () => {
-  expect(screen.getAllByTestId('pokemon-name').length).toBe(1);
-});
+  });
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    expect(screen.getAllByTestId('pokemon-name').length).toBe(1);
+  });
 
-test('Testa se a Pokedex tem os botões de filtro', () => {
-  const numberSeven = 7;
-  const buttonAll = screen.getByText('All');
-  const btnpokemons = screen.getAllByTestId('pokemon-type-button');
+  test('Testa se a Pokedex tem os botões de filtro', () => {
+    const numberSeven = 7;
+    const buttonAll = screen.getByText('All');
+    const btnpokemons = screen.getAllByTestId('pokemon-type-button');
 
-  expect(btnpokemons.length).toBe(numberSeven);
-  expect(buttonAll).toBeInTheDocument();
-  expect(btnpokemons[0].innerHTML).toBe('Electric');
-});
+    expect(btnpokemons.length).toBe(numberSeven);
+    expect(buttonAll).toBeInTheDocument();
+    expect(btnpokemons[0].innerHTML).toBe('Electric');
+  });
+  test('Testa se existe um botão para resetar o filtro', () => {
+    expect(screen.getByRole('button', { name: 'All' })).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: 'Poison' }));
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Ekans');
+    fireEvent.click(screen.getByRole('button', { name: 'All' }));
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+  });
 });
