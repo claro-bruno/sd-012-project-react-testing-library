4c567a9ec90d5ad0b21b247cb3d1f36bbf65f460
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-08-06 21:35:54 -0300

    nao sei mais o que fazer

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('testa componente About', () => {
+  test('teste se a página contém as informações sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+
+    expect(screen.getByRole('heading', { name: 'About Pokédex' }))
+      .toBeInTheDocument();
+
+    const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByAltText('Pokédex');
+    expect(img.src).toBe(link);
+
+    expect(screen.getByText(/This application/i));
+    expect(screen.getByText(/One can filter/i));
+  });
+});
