b6d488f3258d4b64d1c78c6b9ffea5afeaae02cf
Martin <escorpmartin97@gmail.com>
2021-08-08 17:39:04 -0300

    [Requisito 1- Verificaçao em componente App]

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,29 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('', () => {});
+describe('Teste o componente App.js', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  test('O primeiro link deve possuir o texto Home.', () => {
+    // acessar os elementos da tela
+    const textHome = screen.getByText('Home');
+    expect(textHome).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const textAbout = screen.getByText('About');
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const textFavoritePokémons = screen.getByText('Favorite Pokémons');
+    expect(textFavoritePokémons).toBeInTheDocument();
+  });
+});
+
+// interagir com eles se houver necessidade
+
+// fazer teste
