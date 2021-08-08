0a5678994b1883bff2eb686e00c0fa3d28fb65ea
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-08 20:21:00 -0300

    Testa se exibe na tela a mensagem sem pokemon favorito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  beforeEach(() => {
+    render(<FavoritePokemons />);
+  });
+
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found,'
+  + ' se a pessoa não tiver pokémons favoritos.', () => {
+    const noFavoritePokemon = screen.getByText('No favorite pokemon found');
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+});
