3878941a82a2c3be29f6cf3a0a288ef20636f085
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-08-09 16:58:32 -0300

    Adiciona requisito 5.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa todo o componente Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const correctMsg = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(correctMsg).toBeInTheDocument();
+  });
+  it('Teste se é exibido o próximo Pokémon da lista', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    userEvent.click(nextButton);
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+    userEvent.click(nextButton);
+    expect(screen.getByText('Caterpie')).toBeInTheDocument();
+  });
+  it('Testa se é mostrado apenas um Pokémon por vez', () => {
+    renderWithRouter(<App />);
+    const nameList = screen.getAllByTestId('pokemon-name');
+    expect(nameList.length).toBe(1);
+  });
+  it('Testa se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    const types = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    expect(buttons.length).toBe(types.length);
+    buttons.forEach((button, index) => expect(button).toHaveTextContent(types[index]));
+  });
+  it('Testa se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const allButton = screen.getByRole('button', { name: 'All' });
+    expect(allButton).toBeInTheDocument();
+    expect(allButton).toHaveTextContent('All');
+    userEvent.click(allButton);
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+  });
+});
