4a12aff6bcf5248d63b135911f182152869e3cc0
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-10 14:33:16 -0300

    Testando se o componente renderiza o gif do Pikachu chorando

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -12,4 +12,11 @@ describe('Testa o componente NotFound.js', () => {
       { level: 2, name: (content) => content.includes('Page requested not found') });
     expect(componentHeader).toBeInTheDocument();
   });
+
+  it('Testa se a página mostra a imagem', () => {
+    const altText = 'Pikachu crying because the page requested was not found';
+    const imagePikachuCrying = screen.getByAltText(altText);
+    const pikachuGif = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imagePikachuCrying.src).toBe(pikachuGif);
+  });
 });
