267d7a234122e494d69954f921317bfa37866df2
Martin <escorpmartin97@gmail.com>
2021-08-10 16:19:55 -0300

    [Metade do requsisito 3 - tela de favoritos]

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons />', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+  test('Exibir na tela a mensagem se a pessoa não tiver pokémons favoritos', () => {
+    const notFavoritePoquemon = screen.getByText('No favorite pokemon found');
+    expect(notFavoritePoquemon).toBeDefined();
+  });
+});
