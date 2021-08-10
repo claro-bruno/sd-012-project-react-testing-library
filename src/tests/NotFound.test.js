7621fbb1c01d58cb2e0e46abb4f3ac9a7ac1c5a3
Martin <escorpmartin97@gmail.com>
2021-08-10 17:17:29 -0300

    [Requsisito 4 - Tela de NotFound]

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente NotFound', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    const headingText = screen.getByRole('heading');
+    expect(headingText).toBeInTheDocument();
+    expect(headingText.tagName).toBe('H2');
+    expect(headingText).toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  test('Teste se a página mostra a seguinte imagem', () => {
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
