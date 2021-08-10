80fa7a44aab012177a25e5f2dd77d2de17634df6
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-10 14:51:20 -0300

    Testando se a pagina renderiza um h2 com o texto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Testa se a página renderiza um h2 com o texto', () => {
+    const headingPokedex = screen.getByRole('heading',
+      { level: 2, name: 'Encountered pokémons' });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+});
