1bec8726f4f491ca3499c65c2b805a36c775c9cb
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-10 14:39:32 -0300

    implementa test se rederiza not found com rota inexistente e com elementos corretos rederizados

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import NotFound from '../components/NotFound';
+import render from './renderWithRouter';
+
+describe('Verifica pagina "Not found".', () => {
+  it('Pagina "Not found" é rederizada quando uma rota inexistente é passada.', () => {
+    const { history } = render(<App />);
+    history.push('/erradateste');
+    const message = screen.getByText(/page requested not found/i);
+    expect(message).toBeInTheDocument();
+  });
+  it('Pagina "Not found" contém mensagem em h2 e gif corretos.', () => {
+    render(<NotFound />);
+    const gif = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    const message = screen.getByRole('heading', { level: 2 });
+    expect(message).toBeInTheDocument();
+    expect(gif).toBeInTheDocument();
+    expect(gif.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
