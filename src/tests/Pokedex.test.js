c046acc73f002d862519f33d711d61f2eec83d36
Ed Santos <edsantos2822@gmail.com>
2021-08-10 15:49:57 -0300

    Finaliza - Requisito 5 - Testa o componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import Pokedex from '../components/Pokedex';
+import App from '../App';
 import pokemons from '../data';
+import userEvent from '@testing-library/user-event';
 
 describe('Requisito 5 - Testa o componenete <Pokedex />', () => {
   const isPokemonFavorite = pokemons.reduce((acc, pokemon) => {
@@ -11,22 +12,44 @@ describe('Requisito 5 - Testa o componenete <Pokedex />', () => {
   }, {});
 
   beforeEach(() => {
-    renderWithRouter(<Pokedex
+    renderWithRouter(<App
       pokemons={ pokemons }
       isPokemonFavoriteById={ isPokemonFavorite }
     />);
   });
 
+  // const allPokemonsName = pokemons.map((pokemon) => pokemon.name);
+
+  // Desenvolvido com a ajuda do David Gonzaga
   it('Testa se há um <h2> com o texto "Encountered pokémons"', () => {
     const takeH2 = screen.getByRole('heading', { name: /encountered pokémons/i });
     expect(takeH2).toBeInTheDocument();
   });
 
   it('Testa se há um botão com o texto "Próximo pokémon"', () => {
-    const takeBtn = screen.getByTestId('next-pokemon');
-    const btnText = takeBtn.textContent;
+    const takeBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
 
     expect(takeBtn).toBeInTheDocument();
-    expect(btnText).toBe('Próximo pokémon');
+  });
+
+  it('Testa a funcionalidade do botão "All', () => {
+    const takeBtn = screen.getByRole('button', { name: /all/i });
+    const takeBtnPikachu = screen.getByText('Pikachu');
+
+    userEvent.click(takeBtn);
+    expect(takeBtn).toBeInTheDocument();
+    expect(takeBtnPikachu.textContent).toBe('Pikachu');
+  });
+
+  it('Testa se aparece o botao com o tipo do Pokémon', () => {
+    const takeBtn = screen.getByRole('button', { name: /electric/i });
+
+    expect(takeBtn).toBeInTheDocument();
+  });
+
+  it('Testa se há apenas 1 pokémon sendo mostrado', () => {
+    const pokemonAmount = screen.getAllByTestId('pokemon-name');
+    expect(pokemonAmount.length).toBe(1);
+    expect(pokemonAmount[0]).toBeInTheDocument();
   });
 });
