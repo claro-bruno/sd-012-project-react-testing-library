8d553b221c59bd7d78da3d5a86fe83ab68b82c18
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-10 14:28:56 -0300

    Testa se o h2 contem o texto descrito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound.js', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it('Testa se o componente renderiza um h2 com o texto', () => {
+    const componentHeader = screen.getByRole('heading',
+      { level: 2, name: (content) => content.includes('Page requested not found') });
+    expect(componentHeader).toBeInTheDocument();
+  });
+});
