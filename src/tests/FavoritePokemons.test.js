9baedbd7fc07c2d4a432a86aa71fdcf80da1319b
Uriel <urielbochi@gmail.com>
2021-08-10 18:49:18 -0300

    Requisito 3, verificação dos pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Verifica o componente FavoritePokemons.test.js', () => {
+  test('Verifica se é exibido: "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
