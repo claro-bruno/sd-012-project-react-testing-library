c36fe521fde20284026f81a1dba0642bde926ef8
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-11 11:45:13 -0300

    corrigindo linter

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,13 +5,15 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Testa o componente Pokemon.js', () => {
+  const moreDetails = 'More details';
+
   it('testa se é renderizado um card com as informações do pokemon', () => {
     renderWithRouter(<App />);
     const pokemonName = screen.getByTestId('pokemon-name');
     const pokemonType = screen.getByTestId('pokemon-type');
     const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const detailedInfoLink = screen.getByRole('link', { name: moreDetails });
     const nextPokemonBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
-    const detailedInfoLink = screen.getByRole('link', { name: 'More details' });
     pokemons.forEach((pokemon) => {
       const { id, name, type, averageWeight, image } = pokemon;
       const pokemonImage = screen.getByAltText(`${name} sprite`);
@@ -31,14 +33,14 @@ describe('Testa o componente Pokemon.js', () => {
 
   it('testa se ao clicar no link de detalhes do pokemon a página é redirecionada', () => {
     const { history } = renderWithRouter(<App />);
-    const detailedInfoLink = screen.getByRole('link', { name: 'More details' });
+    const detailedInfoLink = screen.getByRole('link', { name: moreDetails });
     fireEvent.click(detailedInfoLink);
     expect(history.location.pathname).toBe('/pokemons/25');
   });
 
   it('testa se existe um ícone de estrela nos pokémons favoritados', () => {
     const { history } = renderWithRouter(<App />);
-    const detailedInfoLink = screen.getByRole('link', { name: 'More details' });
+    const detailedInfoLink = screen.getByRole('link', { name: moreDetails });
     fireEvent.click(detailedInfoLink);
     expect(history.location.pathname).toBe('/pokemons/25');
     const toFavoritePokemon = screen.getByLabelText('Pokémon favoritado?',
