19f6eb3776265b72e3c6ac0697456289920552b9
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-11 12:10:23 -0300

    setup inicial

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testa a página de detalhes do Pokemon', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    fireEvent.click(moreDetailsLink);
+  });
+
+  it('testa se as inforamções detalhadas são exibidas na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    fireEvent.click(moreDetailsLink);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+});
