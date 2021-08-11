8016bc558057227981d233fb5beaadffb8e582b5
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-08-11 12:59:59 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('`Not found` page tests', () => {
+  test('Check if the title is the one expect', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/wrongurl');
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Page requested not found');
+    const image = screen.getAllByRole('img');
+    const imgSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image[1]).toHaveAttribute('src', imgSource);
+  });
+});
