65bfd8f4fd52f9beedbb8588fc7625c9ac2d3749
Djaniza Vasques <djanizav@gmail.com>
2021-08-11 11:16:09 -0300

    Adiciona testes para FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import historyRender from './GenericHistory';
+
+describe('Testa componente About', () => {
+  test('Testa se aparece a mensagem quando não há favoritos', () => {
+    historyRender(<FavoritePokemons pokemons={ [] } />);
+    const nofavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(nofavorite).toBeInTheDocument();
+  });
+});
