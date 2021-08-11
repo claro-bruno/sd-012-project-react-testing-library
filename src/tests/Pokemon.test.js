cbe49bf66d408a36e33e1829898deb16165cd52c
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-11 11:24:07 -0300

    testa se o link leva para a página de detalhes do pokemon correto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -11,14 +11,17 @@ describe('Testa o componente Pokemon.js', () => {
     const pokemonType = screen.getByTestId('pokemon-type');
     const pokemonWeight = screen.getByTestId('pokemon-weight');
     const nextPokemonBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const detailedInfoLink = screen.getByRole('link', { name: 'More details' });
     pokemons.forEach((pokemon) => {
-      const { name, type, averageWeight } = pokemon;
+      const { id, name, type, averageWeight } = pokemon;
       expect(pokemonName).toHaveTextContent(name);
       expect(pokemonType).toHaveTextContent(type);
       expect(pokemonWeight)
         .toHaveTextContent(
           `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`,
         );
+      // Aqui resolvi centralizar o teste, aqui testa se o link leva para a página de detalhes desse pokemon.
+      expect(detailedInfoLink.href).toBe(`http://localhost/pokemons/${id}`);
       fireEvent.click(nextPokemonBtn);
     });
   });
