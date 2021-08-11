cc7b73a5ddf97ad4e6ea81e8988ea6b134bbef74
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-11 11:12:52 -0300

    testando se são renderizadas as informações sobre o pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa o componente Pokemon.js', () => {
+  it('testa se é renderizado um card com as informações do pokemon', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const nextPokemonBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    pokemons.forEach((pokemon) => {
+      const { name, type, averageWeight } = pokemon;
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toHaveTextContent(type);
+      expect(pokemonWeight)
+        .toHaveTextContent(
+          `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`,
+        );
+      fireEvent.click(nextPokemonBtn);
+    });
+  });
+});
