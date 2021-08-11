dfce8383a8d0be481f19ebdd62f2624f9906e48d
Djaniza Vasques <djanizav@gmail.com>
2021-08-11 11:16:54 -0300

    Corrige lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import historyRender from './GenericHistory';
 
