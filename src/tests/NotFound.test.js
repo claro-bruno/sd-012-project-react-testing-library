976e43b3ccf356075c784c9f887297e5ca4b5d91
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-08-11 16:43:23 -0300

    Fin Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testes do componente NotFound.js', () => {
+  it('Checa o texto do heading h2', () => {
+    renderWithRouter(<NotFound />);
+
+    const head = screen.getByText('Page requested not found');
+
+    expect(head).toBeInTheDocument();
+  });
+
+  it('Checa a imagem do componente', () => {
+    renderWithRouter(<NotFound />);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const alt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(alt);
+    expect(img).toHaveProperty('src', url);
+  });
+});
