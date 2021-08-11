b4e386f7ee6138c8c52ebb48fb4fd5bea800b3e3
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-08-11 13:10:57 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('Pokedex tests', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  test('Test title', () => {
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Encountered pokémons');
+  });
+
+  test('Tests the `Próximo pokémon` button', () => {
+    const button = screen.getByTestId('next-pokemon');
+    expect(button).toHaveTextContent('Próximo pokémon');
+    userEvent.click(button);
+  });
+
+  test('Check if there is filter buttons', () => {
+    const allButton = screen.getByRole('button', { name: 'All' });
+    expect(allButton).toBeDefined();
+    userEvent.click(allButton);
+    const types = screen.getAllByTestId('pokemon-type-button');
+    expect(types.length).toBe(types.length);
+    const bugButton = screen.getByRole('button', { name: 'Bug' });
+    userEvent.click(bugButton);
+  });
+});
