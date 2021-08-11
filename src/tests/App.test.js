61cc3c3d6e89891a3dc6fc0d9c6d0bee264c723c
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-08-11 12:02:43 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helper/renderWithRouter';
+import App from '../App';
+
+describe('Requirement 1', () => {
+  test('`Home` link test', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    expect(home).toBeDefined();
+    userEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('`About` link test', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: 'About' });
+    expect(about).toBeDefined();
+    userEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('`Favorite Pokémons` link test', () => {
+    const { history } = renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favorite).toBeDefined();
+    userEvent.click(favorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('`Not Found` link test', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/doesntexist');
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeDefined();
+  });
+});
