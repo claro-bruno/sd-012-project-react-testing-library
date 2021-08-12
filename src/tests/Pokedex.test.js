b85ecbfd75115f229a3818c0da6d7c1481144d56
Janderson Soares <jandinsm@gmail.com>
2021-08-11 22:50:58 -0300

    Pokedex test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,9 +3,10 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
 describe('Verifica Pokedex.test.js', () => {
-  test('Verifica se a links na pagina Inicial', () => {
+  test('Verifica se a links na pagina Inicial e botões', () => {
     renderWithRouter(<App />);
 
     const textPoke = screen.getByText(/Encountered pokémons/i);
@@ -14,7 +15,23 @@ describe('Verifica Pokedex.test.js', () => {
     expect(bttn).toBeDefined();
 
     userEvent.click(bttn);
-    const imgPok = screen.getByRole('img');
-    expect(imgPok.src).toBe('https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+    expect(screen.getByText(pokemons[1].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[2].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[3].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[4].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[5].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[6].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[7].name)).toBeDefined();
+    userEvent.click(bttn);
+    expect(screen.getByText(pokemons[8].name)).toBeDefined();
+    userEvent.click(bttn);
+    userEvent.click(screen.getByRole('button', { name: 'Electric' }));
+    userEvent.click(screen.getByRole('button', { name: 'All' }));
   });
 });
