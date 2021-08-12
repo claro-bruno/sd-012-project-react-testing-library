5089d8a78f3af0aadbbc002d264b0f4cccd60d44
Martin <escorpmartin97@gmail.com>
2021-08-11 23:12:16 -0300

    [Requisito 7 - Testando componente PokemonDetails]

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('Teste o componente PokemonDetails', () => {
-  beforeEach(() => {
+  it('Usuário é redirecionado para a tela de detalhes do Pokémons', () => {
     renderWithRouter(<App />);
-  });
-  test('Teste se as informações detalhadas do Pokémon são mostradas na tela.', () => {
-
+    const details = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(details);
+    const title = screen.getAllByRole('heading', { level: 2 });
+    expect(title[0]).toHaveTextContent('Pikachu Details');
+    expect(title[1]).toHaveTextContent('Summary');
+    expect(details).not.toBeInTheDocument();
+    const paragraph = screen.getByText(/This intelligent Pokémon roasts hard berries/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(title[2]).toHaveTextContent('Game Locations of Pikachu');
+    const images = screen.getAllByRole('img');
+    expect(images.length).toBe(images.length);
+    const imgSrc = 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png';
+    const imgAlt = 'Pikachu location';
+    expect(images[1]).toHaveAttribute('src', imgSrc);
+    expect(images[1]).toHaveAttribute('alt', imgAlt);
+    const check = screen.getByRole('checkbox');
+    userEvent.click(check);
+    screen.getByLabelText('Pokémon favoritado?');
   });
 });
