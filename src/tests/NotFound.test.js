0c2c3f31aaa8dcc6f127ede236c5c0c8aabe1f3e
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-08-12 13:42:27 -0300

    Termina requesito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../components/RenderWithRouter';
+
+describe('Teste todo o componente NotFound', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const correctMsg = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(correctMsg).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem do Pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+    const images = screen.getAllByRole('img');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(images).toHaveLength(2);
+    expect(images[1]).toBeInTheDocument();
+    expect(images[1]).toHaveAttribute('src', url);
+  });
+});
