e1d3eb00863724dce80fc5aef8f34476fa172206
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-08-12 00:02:31 -0300

    Refatora Req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,87 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const nextString = 'Próximo pokémon';
+
+describe('Teste do Pokedex.js', () => {
+  it('Checa o heading', () => {
+    renderWithRouter(<App />);
+
+    const btn = screen.getByText('Encountered pokémons');
+
+    expect(btn).toBeInTheDocument();
+  });
+
+  it('Checa a exibição do Pokémon ao clicar', () => {
+    renderWithRouter(<App />);
+
+    const btn = screen.getByText(nextString);
+    const pikachu = screen.getByText('Pikachu');
+
+    expect(pikachu).toBeInTheDocument();
+
+    fireEvent.click(btn);
+
+    const charmander = screen.getByText('Charmander');
+
+    expect(charmander).toBeInTheDocument();
+  });
+
+  it('Checar se um Pokémon é mostrado por vez', () => {
+    renderWithRouter(<App />);
+
+    const pokeNames = screen.getAllByTestId('pokemon-name');
+
+    expect(pokeNames).toHaveLength(1);
+  });
+
+  it('Checa os botões de filtro do Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const typePokemons = screen.getAllByTestId('pokemon-type-button');
+    const seven = 7;
+
+    expect(typePokemons).toHaveLength(seven);
+
+    const btnTypePokemon = screen.getByText('Fire');
+
+    fireEvent.click(btnTypePokemon);
+
+    const charmander = screen.getByText('Charmander');
+
+    expect(charmander).toBeInTheDocument();
+
+    const btnNext = screen.getByText(nextString);
+
+    fireEvent.click(btnNext);
+
+    const rapidash = screen.getByText('Rapidash');
+
+    expect(rapidash).toBeInTheDocument();
+
+    const btnAll = screen.getByText('All');
+    expect(btnAll).toBeInTheDocument();
+  });
+
+  it('Checa o btn de reset', () => {
+    renderWithRouter(<App />);
+
+    const allBtn = screen.queryByText('All');
+
+    expect(allBtn).toBeInTheDocument();
+
+    fireEvent.click(allBtn);
+
+    const btnNext = screen.getByText(nextString);
+
+    fireEvent.click(btnNext);
+
+    const pikachu = screen.queryByText('Pikachu');
+
+    expect(pikachu).toBeNull();
+  });
+});
+
+// Utilizar a negação do toBeInTheDocument ao checar a existência de um elemento capturado, por exemplo, pelo queryByText.
