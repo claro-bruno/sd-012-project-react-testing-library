41833e8557f5a1996c7a1777298fbf3c71d833b1
Martin <escorpmartin97@gmail.com>
2021-08-11 22:42:42 -0300

    [Requisito 5 - Testando componente PokemonDetails]

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -12,5 +13,32 @@ describe('Teste o componente Pokedex.js', () => {
     const headingH2 = screen.getByText('Encountered pokémons');
     expect(headingH2).toBeInTheDocument();
     expect(headingH2.tagName).toBe('H2');
+  });
+  test('Exibir o próximo Pokémon da lista', () => {
+    const button = screen.getByText('Próximo pokémon');
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    const proxPokemon = screen.getByTestId('pokemon-name');
+    expect(proxPokemon).toBeInTheDocument();
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    const btnAll = screen.getByText('All');
+    expect(btnAll).toBeInTheDocument();
+    userEvent.click(btnAll);
+    const pokedex = screen.getByText('Pikachu');
+    expect(pokedex).toBeInTheDocument();
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    const types = 7;
+    const btn = screen.getAllByTestId('pokemon-type-button');
+    expect(btn.length).toBe(types);
+    const fireBtn = screen.getAllByTestId('pokemon-type-button')[1];
+    expect(fireBtn).toBeInTheDocument();
+    expect(fireBtn).toHaveTextContent('Fire');
+    userEvent.click(fireBtn);
+    const primeiro = screen.getByText('Charmander');
+    expect(primeiro).toBeInTheDocument();
   });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente PokemonDetails', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  test('', () => {
+
+  });
+});
